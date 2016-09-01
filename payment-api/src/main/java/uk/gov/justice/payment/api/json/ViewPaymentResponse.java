91cafdefe395f4694ce7b3325da0c553ef01ac87
zeeshan-alam <zeeshan_alam@yahoo.com>
2016-09-01 15:54:54 +0100

    View Payment Funtionality Value Objects

---
+++ payment-api/src/main/java/uk/gov/justice/payment/api/json/ViewPaymentResponse.java
@@ -0,0 +1,150 @@
+
+package uk.gov.justice.payment.api.json;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "total",
+    "count",
+    "page",
+    "results",
+    "_links"
+})
+public class ViewPaymentResponse {
+
+    @JsonProperty("total")
+    private Integer total;
+    @JsonProperty("count")
+    private Integer count;
+    @JsonProperty("page")
+    private Integer page;
+    @JsonProperty("results")
+    private List<Result> results = new ArrayList<Result>();
+    @JsonProperty("_links")
+    private Links_ links;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The total
+     */
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    /**
+     * 
+     * @param total
+     *     The total
+     */
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    /**
+     * 
+     * @return
+     *     The count
+     */
+    @JsonProperty("count")
+    public Integer getCount() {
+        return count;
+    }
+
+    /**
+     * 
+     * @param count
+     *     The count
+     */
+    @JsonProperty("count")
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    /**
+     * 
+     * @return
+     *     The page
+     */
+    @JsonProperty("page")
+    public Integer getPage() {
+        return page;
+    }
+
+    /**
+     * 
+     * @param page
+     *     The page
+     */
+    @JsonProperty("page")
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    /**
+     * 
+     * @return
+     *     The results
+     */
+    @JsonProperty("results")
+    public List<Result> getResults() {
+        return results;
+    }
+
+    /**
+     * 
+     * @param results
+     *     The results
+     */
+    @JsonProperty("results")
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    /**
+     * 
+     * @return
+     *     The links
+     */
+    @JsonProperty("_links")
+    public Links_ getLinks() {
+        return links;
+    }
+
+    /**
+     * 
+     * @param links
+     *     The _links
+     */
+    @JsonProperty("_links")
+    public void setLinks(Links_ links) {
+        this.links = links;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
