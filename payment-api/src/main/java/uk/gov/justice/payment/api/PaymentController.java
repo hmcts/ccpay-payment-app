73802e8b68357c04372b750eb22d716ff49b481e
zeeshan-alam <zeeshan_alam@yahoo.com>
2016-09-12 15:03:06 +0100

    Added swagger support and error handling

--- payment-api/src/main/java/uk/gov/justice/payment/api/PaymentController.java
+++ payment-api/src/main/java/uk/gov/justice/payment/api/PaymentController.java
@@ -12,6 +12,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpsParameters;
+import io.swagger.annotations.*;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -41,22 +44,39 @@ public class PaymentController {
     private String url;
     private String BEARER = "Bearer ";;
 
+    @ApiOperation(value = "Create payment", notes = "Create payment")
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created payment"),
+            @ApiResponse(code = 500, message = "Internal server error", response = java.lang.String.class)
+    })
     @RequestMapping(value = "/payments", method=RequestMethod.POST)
-    public CreatePaymentResponse createPayment(@RequestBody CreatePaymentRequest createPaymentRequest) {
-        logger.debug("Request : " + getJson(createPaymentRequest));
+    public ResponseEntity<CreatePaymentResponse> createPayment(@ApiParam(value = "payment request body") @RequestBody CreatePaymentRequest payload) {
+        logger.debug("Request : " + getJson(payload));
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set(HttpHeaders.AUTHORIZATION, BEARER +authKey);
-        HttpEntity<CreatePaymentRequest> entity = new HttpEntity<CreatePaymentRequest>(createPaymentRequest,headers);
-        CreatePaymentResponse response = restTemplate.postForObject(url, entity, CreatePaymentResponse.class);
+        HttpEntity<CreatePaymentRequest> entity = new HttpEntity<CreatePaymentRequest>(payload,headers);
+        ResponseEntity<CreatePaymentResponse> response = restTemplate.exchange(url, HttpMethod.POST, entity, CreatePaymentResponse.class);
         logger.debug("Response : " + getJson(response));
-        return response;
+        if(HttpStatus.CREATED.equals(response.getStatusCode())) {
+            return response;
+        } else {
+            return new ResponseEntity(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
+    @ApiOperation(value = "Get payment details by id", notes = "Get payment details for supplied payment id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved payment details"),
+            @ApiResponse(code = 404, message = "Payment not found"),
+            @ApiResponse(code = 500, message = "Internal server error", response = java.lang.String.class)
+    })
     @RequestMapping(value="/payments/{paymentId}", method=RequestMethod.GET)
-    public ResponseEntity<ViewPaymentResponse> viewPayment(@PathVariable("paymentId") String paymentId)  {
+    public ResponseEntity<ViewPaymentResponse> viewPayment(@ApiParam(value = "Payment id") @PathVariable("paymentId") String paymentId)  {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
         HttpHeaders headers = new HttpHeaders();
@@ -65,7 +85,11 @@ public class PaymentController {
         HttpEntity entity = new HttpEntity(headers);
         ResponseEntity<ViewPaymentResponse> response = restTemplate.exchange(url+"/"+paymentId, HttpMethod.GET ,entity, ViewPaymentResponse.class);
         logger.debug("Response : " + getJson(response));
-        return response;
+        if(HttpStatus.OK.equals(response.getStatusCode())) {
+            return response;
+        } else {
+            return new ResponseEntity(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     private String getJson(Object obj) {
