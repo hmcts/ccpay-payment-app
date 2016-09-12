dc4941f9aece2587b10d42e63670ad6f4d7281cc
zeeshan-alam <zeeshan_alam@yahoo.com>
2016-09-12 10:26:25 +0100

    Added swagger support

--- payment-api/src/main/java/uk/gov/justice/payment/api/PaymnetApiApplication.java
+++ payment-api/src/main/java/uk/gov/justice/payment/api/PaymnetApiApplication.java
@@ -5,12 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.io.File;
 import java.io.IOException;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class PaymnetApiApplication {
 
 	private static final Logger logger = LoggerFactory
@@ -23,4 +33,27 @@ public class PaymnetApiApplication {
 
         SpringApplication.run(PaymnetApiApplication.class, args);
 	}
+
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("payment")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/payment.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .contact("Niklas Heidloff")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+    }
 }
