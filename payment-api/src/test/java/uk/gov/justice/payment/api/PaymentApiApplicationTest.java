9457cbec4ee7dd4077f888e07847481b1d97a3df
zeeshan-alam <zeeshan_alam@yahoo.com>
2016-09-20 09:50:30 +0100

    Tests

--- payment-api/src/test/java/uk/gov/justice/payment/api/PaymentApiApplicationTest.java
+++ payment-api/src/test/java/uk/gov/justice/payment/api/PaymentApiApplicationTest.java
@@ -9,43 +9,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.justice.payment.api.json.CreatePaymentRequest;
-
 import static com.jayway.restassured.RestAssured.given;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Configuration
-
-
 public class PaymentApiApplicationTest {
 
-	@Test
-	public void createPayment() {
+    @Test
+    public void createPayment() {
+        RestAssured.port = 8181;
+        CreatePaymentRequest paymentRequest = new CreatePaymentRequest();
+        paymentRequest.setAmount(10);
+        paymentRequest.setDescription("TestDesc");
+        paymentRequest.setReference("TestRef");
+        paymentRequest.setReturnUrl("https://localhost:8443/payment-result");
+        given().contentType("application/json").
+                body(getJson(paymentRequest)).
+                when().post("/payments").
+                then().statusCode(201);
+    }
 
-
-		RestAssured.port = 8181;
-
-		CreatePaymentRequest paymentRequest = new CreatePaymentRequest();
-		paymentRequest.setAmount(10);
-		paymentRequest.setDescription("TestDesc");
-		paymentRequest.setReference("TestRef");
-		paymentRequest.setReturnUrl("https://localhost:8443/payment-result");
-
-
-		given().contentType("application/json").
-				body(getJson(paymentRequest)).
-				when().post("/payments").
-				then().statusCode(201);
-	}
-
-	private String getJson(Object obj) {
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			return mapper.writeValueAsString(obj);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
+    private String getJson(Object obj) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
