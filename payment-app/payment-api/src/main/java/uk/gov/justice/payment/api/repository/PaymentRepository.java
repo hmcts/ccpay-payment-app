42e6773b3e8c4a99e128f8ebfeff51e58c0140a4
Zeeshan Alam <zeeshan.alam@agilesphere.co.uk>
2016-10-13 16:34:42 +0100

    Added database support.

--- payment-app/payment-api/src/main/java/uk/gov/justice/payment/api/repository/PaymentRepository.java
+++ payment-app/payment-api/src/main/java/uk/gov/justice/payment/api/repository/PaymentRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import uk.gov.justice.payment.api.domain.PaymentDetails;
 
 
-public interface PaymentRepository extends CrudRepository <PaymentDetails, Integer>{
+public interface PaymentRepository extends CrudRepository <PaymentDetails, Integer> {
 
     <S extends PaymentDetails> S  save(S entity);
     Iterable<PaymentDetails> findAll();
 
     PaymentDetails findByPaymentId(String paymentId);
+
+    //Iterable<PaymentDetails> findAll(BooleanExpression criteria);
+
 }
