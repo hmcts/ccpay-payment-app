4fb2e7c02f249e222cc923a257f77d189b8cd114
Zeeshan Alam <zeeshan.alam@agilesphere.co.uk>
2016-10-13 16:50:26 +0100

    Removed old database module

--- payment-app/payment-api/src/main/db/liquibase/migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar liquibase.jar --classpath=postgresql-9.2-1002-jdbc4.jar --changeLogFile=payment_migrations.xml --username=payment --password=payment --url="jdbc:postgresql://localhost:5432/payment" --driver=org.postgresql.Driver --defaultSchemaName=payment --logLevel=info  update
