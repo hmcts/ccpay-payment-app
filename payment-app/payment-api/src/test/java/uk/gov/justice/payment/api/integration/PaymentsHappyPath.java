b4a0438ef3cf54438fb25f651f0e536ccfa72bec
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2016-11-10 12:52:37 +0000

    Simplified some testing code

--- payment-app/payment-api/src/test/java/uk/gov/justice/payment/api/integration/PaymentsHappyPath.java
+++ payment-app/payment-api/src/test/java/uk/gov/justice/payment/api/integration/PaymentsHappyPath.java
@@ -1,77 +1,56 @@
 package uk.gov.justice.payment.api.integration;
 
-import io.restassured.response.Response;
-
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.util.ResourceUtils.getFile;
 
-
-
-@RunWith(SpringRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PaymentsHappyPath extends TestBase {
     private static String payment_id = null;
 
     @Test
     public void test1_POST_Payment_Call() throws IOException {
-        Response response =
-                givenValidRequest()
-                        .body(loadFile("Messages_Appeal_Post.json"))
-                        .when()
-                        .post();
-
-        response
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        payment_id = response.path("payment_id");
+        payment_id =
+                scenario.given().body(getFile("classpath:Messages_Appeal_Post.json"))
+                        .when().post()
+                        .then().statusCode(CREATED.value())
+                        .extract().path("payment_id");
     }
 
     @Test
     public void test2_GET_Status() {
-        givenValidRequest()
-                .when()
-                .get(payment_id)
-                .then()
-                .statusCode(HttpStatus.OK.value());
+        scenario.given()
+                .when().get(payment_id)
+                .then().statusCode(OK.value());
 
     }
 
     @Test
     public void test3_GET_Header() {
-        givenValidRequest()
-                .when()
-                .get(payment_id)
-                .then()
-                .header("content-type", "application/json;charset=UTF-8");
+        scenario.given()
+                .when().get(payment_id)
+                .then().header("content-type", "application/json;charset=UTF-8");
     }
 
     @Test
     public void test4_POST_Cancel() {
-        givenValidRequest()
-                .when()
-                .post(payment_id + "/cancel")
-                .then()
-                .statusCode(HttpStatus.NO_CONTENT.value());
+        scenario.given()
+                .when().post(payment_id + "/cancel")
+                .then().statusCode(NO_CONTENT.value());
     }
 
     @Test
     public void test5_GET_Cancel_Status() {
-        givenValidRequest()
-                .when()
-                .get(payment_id)
+        scenario.given()
+                .when().get(payment_id)
                 .then()
                 .body("state.status", is("cancelled"))
                 .body("state.message", is("Payment was cancelled by the service"));
     }
-
-
 }
