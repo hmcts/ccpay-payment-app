098263e7f01e91f416bf6943e5ec26b7af35567a
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-05-05 10:20:24 +0100

    Use HttpComponents interceptors so they can be shared across applications

--- api/src/test/java/uk/gov/hmcts/payment/api/logging/OutboundRequestLoggingInterceptorTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/logging/OutboundRequestLoggingInterceptorTest.java
@@ -1,22 +1,23 @@
 package uk.gov.hmcts.payment.api.logging;
 
 import ch.qos.logback.classic.Logger;
+import java.net.URI;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.mock.http.client.MockClientHttpRequest;
-import org.springframework.mock.http.client.MockClientHttpResponse;
 
-import java.io.IOException;
-import java.net.URI;
-
-import static ch.qos.logback.classic.Level.*;
+import static ch.qos.logback.classic.Level.INFO;
 import static net.logstash.logback.argument.StructuredArguments.keyValue;
 import static org.slf4j.Logger.ROOT_LOGGER_NAME;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpStatus.OK;
 
 public class OutboundRequestLoggingInterceptorTest {
 
@@ -34,33 +35,15 @@ public class OutboundRequestLoggingInterceptorTest {
 
     @Test
     public void logsRequestAndResponseFields() throws Exception {
-        new OutboundRequestLoggingInterceptor(new FakeTicker(20)).intercept(
-                requestFor(GET, "http://www.google.com"), withBody("Request payload"),
-                (req, body) -> new MockClientHttpResponse(withBody("Response payload"), OK)
-        );
+        HttpContext context = new BasicHttpContext();
+
+        OutboundRequestLoggingInterceptor interceptor = new OutboundRequestLoggingInterceptor(new FakeTicker(20));
+
+        interceptor.process(new BasicHttpRequest("GET", "http://www.google.com"), context);
+        interceptor.process(new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("any", 0, 0), 200, "any")), context);
 
         testAppender.assertEvent(0, INFO, "Outbound request start", keyValue("method", "GET"), keyValue("uri", "http://www.google.com"));
-        testAppender.assertEvent(1, DEBUG, "Request payload");
-        testAppender.assertEvent(2, INFO, "Outbound request finish", keyValue("responseTime", 20L), keyValue("responseStatus", 200));
-        testAppender.assertEvent(3, DEBUG, "Response payload");
-    }
-
-    @Test
-    public void logsFailedRequest() throws Exception {
-        try {
-            new OutboundRequestLoggingInterceptor(new FakeTicker(20)).intercept(
-                    requestFor(GET, "http://www.google.com"), withBody("Request payload"),
-                    (req, body) -> {
-                        throw new IOException();
-                    }
-            );
-        } catch (IOException e) {
-            // expected
-        }
-
-        testAppender.assertEvent(0, INFO, "Outbound request start", keyValue("method", "GET"), keyValue("uri", "http://www.google.com"));
-        testAppender.assertEvent(1, DEBUG, "Request payload");
-        testAppender.assertEvent(2, ERROR, "Outbound request failed", keyValue("responseTime", 20L));
+        testAppender.assertEvent(1, INFO, "Outbound request finish", keyValue("responseTime", 20L), keyValue("responseStatus", 200));
     }
 
     private MockClientHttpRequest requestFor(HttpMethod httpMethod, String uri) {
@@ -68,9 +51,5 @@ public class OutboundRequestLoggingInterceptorTest {
         request.setMethod(httpMethod);
         request.setURI(URI.create(uri));
         return request;
-    }
-
-    private byte[] withBody(String bodyPayload) {
-        return bodyPayload.getBytes();
     }
 }
