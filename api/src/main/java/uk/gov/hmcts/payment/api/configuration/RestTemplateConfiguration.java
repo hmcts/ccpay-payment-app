a81385b71085fa1f281a6f2a25ff2a7bf5706821
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-06-21 13:14:28 +0100

    Use httpcomponents in the client

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
@@ -6,22 +6,11 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.logging.httpcomponents.OutboundRequestIdSettingInterceptor;
 import uk.gov.hmcts.reform.logging.httpcomponents.OutboundRequestLoggingInterceptor;
 
 @Configuration
 public class RestTemplateConfiguration {
-
-    @Bean
-    public RestTemplate restTemplate(CloseableHttpClient paymentsHttpClient) {
-        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(paymentsHttpClient));
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        return restTemplate;
-    }
-
     @Bean(name = {"paymentsHttpClient", "serviceTokenParserHttpClient", "userTokenParserHttpClient"})
     public CloseableHttpClient paymentsHttpClient() {
         return HttpClients.custom()
