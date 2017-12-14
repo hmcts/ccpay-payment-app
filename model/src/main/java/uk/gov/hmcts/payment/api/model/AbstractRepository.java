3c53ccf83907e775a88e8aa110fa0510e6afba2f
tpalisetty <tarun.palisetty@hmcts.net>
2017-12-14 10:52:08 +0000

    PAY-482 Payment reference data changes

---
+++ model/src/main/java/uk/gov/hmcts/payment/api/model/AbstractRepository.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.payment.api.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import uk.gov.hmcts.payment.api.model.exceptions.PaymentRefDataNotFoundException;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface AbstractRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
+    String getEntityName();
+
+    Optional<T> findByName(String name);
+
+    default T findByNameOrThrow(String name) {
+        return findByName(name).orElseThrow(() -> new PaymentRefDataNotFoundException(getEntityName() + " with " + name + " is not found"));
+    }
+
+}
---
+++ model/src/main/java/uk/gov/hmcts/payment/api/model/PaymentChannel.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.payment.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder(builderMethodName = "paymentChannelWith")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payment_channel")
+public class PaymentChannel {
+
+    @Id
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+}
