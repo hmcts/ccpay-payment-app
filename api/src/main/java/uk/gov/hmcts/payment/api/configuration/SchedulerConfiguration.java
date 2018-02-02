90950d98fc760fe7406128fbb5f8dd52cc4b41bd
sachi <sachi.kuppuswami@hmcts.net>
2018-02-02 12:40:07 +0000

    PAY-576 - Scheduler enabled to run conditionally on environment property

---
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/SchedulerConfiguration.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.payment.api.configuration;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+import org.springframework.scheduling.config.TaskManagementConfigUtils;
+
+@Configuration
+public class SchedulerConfiguration {
+
+    @Conditional(SchedulerCondition.class)
+    @Bean(name = TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public ScheduledAnnotationBeanPostProcessor scheduledAnnotationProcessor() {
+        return new ScheduledAnnotationBeanPostProcessor();
+    }
+}
