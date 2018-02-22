7ebafbafc501226f6a148b930192a3e8fe0e03f9
tarun-palisetty <tarun.mca@gmail.com>
2018-02-22 13:54:39 +0000

    Unit tests added for CreditAccountPaymentService (#78)
    * Mock test added for CreditAccountPaymentService
    
    * Mock test added for CreditAccountPaymentService
    
    * Mock test added for CreditAccountPaymentService

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/CardPaymentServiceTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/CardPaymentServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.payment.api.componenttests.util.PaymentsDataUtil;
 import uk.gov.hmcts.payment.api.model.*;
 import uk.gov.hmcts.payment.api.v1.componenttests.ComponentTestBase;
 
@@ -26,21 +27,24 @@ import static uk.gov.hmcts.payment.api.model.Fee.feeWith;
 import static uk.gov.hmcts.payment.api.model.Payment.paymentWith;
 
 public class CardPaymentServiceTest extends ComponentTestBase {
+    private PaymentsDataUtil paymentsDataUtil;
+
 
     @Before
     public void setUp() {
+        paymentsDataUtil = new PaymentsDataUtil();
 
         PaymentFeeLink paymentFeeLink = PaymentFeeLink.paymentFeeLinkWith()
-            .payments(getCardPaymentsData())
-            .fees(getFeesData())
+            .payments(paymentsDataUtil.getCardPaymentsData())
+            .fees(paymentsDataUtil.getFeesData())
             .build();
 
         paymentFeeLinkRepository.save(paymentFeeLink);
 
         //pba payments
         paymentFeeLinkRepository.save(PaymentFeeLink.paymentFeeLinkWith()
-            .payments(getCreditAccountPaymentsData())
-            .fees(getFeesData())
+            .payments(paymentsDataUtil.getCreditAccountPaymentsData())
+            .fees(paymentsDataUtil.getFeesData())
             .build());
     }
 
@@ -63,52 +67,6 @@ public class CardPaymentServiceTest extends ComponentTestBase {
             });
         });
 
-    }
-
-    private List<Payment> getCardPaymentsData() {
-        List<Payment> payments = new ArrayList<>();
-        payments.add(paymentWith().amount(BigDecimal.valueOf(10000).movePointRight(2)).reference("reference1").description("desc1")
-            .returnUrl("returnUrl1")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo1").caseReference("caseRef1").serviceType("cmc").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(20000).movePointRight(2)).reference("reference2").description("desc2")
-            .returnUrl("returnUrl2")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo2").caseReference("caseRef2").serviceType("divorce").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(30000).movePointRight(2)).reference("reference3").description("desc3")
-            .returnUrl("returnUrl3")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo3").caseReference("caseRef3").serviceType("probate").currency("GBP").build());
-
-        return payments;
-    }
-
-    private List<Payment> getCreditAccountPaymentsData() {
-        List<Payment> payments = new ArrayList<>();
-        payments.add(paymentWith().amount(BigDecimal.valueOf(10000).movePointRight(2)).reference("reference1").description("desc1")
-            .returnUrl("returnUrl1")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo1").caseReference("caseRef1").serviceType("cmc").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(20000).movePointRight(2)).reference("reference2").description("desc2")
-            .returnUrl("returnUrl2")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo2").caseReference("caseRef2").serviceType("divorce").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(30000).movePointRight(2)).reference("reference3").description("desc3")
-            .returnUrl("returnUrl3")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo3").caseReference("caseRef3").serviceType("probate").currency("GBP").build());
-
-        return payments;
-    }
-
-    private List<Fee> getFeesData() {
-        List<Fee> fees = new ArrayList<>();
-        fees.add(feeWith().code("X0011").version("1").build());
-        fees.add(feeWith().code("X0022").version("2").build());
-        fees.add(feeWith().code("X0033").version("3").build());
-        fees.add(feeWith().code("X0044").version("4").build());
-
-        return fees;
     }
 
 }
--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/CreditAccountPaymentServiceTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/CreditAccountPaymentServiceTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.payment.api.componenttests;
 import org.joda.time.MutableDateTime;
 import org.junit.Before;
 import org.junit.Test;
+import uk.gov.hmcts.payment.api.componenttests.util.PaymentsDataUtil;
 import uk.gov.hmcts.payment.api.model.*;
 import uk.gov.hmcts.payment.api.v1.componenttests.ComponentTestBase;
 
@@ -17,21 +18,23 @@ import static uk.gov.hmcts.payment.api.model.Fee.feeWith;
 import static uk.gov.hmcts.payment.api.model.Payment.paymentWith;
 
 public class CreditAccountPaymentServiceTest extends ComponentTestBase{
+    private PaymentsDataUtil paymentsDataUtil;
 
     @Before
     public void setUp() {
+        paymentsDataUtil = new PaymentsDataUtil();
 
         PaymentFeeLink paymentFeeLink = PaymentFeeLink.paymentFeeLinkWith()
-            .payments(getCreditAccountPaymentsData())
-            .fees(getFeesData())
+            .payments(paymentsDataUtil.getCreditAccountPaymentsData())
+            .fees(paymentsDataUtil.getFeesData())
             .build();
 
         paymentFeeLinkRepository.save(paymentFeeLink);
 
         //card payments
         paymentFeeLinkRepository.save(PaymentFeeLink.paymentFeeLinkWith()
-            .payments(getCardPaymentsData())
-            .fees(getFeesData()).build());
+            .payments(paymentsDataUtil.getCardPaymentsData())
+            .fees(paymentsDataUtil.getFeesData()).build());
     }
 
     @Test
@@ -55,49 +58,5 @@ public class CreditAccountPaymentServiceTest extends ComponentTestBase{
 
     }
 
-    private List<Payment> getCreditAccountPaymentsData() {
-        List<Payment> payments = new ArrayList<>();
-        payments.add(paymentWith().amount(BigDecimal.valueOf(10000).movePointRight(2)).reference("reference1").description("desc1")
-            .returnUrl("returnUrl1")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo1").caseReference("caseRef1").serviceType("cmc").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(20000).movePointRight(2)).reference("reference2").description("desc2")
-            .returnUrl("returnUrl2")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo2").caseReference("caseRef2").serviceType("divorce").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(30000).movePointRight(2)).reference("reference3").description("desc3")
-            .returnUrl("returnUrl3")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("payment by account").build())
-            .ccdCaseNumber("ccdCaseNo3").caseReference("caseRef3").serviceType("probate").currency("GBP").build());
 
-        return payments;
-    }
-
-    private List<Payment> getCardPaymentsData() {
-        List<Payment> payments = new ArrayList<>();
-        payments.add(paymentWith().amount(BigDecimal.valueOf(10000).movePointRight(2)).reference("reference1").description("desc1")
-            .returnUrl("returnUrl1")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo1").caseReference("caseRef1").serviceType("cmc").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(20000).movePointRight(2)).reference("reference2").description("desc2")
-            .returnUrl("returnUrl2")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo2").caseReference("caseRef2").serviceType("divorce").currency("GBP").build());
-        payments.add(paymentWith().amount(BigDecimal.valueOf(30000).movePointRight(2)).reference("reference3").description("desc3")
-            .returnUrl("returnUrl3")
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .ccdCaseNumber("ccdCaseNo3").caseReference("caseRef3").serviceType("probate").currency("GBP").build());
-
-        return payments;
-    }
-
-    private List<Fee> getFeesData() {
-        List<Fee> fees = new ArrayList<>();
-        fees.add(feeWith().code("X0011").version("1").build());
-        fees.add(feeWith().code("X0022").version("2").build());
-        fees.add(feeWith().code("X0033").version("3").build());
-        fees.add(feeWith().code("X0044").version("4").build());
-
-        return fees;
-    }
 }
