3c4b041c11a9617d24af0196de998db097343560
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2018-03-28 10:40:51 +0100

    PAY-697 - CNP config for payment app. (#123)
    * PAY-697 - CNP config for payment app.
    
    * PAY-697 - updated email-to config with vault

---
+++ infrastructure/variables.tf
@@ -0,0 +1,93 @@
+variable "product" {
+  type    = "string"
+  default = "payment"
+}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+variable "subscription" {
+  type = "string"
+}
+
+variable "ilbIp"{}
+
+variable "tenant_id" {}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "microservice" {
+  type = "string"
+  default = "payment-app"
+}
+
+variable "vault_section" {
+  default = "test"
+}
+
+variable "idam_api_url" {
+  default = "https://idam-test.dev.ccidam.reform.hmcts.net"
+}
+
+variable "s2s_url" {
+  default = "http://betaDevBccidamS2SLB.reform.hmcts.net"
+}
+
+variable "gov_pay_url" {
+  default = "https://publicapi.payments.service.gov.uk/v1/payments"
+}
+
+variable "spring_mail_host" {
+  default = "mta.reform.hmcts.net"
+}
+variable "spring_mail_port" {
+  default = "25"
+}
+variable "spring_mail_properties_mail_smtp_starttls_enable" {
+  default = "true"
+}
+variable "spring_mail_properties_email_smtp_ssl_trust" {
+  default = "*"
+}
+variable "card_payments_report_schedule" {
+  default = "0 */30 * * * ?"
+}
+variable "card_payments_report_scheduler_enabled" {
+  default = "false"
+}
+variable "card_payments_email_from" {
+  default = "no-reply@reform.hmcts.net"
+}
+variable "card_payments_email_subject" {
+  default = "Test Env: Card Payments Reconciliation Report"
+}
+variable "card_payments_email_message" {
+  default = "Hi <br/><br/>Please find attached today''s reconciliation report. <br/><br/>Regards <br/><br/>Payments team<br/><br/>"
+}
+variable "pba_payments_report_schedule" {
+  default = "0 */30 * * * ?"
+}
+variable "pba_payments_report_scheduler_enabled" {
+  default = "true"
+}
+variable "pba_payments_email_from" {
+  default = "no-reply@reform.hmcts.net"
+}
+variable "pba_payments_email_subject" {
+  default = "Test Env : PBA Reconciliation Report'"
+}
+variable "pba_payments_email_message" {
+  default = "Hi <br/><br/>Please find attached today''s Payment by Account reconciliation report. <br/><br/>Regards <br/><br/>Payments team<br/><br/>"
+}
+
+variable "fees_register_url" {
+  default = "https://test.fees-register.reform.hmcts.net:4431"
+}
