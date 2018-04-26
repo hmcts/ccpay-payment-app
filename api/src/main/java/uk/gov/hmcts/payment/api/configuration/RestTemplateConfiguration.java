84c7d941d3606f733024147cb8cab5571b52e33c
Sateesh Mathangi <a-smathangi@hotels.com>
2018-04-26 16:15:37 +0100

    Spring boot upgrade final commit

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
@@ -1,13 +1,9 @@
 package uk.gov.hmcts.payment.api.configuration;
 
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.hmcts.reform.logging.httpcomponents.OutboundRequestIdSettingInterceptor;
-import uk.gov.hmcts.reform.logging.httpcomponents.OutboundRequestLoggingInterceptor;
 
 @Configuration
 public class RestTemplateConfiguration {
@@ -15,9 +11,6 @@ public class RestTemplateConfiguration {
     public CloseableHttpClient paymentsHttpClient() {
         return HttpClients.custom()
             .useSystemProperties()
-            .addInterceptorFirst(new OutboundRequestIdSettingInterceptor())
-            .addInterceptorFirst((HttpRequestInterceptor) new OutboundRequestLoggingInterceptor())
-            .addInterceptorLast((HttpResponseInterceptor) new OutboundRequestLoggingInterceptor())
             .build();
     }
 }
