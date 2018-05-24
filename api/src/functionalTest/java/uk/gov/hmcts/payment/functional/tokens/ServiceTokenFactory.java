ca1744f7ae31025262712f8cca7f9c4212159be9
mswirski <mateusz.swirski@hmcts.net>
2018-05-24 15:16:46 +0100

    functional tests migrated from another git project

---
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/tokens/ServiceTokenFactory.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.payment.functional.tokens;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestComponent;
+
+import static io.restassured.RestAssured.post;
+
+@TestComponent
+public class ServiceTokenFactory {
+
+    private final String baseUrl;
+
+    @Autowired
+    public ServiceTokenFactory(@Value("${base-urls.service-auth-provider}") String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String validTokenForService(String microservice) {
+        return "Bearer " + post(baseUrl + "/testing-support/lease?microservice={microservice}", microservice).body().asString();
+    }
+}
