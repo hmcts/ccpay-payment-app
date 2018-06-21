fb0acd7ed6d7ba6a77e557b9384feadcb7e52005
Sateesh Mathangi <a-smathangi@hotels.com>
2018-06-21 10:24:31 +0100

    env access fix

--- App_Data/jobs/triggered/csv-report/run.sh
+++ App_Data/jobs/triggered/csv-report/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -X POST http://${WEBSITE_HOSTNAME}/payments/email-pay-reports
+curl -X POST http://$WEBSITE_HOSTNAME/payments/email-pay-reports
 
 echo "finished generating csv reports"
