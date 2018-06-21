a6637d3296494b84a7336c8d8cb2546af4734ad1
Sateesh Mathangi <a-smathangi@hotels.com>
2018-06-21 14:32:54 +0100

    handling duplicate fee codes

--- api/src/main/java/uk/gov/hmcts/payment/api/reports/FeesRegisterClient.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/reports/FeesRegisterClient.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.payment.api.reports;
 
+import org.slf4j.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.hmcts.fees2.register.api.contract.Fee2Dto;
@@ -9,13 +10,25 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @FeignClient(name = "fees-register-client", url = "${fees.register.url}")
 public interface FeesRegisterClient {
+    Logger LOG = getLogger(FeesRegisterClient.class);
+
     @GetMapping(value = "/fees-register/fees")
     List<Fee2Dto> getFeesData();
 
     default Optional<Map<String,Fee2Dto>> getFeesDataAsMap() {
-        return Optional.ofNullable(getFeesData().stream().collect(Collectors.toMap(fee->fee.getCode(),fee ->fee)));
+        return Optional.ofNullable(getFeesData().stream()
+            .collect(Collectors.toMap(
+                Fee2Dto::getCode,
+                fee ->fee,
+                (fee1, fee2) -> {
+                    LOG.warn("duplicate feeCode key found :{}", fee2);
+                    return fee1;
+                }
+            )));
         }
 
 }
