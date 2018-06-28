bcf6b9505351a57246e5a3a4c4922b1f00b9d0b3
Sateesh Mathangi <a-smathangi@hotels.com>
2018-06-28 20:42:29 +0100

    vault resource group fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,7 +16,7 @@ locals {
 
 data "azurerm_key_vault" "payment_key_vault" {
   name = "${local.vaultName}"
-  resource_group_name = "${var.product}-${var.env}"
+  resource_group_name = "payment-${local.local_env}"
 }
 
 data "azurerm_key_vault_secret" "gov_pay_keys_reference" {
