d30a3bc81d3c04de6c45739a8db3f0ba145334b4
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2018-07-09 15:48:27 +0100

    report header fix (#211)

--- api/src/main/java/uk/gov/hmcts/payment/api/reports/PaymentsReportService.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/reports/PaymentsReportService.java
@@ -56,7 +56,7 @@ public class PaymentsReportService {
     private static final String CARD_PAYMENTS_HEADER = "Service,Payment Group reference,Payment reference," +
         "CCD reference,Case reference,Payment created date,Payment status updated date,Payment status," +
         "Payment channel,Payment method,Payment amount,Site id,Fee code,Version,Calculated amount,Memo line,NAC," +
-        "PaymentFee volume";
+        "Fee volume";
 
     private static final String CREDIT_ACCOUNT_PAYMENTS_HEADER = "Service,Payment Group reference,Payment reference," +
         "CCD reference,Case reference,Organisation name,Customer internal reference,PBA Number,Payment created date," +
