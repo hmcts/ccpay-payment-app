583087b5e15befd0697c674b68296baae7d7df49
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2018-07-19 12:34:30 +0100

    shell script fix (#222)

--- App_Data/jobs/triggered/cad-csv-report/run.sh
+++ App_Data/jobs/triggered/cad-csv-report/run.sh
@@ -2,7 +2,7 @@
 
 if [ "$SLOT" == "PRODUCTION" ]
 then
-    var URL = $PAYMENT_SERVER_URL/jobs/email-pay-reports?payment_method=CARD
+    URL=$PAYMENT_SERVER_URL/jobs/email-pay-reports?payment_method=CARD
     printf "Invoking email-pay-reports on :%s \n" $URL
     curl -X POST $URL -d {}
     printf "\nFinished emailing CARD csv report"
