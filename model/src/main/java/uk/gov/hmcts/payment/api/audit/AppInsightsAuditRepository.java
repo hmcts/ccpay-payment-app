b9c74248a896c14b6169054797852a9c46717160
Sateesh Mathangi <a-smathangi@hotels.com>
2018-07-27 09:17:08 +0100

    tracking custom audit events in azure appInsights

--- model/src/main/java/uk/gov/hmcts/payment/api/audit/AppInsightsAuditRepository.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/audit/AppInsightsAuditRepository.java
@@ -1,17 +1,32 @@
 package uk.gov.hmcts.payment.api.audit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.TelemetryConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.payment.api.model.Payment;
+import uk.gov.hmcts.payment.api.model.PaymentFee;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class AppInsightsAuditRepository implements AuditRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AppInsightsAuditRepository.class);
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     private final TelemetryClient telemetry;
 
@@ -29,13 +44,44 @@ public class AppInsightsAuditRepository implements AuditRepository {
     }
 
     @Override
-    public void trackPaymentEvent(String name, Payment payment) {
+    public void trackPaymentEvent(String name, Payment payment, List<PaymentFee> fees) {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
             .put("paymentReference", payment.getReference())
             .put("amount", payment.getAmount().toString())
             .put("serviceType", payment.getServiceType())
             .put("status", payment.getStatus())
+            .put("fees", toFeeDtoJson(fees))
             .build();
         telemetry.trackEvent(name, properties,null);
+    }
+
+    private String toFeeDtoJson(List<PaymentFee> fees) {
+        List<FeeDto> feeDtos = fees.stream()
+            .map(a -> toFeeDto(a))
+            .collect(Collectors.toList());
+        try {
+            return mapper.writeValueAsString(feeDtos);
+        } catch (JsonProcessingException ex) {
+            LOG.error("Error Json processing feeDtos:{} with message:{}", feeDtos, ex.getMessage());
+            return null;
+        }
+    }
+
+    private FeeDto toFeeDto(PaymentFee paymentFee) {
+        return new FeeDto.FeeDtoBuilder()
+            .code(paymentFee.getCode())
+            .version(paymentFee.getVersion())
+            .volume(paymentFee.getVolume())
+            .build();
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    private static final class FeeDto {
+        private String code;
+        private String version;
+        private Integer volume;
     }
 }
--- model/src/main/java/uk/gov/hmcts/payment/api/audit/AuditRepository.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/audit/AuditRepository.java
@@ -2,13 +2,15 @@ package uk.gov.hmcts.payment.api.audit;
 
 import org.springframework.scheduling.annotation.Async;
 import uk.gov.hmcts.payment.api.model.Payment;
+import uk.gov.hmcts.payment.api.model.PaymentFee;
 
+import java.util.List;
 import java.util.Map;
 
 @Async
 public interface AuditRepository {
 
-    void trackPaymentEvent(String name, Payment payment);
+    void trackPaymentEvent(String name, Payment payment, List<PaymentFee> fees);
 
     void trackEvent(String name, Map<String, String> properties);
 }
