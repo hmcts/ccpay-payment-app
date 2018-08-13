03da5bf27cfac2599690388065628849c5030d2f
Sateesh Mathangi <a-smathangi@hotels.com>
2018-08-13 16:27:54 +0100

    removed tactical vault reference.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,3 @@
-provider "vault" {
-  // # tactical vault - for example: use `data "vault_generic_secret" "s2s_secret" {`
-  address = "https://vault.reform.hmcts.net:6200"
-}
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
