3a1e0a38e121bc4b73d102181112dcfb998ffb1f
Mateusz Swirski <CrazyFunker@users.noreply.github.com>
2018-08-30 14:25:32 +0100

    Pay 887 2 (#258)
    * WIP
    
    * some working tests locally
    
    * refactoring
    
    * WIP
    
    * refactored tests, removed old functional tests, updated Payment model by removing external_provider
    
    * update
    
    * update

---
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/IntegrationTestBase.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.payment.functional;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestContextConfiguration.class)
+public class IntegrationTestBase {
+
+    @Value("${probate.user.id}")
+    protected String probateUserId;
+
+    @Value("${probate.user.role}")
+    protected String probateUserRole;
+
+    @Value("${probate.service.name}")
+    protected String probateServiceName;
+
+    @Value("${probate.service.secret}")
+    protected String probateSecret;
+}
---
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/tokens/OneTimePasswordFactory.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.payment.functional.tokens;
+
+import org.jboss.aerogear.security.otp.Totp;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
+public class OneTimePasswordFactory {
+    private Totp totp;
+
+    public String validOneTimePassword(String secret) {
+        totp = new Totp(secret);
+        return totp.now();
+    }
+}
