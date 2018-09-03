4233096d67e82a4741a5bc331120b44d37ba9209
tarun-palisetty <tarun.mca@gmail.com>
2018-09-03 10:30:24 +0100

    Functional test with SIDAM integration

---
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/BuildInfoIntegrationTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.payment.functional;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.payment.functional.dsl.PaymentsV2TestDsl;
+import java.io.IOException;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BuildInfoIntegrationTest extends IntegrationTestBase {
+
+    @Autowired
+    private PaymentsV2TestDsl scenario;
+
+    @Test
+    public void buildInfoShouldBePresent() throws IOException, NoSuchFieldException {
+        scenario.given()
+            .when().getBuildInfo()
+            .then().got(JsonNode.class, response -> {
+            assertThat(response.at("/git/commit/id").asText()).isNotEmpty();
+            assertThat(response.at("/build/version").asText()).isNotEmpty();
+        });
+    }
+}
