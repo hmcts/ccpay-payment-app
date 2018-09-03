f49af9a78b118f8831cbdfb4f441bb1f6605157b
tarun-palisetty <tarun.mca@gmail.com>
2018-09-03 11:20:04 +0100

    Added payments api url to properties file

--- api/src/functionalTest/java/uk/gove/hmcts/payment/functional/FunctionalTest.java
+++ api/src/functionalTest/java/uk/gove/hmcts/payment/functional/FunctionalTest.java
@@ -13,7 +13,7 @@ import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 @RunWith(SpringRunner.class)
 public class FunctionalTest {
 
-    @Value("${TEST_URL:http://localhost:8080}")
+    @Value("${payment.api.url:http://localhost:8080}")
     private String testUrl;
 
     @Before
