0006db4828c329e3e6d60b1011a17f22858bcef2
tarun-palisetty <tarun.mca@gmail.com>
2018-09-06 13:07:31 +0100

    Refactored functional tests

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CreateCardPaymentIntegrationTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CreateCardPaymentIntegrationTest.java
@@ -1,32 +1,14 @@
 package uk.gov.hmcts.payment.functional;
 
-import io.restassured.RestAssured;
-import io.restassured.parsing.Parser;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import org.apache.commons.lang.RandomStringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import uk.gov.hmcts.payment.api.contract.CardPaymentRequest;
-import uk.gov.hmcts.payment.api.contract.FeeDto;
-import uk.gov.hmcts.payment.api.contract.util.CurrencyCode;
-import uk.gov.hmcts.payment.api.contract.util.Service;
+import uk.gov.hmcts.payment.api.contract.PaymentDto;
 import uk.gov.hmcts.payment.functional.dsl.PaymentsV2TestDsl;
-import uk.gov.hmcts.payment.functional.tokens.ServiceTokenFactory;
-import uk.gov.hmcts.payment.functional.tokens.UserTokenFactory;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
-import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
 public class CreateCardPaymentIntegrationTest extends IntegrationTestBase {
@@ -34,79 +16,63 @@ public class CreateCardPaymentIntegrationTest extends IntegrationTestBase {
     @Autowired(required = true)
     private PaymentsV2TestDsl dsl;
 
-    @Value("${test.url:http://localhost:8080}")
-    private String baseURL;
+    @Test
+    public void createCMCCardPaymentShouldReturn201() {
+        String userId = dsl.generateUserId();
+        String password = dsl.generatePassword();
 
-    @Autowired
-    private UserTokenFactory userTokenFactory;
+        dsl.given().createUser(userId, password, cmcUserRole, cmcUserGroup).userId(userId, password).serviceId(cmcServiceName, cmcSecret).returnUrl("https://www.google.com")
+            .when().createCardPayment(getCardPaymentRequest())
+            .then().created(paymentDto -> {
+                assertNotNull(paymentDto.getReference());
+                assertEquals("payment status is properly set", "Initiated", paymentDto.getStatus());
+        });
 
-    @Autowired
-    private ServiceTokenFactory serviceTokenFactory;
-
-    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    private static String cmcUserId = UUID.randomUUID().toString() + "@hmcts.net";
-
-    private static String cmcUserPassword = RandomStringUtils.random(15, characters);
-
+        dsl.given().deleteUser(userId);
+    }
 
     @Test
-    public void createCMCCardPaymentShoudReturn201() {
-        proxy(localProxyHost, Integer.parseInt(localProxyPort));
+    public void retrieveCMCCardPaymentShouldReturn200() {
+        String userId = dsl.generateUserId();
+        String password = dsl.generatePassword();
+        final String[] reference = new String[1];
 
-        String userAuthToken = userTokenFactory.validTokenForUser(cmcUserId, cmcUserPassword, cmcUserRole, cmcUserGroup);
-        String serviceAuthToken = serviceTokenFactory.validTokenForService(cmcServiceName, cmcSecret);
+        // create card payment
+        dsl.given().createUser(userId, password, cmcUserRole, cmcUserGroup).userId(userId, password).serviceId(cmcServiceName, cmcSecret).returnUrl("https://www.google.com")
+            .when().createCardPayment(getCardPaymentRequest())
+            .then().created(savedPayment -> {
+                reference[0] = savedPayment.getReference();
 
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", userAuthToken);
-        headers.put("ServiceAuthorization", serviceAuthToken);
-        headers.put("return-url", "https://www.google.com");
-
-        String reference = given()
-            .relaxedHTTPSValidation()
-            .headers(headers)
-            .header("Content-Type", "application/json")
-            .body(getCardPaymentRequest())
-            .when()
-            .baseUri(baseURL)
-            .post("/card-payments")
-            .then()
-            .statusCode(201)
-            .extract()
-            .path("reference");
-
-        assertNotNull(reference);
+                assertNotNull(savedPayment.getReference());
+                assertEquals("payment status is properly set", "Initiated", savedPayment.getStatus());
+        });
 
 
-//        CardPaymentRequest cardPaymentRequest = CardPaymentRequest.createCardPaymentRequestDtoWith()
-//            .amount(new BigDecimal("123.11"))
-//            .description("A functional test card payment")
-//            .caseReference("REF_123")
-//            .service(Service.CMC)
-//            .currency(CurrencyCode.GBP)
-//            .siteId("SITEID_123")
-//            .fees(Arrays.asList(FeeDto.feeDtoWith()
-//                .calculatedAmount(new BigDecimal("123.11"))
-//                .code("FEE0123")
-//                .reference("REF_123")
-//                .version("1")
-//                .volume(new Double("1"))
-//                .build()))
-//            .build();
-//
-//
-//        dsl.given().userId(cmcUserId, cmcUserPassword, cmcUserRole, cmcUserGroup).serviceId(cmcServiceName, cmcSecret).returnUrl("https://www.google.com")
-//            .when().createCardPayment(cardPaymentRequest)
-//            .then().created(paymentDto -> {
-//                Assert.assertEquals("payment status is properly set", "Initiated", paymentDto.getStatus());
-//        });
+        // retrieve card payment
+        PaymentDto paymentDto = dsl.given().userId(userId, password).serviceId(cmcServiceName, cmcSecret)
+            .when().getCardPayment(reference[0])
+            .then().get();
+
+        assertNotNull(paymentDto);
+        assertEquals(paymentDto.getAmount(), new BigDecimal("123.11"));
+        assertEquals(paymentDto.getDescription(), "A functional test card payment");
+        assertEquals(paymentDto.getReference(), reference[0]);
+        assertEquals(paymentDto.getExternalProvider(), "gov pay");
+        assertEquals(paymentDto.getServiceName(), "Civil Money Claims");
+        assertEquals(paymentDto.getStatus(), "Initiated");
+        paymentDto.getFees().stream().forEach(f -> {
+            assertEquals(f.getCode(), "FEE0123");
+            assertEquals(f.getVersion(), "1");
+            assertEquals(f.getCalculatedAmount(), new BigDecimal("123.11"));
+        });
+
+        dsl.given().deleteUser(userId);
     }
 
     private String getCardPaymentRequest() {
         JSONObject payment = new JSONObject();
+
         try {
-
-
             payment.put("amount", 123.11);
             payment.put("description", "A functional test card payment");
             payment.put("case_reference", "REF_123");
