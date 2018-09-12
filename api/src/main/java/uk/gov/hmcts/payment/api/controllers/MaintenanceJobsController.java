ae3cc8bf9d65f7a8a743a7690867a0d6321cd84a
tarun-palisetty <tarun.mca@gmail.com>
2018-09-12 16:56:47 +0100

    Search payments using by date time

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/MaintenanceJobsController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/MaintenanceJobsController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class MaintenanceJobsController {
 
     @Autowired
     public MaintenanceJobsController(PaymentService<PaymentFeeLink, String> paymentService,
-                                     CardPaymentService<PaymentFeeLink, String> cardPaymentService) {
+                                     @Qualifier("loggingCardPaymentService") CardPaymentService<PaymentFeeLink, String> cardPaymentService) {
         this.paymentService = paymentService;
         this.cardPaymentService = cardPaymentService;
     }
---
+++ model/src/main/java/uk/gov/hmcts/payment/api/util/DateUtil.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.payment.api.util;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DateUtil {
+
+    DateTimeParser[] ISO_DATE_TIME_PARSERS = {
+        DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").getParser(),
+        DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss").getParser(),
+        DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
+    };
+
+    public DateTimeFormatter getIsoDateTimeFormatter() {
+        return new DateTimeFormatterBuilder().append(null, ISO_DATE_TIME_PARSERS).toFormatter();
+    }
+
+}
