d21051a3e90056edf8f063bf9872329761e03688
mswirski <mateusz.swirski@hmcts.net>
2018-09-19 09:17:42 +0100

    Added error logging for general application crash

--- api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,8 +11,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @SpringBootApplication
 public class PaymentApiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(PaymentApiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApiApplication.class, args);
+        try {
+            SpringApplication.run(PaymentApiApplication.class, args);
+        } catch (Exception ex) {
+            LOG.error("Application crashed with error message: ", ex);
+        }
     }
 }
