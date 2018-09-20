ce3bf8d56b14514526ed9bb788970445e0985c88
tarun-palisetty <tarun.mca@gmail.com>
2018-09-20 11:48:17 +0100

    Refactored integrationTestBase

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/BuildInfoIntegrationTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/BuildInfoIntegrationTest.java
@@ -2,12 +2,17 @@ package uk.gov.hmcts.payment.functional;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.payment.functional.dsl.PaymentsTestDsl;
 import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BuildInfoIntegrationTest extends  IntegrationTestBase {
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestContextConfiguration.class)
+public class BuildInfoIntegrationTest {
 
     @Autowired
     private PaymentsTestDsl scenario;
--- api/src/functionalTest/java/uk/gove/hmcts/payment/functional/FunctionalTest.java
+++ api/src/functionalTest/java/uk/gove/hmcts/payment/functional/FunctionalTest.java
@@ -3,13 +3,19 @@ package uk.gove.hmcts.payment.functional;
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.payment.functional.IntegrationTestBase;
+import uk.gov.hmcts.payment.functional.TestContextConfiguration;
 
 import static io.restassured.RestAssured.given;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
-public class FunctionalTest extends IntegrationTestBase {
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestContextConfiguration.class)
+public class FunctionalTest {
 
     @Value("${test.url:http://localhost:8080}")
     private String testUrl;
