59687a7e4b4ba96609a39d883dd8aa00b31d465b
mswirski <mateusz.swirski@hmcts.net>
2018-09-24 10:31:40 +0100

    implemented context closed event listener

--- api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
