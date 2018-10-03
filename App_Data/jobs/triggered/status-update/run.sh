888cf549618852dc8d37e9d136f9e38cc13e8bab
Sateesh Mathangi <a-smathangi@hotels.com>
2018-10-03 12:10:40 +0100

    PAY-1098 - added security to other jobs

--- App_Data/jobs/triggered/status-update/run.sh
+++ App_Data/jobs/triggered/status-update/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+source ../s2sToken.sh
 
+AUTH_TOKEN=$(s2sToken)
 printf "Invoke status-update endpoint on :%s\n" $PAYMENT_SERVER_URL
 
-curl -X PATCH $PAYMENT_SERVER_URL/jobs/card-payments-status-update -d {}
+curl -X PATCH $PAYMENT_SERVER_URL/jobs/card-payments-status-update -H "ServiceAuthorization: Bearer $AUTH_TOKEN" -d {}
 
 printf "\nFinished updating status"
