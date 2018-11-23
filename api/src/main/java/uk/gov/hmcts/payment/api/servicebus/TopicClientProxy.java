c5b54facc48685bf08310787f3f8ee874a336bce
emagdalena <eduardo.magdalena@hmtcs.net>
2018-11-23 13:19:35 +0000

    Bug fix for timeout on topic client

--- api/src/main/java/uk/gov/hmcts/payment/api/servicebus/TopicClientProxy.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/servicebus/TopicClientProxy.java
@@ -2,26 +2,28 @@ package uk.gov.hmcts.payment.api.servicebus;
 
 import com.microsoft.azure.servicebus.IMessage;
 import com.microsoft.azure.servicebus.TopicClient;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
 
 @Component
 public class TopicClientProxy {
 
-    @Autowired
-    private WebApplicationContext applicationContext;
+    @Value("${azure.servicebus.connection-string}")
+    private String connectionString;
+
+    @Value("${azure.servicebus.topic-name}")
+    private String topic;
 
     public void send(IMessage message) throws InterruptedException, ServiceBusException {
 
+        ConnectionStringBuilder connectionStringBuilder = new ConnectionStringBuilder(connectionString, topic);
 
-        TopicClient topicClient = applicationContext.getBean(TopicClient.class);
+        TopicClient topicClient = new TopicClient(connectionStringBuilder);
+
         topicClient.send(message);
         topicClient.close();
-    }
-
-    public void close() throws ServiceBusException {
     }
 
 }
