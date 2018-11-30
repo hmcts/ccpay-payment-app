2def921afe70bbad4db7708e2767d226b1e3c31c
emagdalena <eduardo.magdalena@hmtcs.net>
2018-11-30 14:34:25 +0000

    Function Index and some extra tests for servicebus

---
+++ api/src/test/java/uk/gov/hmcts/payment/api/unit/SendMessageApplication.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.payment.api.unit;
+
+import com.microsoft.azure.servicebus.Message;
+import org.codehaus.jackson.map.ObjectMapper;
+import uk.gov.hmcts.payment.api.componenttests.CardPaymentComponentTest;
+import uk.gov.hmcts.payment.api.componenttests.util.PaymentsDataUtil;
+import uk.gov.hmcts.payment.api.contract.PaymentDto;
+import uk.gov.hmcts.payment.api.dto.mapper.PaymentDtoMapper;
+import uk.gov.hmcts.payment.api.model.Payment;
+import uk.gov.hmcts.payment.api.model.PaymentFeeLink;
+import uk.gov.hmcts.payment.api.servicebus.TopicClientProxy;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SendMessageApplication {
+
+
+    public static void main(String[] args) throws Exception{
+
+        TopicClientProxy client = new TopicClientProxy("Endpoint=sb://ccpay-servicebus-aat.servicebus.windows.net/;SharedAccessKeyName=SendAndListenSharedAccessKey;SharedAccessKey=ATfNGbCzML84hjtH8T0EoAyI1PyLvxMQ/VOd1lr6UCA=", "serviceCallbackTopic");
+
+        Payment payment = CardPaymentComponentTest.getPaymentsData().get(2);
+
+        PaymentFeeLink paymentFeeLink = PaymentFeeLink.paymentFeeLinkWith().paymentReference("00000005")
+            .payments(Arrays.asList(payment))
+            .fees(PaymentsDataUtil.getFeesData())
+            .build();
+
+        PaymentDto dto = new PaymentDtoMapper()
+            .toRetrievePaymentStatusesDto(paymentFeeLink);
+
+        Message msg = new Message(new ObjectMapper().writeValueAsString(dto));
+
+        msg.setContentType("application/json");
+        msg.setLabel("Service Callback Message");
+        msg.setProperties(Collections.singletonMap("serviceCallbackUrl", payment.getServiceCallbackUrl()));
+
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+        client.send(msg);
+
+        client.close();
+
+    }
+
+
+}
