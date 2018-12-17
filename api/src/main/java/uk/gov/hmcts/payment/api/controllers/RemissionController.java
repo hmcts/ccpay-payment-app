1e05de4526eb88b7efd41f94cacd7c615545c64b
mswirski <mateusz.swirski@hmcts.net>
2018-12-17 15:56:19 +0000

    working remission controller

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/RemissionController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/RemissionController.java
@@ -9,11 +9,14 @@ import io.swagger.annotations.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.payment.api.dto.RemissionRequest;
 import uk.gov.hmcts.payment.api.service.RemissionService;
@@ -40,5 +43,12 @@ public class RemissionController {
         remissionService.create(remissionRequest.toRemission());
 
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public String return400onDataIntegrityViolation(DataIntegrityViolationException ex) {
+        LOG.error("Error while creating remission", ex);
+        return "Could not create remission";
     }
 }
