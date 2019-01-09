2df68997712638d1176b4e679a5a4495932c3d55
mswirski <mateusz.swirski@hmcts.net>
2019-01-09 11:40:31 +0000

    sonar fixes

--- api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import uk.gov.hmcts.payment.api.logging.Markers;
