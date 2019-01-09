e6010199c07574b1308f4de5442e15b5b2c3e07a
mswirski <mateusz.swirski@hmcts.net>
2019-01-09 10:47:50 +0000

    using SpringApplication.exit()

--- api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import uk.gov.hmcts.payment.api.logging.Markers;
@@ -20,11 +21,12 @@ public class PaymentApiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(PaymentApiApplication.class);
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = null;
         try {
-            SpringApplication.run(PaymentApiApplication.class, args);
+            context = SpringApplication.run(PaymentApiApplication.class, args);
         } catch (RuntimeException ex) {
             LOG.error(Markers.fatal, "Application crashed with error message: ", ex);
-            throw ex;
+            SpringApplication.exit(context);
         }
     }
 
