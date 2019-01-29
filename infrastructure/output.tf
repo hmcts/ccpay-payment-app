941573655a8c2cda333e4624396222fce0ec749a
Sateesh Mathangi <a-smathangi@hotels.com>
2019-01-29 12:10:27 +0000

    api-gw config for telephony product (api)

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -21,3 +21,8 @@ output "s2s_url" {
 output "OAUTH2_REDIRECT_URI" {
   value = "${var.test_frontend_url}"
 }
+
+# this variable will be accessible to tests as API_GATEWAY_URL environment variable
+output "api_gateway_url" {
+  value = "https://core-api-mgmt-${var.env}.azure-api.net/${local.api_base_path}"
+}
