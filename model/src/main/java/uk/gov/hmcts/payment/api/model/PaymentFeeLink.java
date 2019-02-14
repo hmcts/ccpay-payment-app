badc7637b49f9a07b14f9a85d6305947c381ef33
mswirski <mateusz.swirski@hmcts.net>
2019-02-14 14:47:25 +0000

    fix

--- model/src/main/java/uk/gov/hmcts/payment/api/model/PaymentFeeLink.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/model/PaymentFeeLink.java
@@ -1,10 +1,22 @@
 package uk.gov.hmcts.payment.api.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Date;
 import java.util.List;
 
