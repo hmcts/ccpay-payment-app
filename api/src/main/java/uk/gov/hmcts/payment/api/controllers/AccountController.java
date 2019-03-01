fa3dc270f41dc854237f80ff0a6eda73ee6e74d5
mswirski <mateusz.swirski@hmcts.net>
2019-03-01 10:51:50 +0000

    added extra exception handling

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/AccountController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/AccountController.java
@@ -10,14 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import uk.gov.hmcts.payment.api.dto.AccountDto;
 import uk.gov.hmcts.payment.api.exception.AccountNotFoundException;
+import uk.gov.hmcts.payment.api.exception.LiberataServiceInaccessibleException;
 import uk.gov.hmcts.payment.api.service.AccountService;
 
 @RestController
@@ -33,7 +36,8 @@ public class AccountController {
     @ApiOperation(value = "Get the account status and available balance for a PBA account number", notes = "Get the account status and available balance for a PBA account number")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "Account details retrieved"),
-        @ApiResponse(code = 404, message = "Account not found")
+        @ApiResponse(code = 404, message = "Account not found"),
+        @ApiResponse(code = 503, message = "Failed to connect with Liberata")
     })
     @GetMapping(value = "/accounts/{accountNumber}")
     public AccountDto getAccounts(@PathVariable("accountNumber") String accountNumber) {
@@ -42,6 +46,9 @@ public class AccountController {
         } catch (HttpClientErrorException ex) {
             LOG.error("Error while calling account", ex);
             throw new AccountNotFoundException("Account not found");
+        } catch (Exception ex) {
+            LOG.error("Failed to connect with Liberata. {}", ex.getMessage());
+            throw new LiberataServiceInaccessibleException("Failed to connect with Liberata. " + ex.getMessage());
         }
     }
 
@@ -51,4 +58,9 @@ public class AccountController {
         return ex.getMessage();
     }
 
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    @ExceptionHandler(LiberataServiceInaccessibleException.class)
+    public String return503(LiberataServiceInaccessibleException ex) {
+        return ex.getMessage();
+    }
 }
