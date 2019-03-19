661608d2da7414aadaa068b3dce7ff8720cbc11c
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2019-03-19 18:23:31 +0000

    removed duplicate one

--- model/src/main/java/uk/gov/hmcts/payment/api/util/PayStatusToPayHubStatus.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/util/PayStatusToPayHubStatus.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.payment.api.util;
 
 public enum PayStatusToPayHubStatus {
     created("Initiated"), started("Initiated"), submitted("Initiated"), success("Success"), failed("Failed"), cancelled("Failed"), error("Failed"),
-    pending("Pending"), decline("Failed"), cancelled("Failed);
+    pending("Pending"), decline("Failed");
 
     public String mapedStatus;
 
