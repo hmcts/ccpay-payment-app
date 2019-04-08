c8bef20c146e552c1edb9fbe2b5314a186f4739a
mswirski <matiswir@gmail.com>
2019-04-08 14:52:35 +0100

    fixed tests

--- model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataServiceImpl.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataServiceImpl.java
@@ -1,40 +1,21 @@
 package uk.gov.hmcts.payment.api.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.payment.referencedata.dto.SiteDTO;
+import uk.gov.hmcts.payment.referencedata.model.Site;
+import uk.gov.hmcts.payment.referencedata.service.SiteService;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 @Service
 public class ReferenceDataServiceImpl implements ReferenceDataService<SiteDTO> {
 
-    private String serverAddress = "localhost";
-
-    private int serverPort = 8080;
+    @Autowired
+    private SiteService<Site, String> siteService;
 
     @Override
     public List<SiteDTO> getSiteIDs() {
-        URL url = null;
-        try {
-            url = new URL("http", serverAddress, serverPort, "reference-data/sites");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<SiteDTO>> response = restTemplate.exchange(
-            url.toExternalForm(),
-            HttpMethod.GET,
-            null,
-            new ParameterizedTypeReference<List<SiteDTO>>() {
-            });
-        return response.getBody();
+        return SiteDTO.fromSiteList(siteService.findAll());
     }
 }
