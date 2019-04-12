ea90fec80e1178496932c378e7a565885c37c9b5
tarun-palisetty <tarun.mca@gmail.com>
2019-04-12 12:43:54 +0100

    PaymentGroup and Remission hibernate mapping

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/RemissionController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/RemissionController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.Tag;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.checkdigit.CheckDigitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +20,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.payment.api.contract.FeeDto;
+import uk.gov.hmcts.payment.api.dto.RemissionDto;
 import uk.gov.hmcts.payment.api.dto.RemissionRequest;
-import uk.gov.hmcts.payment.api.model.PaymentFee;
+import uk.gov.hmcts.payment.api.dto.RemissionServiceRequest;
+import uk.gov.hmcts.payment.api.dto.mapper.RemissionDtoMapper;
 import uk.gov.hmcts.payment.api.model.PaymentFeeLink;
-import uk.gov.hmcts.payment.api.model.PaymentFeeLinkRepository;
-import uk.gov.hmcts.payment.api.model.PaymentFeeRepository;
 import uk.gov.hmcts.payment.api.service.RemissionService;
 import uk.gov.hmcts.payment.api.v1.model.exceptions.InvalidPaymentGroupReferenceException;
 import uk.gov.hmcts.payment.api.validators.RemissionValidator;
 
 import javax.validation.Valid;
-import java.util.Collections;
-import java.util.Optional;
 
 @RestController
 @Api(tags = {"Remission"})
@@ -48,10 +44,7 @@ public class RemissionController {
     private RemissionValidator remissionValidator;
 
     @Autowired
-    private PaymentFeeLinkRepository paymentFeeLinkRepository;
-
-    @Autowired
-    private PaymentFeeRepository paymentFeeRepository;
+    private RemissionDtoMapper remissionDtoMapper;
 
     @ApiOperation(value = "Create remission record", notes = "Create remission record")
     @ApiResponses(value = {
@@ -61,42 +54,24 @@ public class RemissionController {
     })
     @PostMapping(value = "/remission")
     @ResponseBody
-    public ResponseEntity<String> createRemission(@Valid @RequestBody RemissionRequest remissionRequest)
+    public ResponseEntity<RemissionDto> createRemission(@Valid @RequestBody RemissionRequest remissionRequest)
         throws CheckDigitException {
         remissionValidator.validate(remissionRequest);
-        Optional<FeeDto> feeDto = Optional.ofNullable(remissionRequest.getFee());
-        PaymentFee paymentFee = feeDto.map(feeDto1 -> PaymentFee.feeWith()
-            .code(feeDto1.getCode())
-            .version(feeDto1.getVersion())
-            .volume(feeDto1.getVolume())
-            .calculatedAmount(feeDto1.getCalculatedAmount())
-            .ccdCaseNumber(feeDto1.getCcdCaseNumber())
-            .reference(feeDto1.getReference())
-            .build()).orElse(null);
 
-        if (StringUtils.isEmpty(remissionRequest.getPaymentGroupReference())) {
-            String paymentGroupReference = PaymentReference.getInstance().getNext();
-            remissionRequest.setPaymentGroupReference(paymentGroupReference);
-            PaymentFeeLink paymentFeeLink = PaymentFeeLink.paymentFeeLinkWith()
-                .paymentReference(paymentGroupReference)
-                .fees(paymentFee != null ? Collections.singletonList(paymentFee) : null)
-                .build();
+        String paymentGroupReference = PaymentReference.getInstance().getNext();
+        RemissionServiceRequest remissionServiceRequest = RemissionServiceRequest.remissionServiceRequestWith()
+            .paymentGroupReference(paymentGroupReference)
+            .hwfAmount(remissionRequest.getHwfAmount())
+            .hwfReference(remissionRequest.getHwfReference())
+            .beneficiaryName(remissionRequest.getBeneficiaryName())
+            .ccdCaseNumber(remissionRequest.getCcdCaseNumber())
+            .caseReference(remissionRequest.getCaseReference())
+            .fees(remissionDtoMapper.toFees(remissionRequest.getFees()))
+            .build();
 
-            paymentFeeLinkRepository.save(paymentFeeLink);
-        } else if (feeDto.isPresent()) {
-            PaymentFeeLink paymentFeeLink = paymentFeeLinkRepository
-                .findByPaymentReference(remissionRequest.getPaymentGroupReference())
-                .orElseThrow(InvalidPaymentGroupReferenceException::new);
+        PaymentFeeLink paymentFeeLink = remissionService.create(remissionServiceRequest);
 
-            if (null != paymentFee)
-                paymentFee.setPaymentLink(paymentFeeLink);
-
-            paymentFeeRepository.save(paymentFee);
-        }
-
-        String generatedRemissionReference = remissionService.create(remissionRequest.toRemission());
-
-        return new ResponseEntity<>(generatedRemissionReference, HttpStatus.CREATED);
+        return new ResponseEntity<>(remissionDtoMapper.toCreateRemissionResponse(paymentFeeLink), HttpStatus.CREATED);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
---
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/RemissionDto.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.payment.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.payment.api.contract.FeeDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Builder(builderMethodName = "remissionDtoWith")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RemissionDto {
+
+    private String id;
+
+    private String remissionReference;
+
+    private String hwfReference;
+
+    private BigDecimal hwfAmount;
+
+    private String beneficiaryName;
+
+    private String ccdCaseNumber;
+
+    private String caseReference;
+
+    private String paymentGroupReference;
+
+    private List<FeeDto> fees;
+}
