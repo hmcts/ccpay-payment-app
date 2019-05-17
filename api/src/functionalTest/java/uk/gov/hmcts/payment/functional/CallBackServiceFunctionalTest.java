a54bc4be7f6e63b1b02a3bfd3bf1b8980293cadd
Sateesh Mathangi <a-smathangi@hotels.com>
2019-05-17 15:16:03 +0100

    added docs

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CallBackServiceFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CallBackServiceFunctionalTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.payment.functional;
 
-import io.restassured.RestAssured;
 import org.awaitility.Duration;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +15,7 @@ import uk.gov.hmcts.payment.functional.fixture.PaymentFixture;
 import uk.gov.hmcts.payment.functional.idam.IdamService;
 import uk.gov.hmcts.payment.functional.s2s.S2sTokenService;
 
+import static io.restassured.RestAssured.given;
 import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -47,7 +47,7 @@ public class CallBackServiceFunctionalTest {
     private static boolean TOKENS_INITIALIZED = false;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         if (!TOKENS_INITIALIZED) {
             USER_TOKEN = idamService.createUserWith(CMC_CITIZEN_GROUP, "citizen").getAuthorisationToken();
             SERVICE_TOKEN = s2sTokenService.getS2sToken(testProps.s2sServiceName, testProps.s2sServiceSecret);
@@ -56,8 +56,19 @@ public class CallBackServiceFunctionalTest {
     }
 
 
+    /**
+     * This test verifies E2E flow of serviceCallBack functionality when there is card payment happens.
+     * <p><ul>
+     * <li>Client POSTs a card payment with serviceCallbackUrl
+     * <li>Client GETs card payment
+     * <li>Cancel payment by invoking gov-pay REST API directly
+     * <li>Trigger status update job scheduler. This will sends a status update message to azure service bus which will be consumed by a azure function.
+     * <li>Verify serviceCallBack API has called from Azure function.
+     * Currently this test uses a Mock-API for serviceCallBack which is hosted in payment-app itself (Ideally a hosted mock server required)
+     * </ul><p>
+     */
     @Test
-    public void shouldInvokeCallBackForStatusUpdate() throws Exception {
+    public void shouldInvokeCallBackForStatusUpdate() {
         final String[] reference = new String[1];
 
         String serviceCallBackUrl = testProps.mockCallBackUrl.replaceFirst("https", "http"); // internal urls with http only.
@@ -80,7 +91,7 @@ public class CallBackServiceFunctionalTest {
             .then().get();
 
         // Step 3: Cancel payment - trigger govPay status change
-        RestAssured.given()
+        given()
             .header(AUTHORIZATION, "Bearer " + govpayCmcKey)
             .post(govpayUrl + "/" + paymentDto.getExternalReference() +"/cancel")
             .then()
@@ -95,12 +106,8 @@ public class CallBackServiceFunctionalTest {
 
         // Step 5: verify callback invocation from azure functions
         await()
-            .pollInterval(Duration.FIVE_HUNDRED_MILLISECONDS)
-            .atMost(Duration.FIVE_SECONDS)
-            .until(() -> RestAssured.given()
-                .header(AUTHORIZATION, USER_TOKEN)
-                .header("ServiceAuthorization", SERVICE_TOKEN)
-                .get(testProps.mockCallBackUrl + "/" + reference[0]).getStatusCode() == 200);
+            .pollInterval(Duration.TWO_HUNDRED_MILLISECONDS)
+            .atMost(Duration.TWO_SECONDS)
+            .until(() -> given().get(testProps.mockCallBackUrl + "/" + reference[0]).getStatusCode() == 200);
     }
-
 }
