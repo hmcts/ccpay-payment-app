7a4b408578ea07be3c797953beb67d8cd945497a
mswirski <matiswir@gmail.com>
2019-05-22 11:12:50 +0100

    WIP

--- api/src/integrationTest/java/uk/gov/hmcts/payment/integration/IntegrationTestContextConfiguration.java
+++ api/src/integrationTest/java/uk/gov/hmcts/payment/integration/IntegrationTestContextConfiguration.java
@@ -1,11 +1,33 @@
 package uk.gov.hmcts.payment.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.config.ObjectMapperConfig;
+import io.restassured.path.json.config.JsonPathConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
+
+import static io.restassured.config.JsonConfig.jsonConfig;
 
 @Configuration
 @ComponentScan("uk.gov.hmcts.payment.integration")
 @PropertySource("classpath:application-integration-tests.properties")
 public class IntegrationTestContextConfiguration {
+//    @Value("${test.url:http://localhost:8080}")
+//    private String baseURL;
+//
+//    @PostConstruct
+//    public void initialize() {
+//        RestAssured.config = RestAssured.config()
+//            .objectMapperConfig(
+//                ObjectMapperConfig.objectMapperConfig().jackson2ObjectMapperFactory((cls, charset) -> new ObjectMapper())
+//            )
+//            .jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL));
+//        RestAssured.useRelaxedHTTPSValidation();
+//        RestAssured.baseURI = baseURL;
+//    }
 }
--- api/src/integrationTest/java/uk/gov/hmcts/payment/integration/SwaggerPublisher.java
+++ api/src/integrationTest/java/uk/gov/hmcts/payment/integration/SwaggerPublisher.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
@@ -15,13 +16,12 @@ import java.nio.file.Paths;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Built-in feature which saves service's swagger specs in temporary directory.
- * Each travis run on master should automatically save and upload (if updated) documentation.
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureMockMvc
+@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles({"local", "componenttest", "mockcallbackservice"})
+//@ContextConfiguration(classes = IntegrationTestContextConfiguration.class)
 public class SwaggerPublisher {
 
     @Autowired
