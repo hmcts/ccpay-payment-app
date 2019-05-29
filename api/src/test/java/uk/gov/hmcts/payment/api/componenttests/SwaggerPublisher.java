3337afb5b5e035799f97fa07e493b06f12900dbe
mswirski <matiswir@gmail.com>
2019-05-29 12:01:17 +0100

    refactoring

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.payment.api.configuration.security.AuthCheckerConfiguration;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
