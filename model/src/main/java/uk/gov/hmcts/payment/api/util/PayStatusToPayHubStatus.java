6da6c83ae1486e2b25050222511bff4c40c3a180
mswirski <matiswir@gmail.com>
2019-06-03 15:15:03 +0100

    refactoring and reassured ccdCaseNumber is stored when passed in the request

--- model/src/main/java/uk/gov/hmcts/payment/api/util/PayStatusToPayHubStatus.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/util/PayStatusToPayHubStatus.java
@@ -3,14 +3,14 @@ package uk.gov.hmcts.payment.api.util;
 public enum PayStatusToPayHubStatus {
     created("Initiated"), started("Initiated"), submitted("Initiated"), success("Success"), failed("Failed"), cancelled("Failed"), error("Failed"), pending("Pending");
 
-    public String mapedStatus;
+    public String mappedStatus;
 
     PayStatusToPayHubStatus(String status) {
-        this.mapedStatus = status;
+        this.mappedStatus = status;
     }
 
-    String getMapedStatus() {
-        return mapedStatus;
+    String getMappedStatus() {
+        return mappedStatus;
     }
 
 }
