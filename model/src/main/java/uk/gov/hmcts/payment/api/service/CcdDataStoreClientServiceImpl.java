1768366025590f127fcf649c87eb813286858821
mswirski <matiswir@gmail.com>
2019-06-24 16:54:30 +0100

    added ccd api dependency

--- model/src/main/java/uk/gov/hmcts/payment/api/service/CcdDataStoreClientServiceImpl.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/CcdDataStoreClientServiceImpl.java
@@ -3,27 +3,24 @@ package uk.gov.hmcts.payment.api.service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.payment.api.dto.CcdCaseDetailsDto;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
 
 @Service
 @Profile("!ccdMock")
-public class CcdDataStoreServiceImpl implements CcdDataStoreService<CcdCaseDetailsDto, String> {
+public class CcdDataStoreClientServiceImpl implements CcdDataStoreClientService<CaseDetails, String> {
 
     @Autowired
-    private OAuth2RestOperations restTemplate;
-
-    @Value("${ccd-data-store.api.url}")
-    private String baseUrl;
+    private CoreCaseDataApi coreCaseDataApi;
 
     @Override
     @HystrixCommand(commandKey = "retrieveCcdCaseInformation", commandProperties = {
         @HystrixProperty(name = "execution.timeout.enabled", value = "false")
     })
-    public CcdCaseDetailsDto retrieve(String ccdCaseReference) {
-        return restTemplate.getForObject(baseUrl + "/cases/" + ccdCaseReference, CcdCaseDetailsDto.class);
+    public CaseDetails getCase(String userAuthToken, String serviceAuthToken, String ccdCaseReference) {
+        return coreCaseDataApi.getCase(userAuthToken, serviceAuthToken, ccdCaseReference);
     }
 }
