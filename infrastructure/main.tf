7b34b0083d7228ec261a31e9ec651fec2f3023c2
tarun-palisetty <tarun.mca@gmail.com>
2019-07-12 11:02:54 +0100

    Added dummy env var

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -178,6 +178,7 @@ module "payment-api" {
 
     # CCD
     CORE_CASE_DATA_API_URL = "${var.core_case_data_api_url}"
+    CCD_CLIENT_URL = "${var.core_case_data_api_url}"
 
     # PCI PAL
     PCI_PAL_ACCOUNT_ID_CMC = "${data.azurerm_key_vault_secret.pci_pal_account_id_cmc.value}"
