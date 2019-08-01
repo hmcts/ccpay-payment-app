c531cfa61c1056071177422d54244a588925c059
Vinoth <vinothkumar.rajadurai@hmcts.net>
2019-08-01 22:53:59 +0100

    interim commit

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentGroupController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentGroupController.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.payment.api.controllers;
 
 import com.google.common.collect.Lists;
 import io.swagger.annotations.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.checkdigit.CheckDigitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +12,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.payment.api.contract.CardPaymentRequest;
 import uk.gov.hmcts.payment.api.contract.FeeDto;
+import uk.gov.hmcts.payment.api.contract.PaymentDto;
 import uk.gov.hmcts.payment.api.dto.PaymentGroupDto;
+import uk.gov.hmcts.payment.api.dto.PaymentServiceRequest;
+import uk.gov.hmcts.payment.api.dto.PciPalPaymentRequest;
+import uk.gov.hmcts.payment.api.dto.mapper.PaymentDtoMapper;
 import uk.gov.hmcts.payment.api.dto.mapper.PaymentGroupDtoMapper;
+import uk.gov.hmcts.payment.api.model.Payment;
 import uk.gov.hmcts.payment.api.model.PaymentFee;
 import uk.gov.hmcts.payment.api.model.PaymentFeeLink;
+import uk.gov.hmcts.payment.api.service.DelegatingPaymentService;
 import uk.gov.hmcts.payment.api.service.PaymentGroupService;
+import uk.gov.hmcts.payment.api.service.PciPalPaymentService;
+import uk.gov.hmcts.payment.api.util.ReferenceUtil;
 import uk.gov.hmcts.payment.api.v1.model.exceptions.InvalidFeeRequestException;
 import uk.gov.hmcts.payment.api.v1.model.exceptions.InvalidPaymentGroupReferenceException;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @Api(tags = {"Payment group"})
@@ -35,11 +49,24 @@ public class PaymentGroupController {
 
     private final PaymentGroupDtoMapper paymentGroupDtoMapper;
 
+    private final DelegatingPaymentService<PaymentFeeLink, String> delegatingPaymentService;
+
+    private final PaymentDtoMapper paymentDtoMapper;
+
+    private final PciPalPaymentService pciPalPaymentService;
+
+    private final ReferenceUtil referenceUtil;
 
     @Autowired
-    public PaymentGroupController(PaymentGroupService paymentGroupService, PaymentGroupDtoMapper paymentGroupDtoMapper) {
+    public PaymentGroupController(PaymentGroupService paymentGroupService, PaymentGroupDtoMapper paymentGroupDtoMapper,
+                                  DelegatingPaymentService<PaymentFeeLink, String> delegatingPaymentService, PaymentDtoMapper paymentDtoMapper,
+                                  PciPalPaymentService pciPalPaymentService, ReferenceUtil referenceUtil) {
         this.paymentGroupService = paymentGroupService;
         this.paymentGroupDtoMapper = paymentGroupDtoMapper;
+        this.delegatingPaymentService = delegatingPaymentService;
+        this.paymentDtoMapper = paymentDtoMapper;
+        this.pciPalPaymentService = pciPalPaymentService;
+        this.referenceUtil = referenceUtil;
     }
 
     @ApiOperation(value = "Get payments/remissions/fees details by payment group reference", notes = "Get payments/remissions/fees details for supplied payment group reference")
@@ -108,6 +135,59 @@ public class PaymentGroupController {
 
         return new ResponseEntity<>(paymentGroupDtoMapper.toPaymentGroupDto(paymentFeeLink), HttpStatus.OK);
     }
+
+    @ApiOperation(value = "Create card payment in Payment Group", notes = "Create card payment in Payment Group")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Payment created"),
+        @ApiResponse(code = 400, message = "Payment creation failed"),
+        @ApiResponse(code = 422, message = "Invalid or missing attribute")
+    })
+    @PostMapping(value = "/card-payments")
+    @ResponseBody
+    public ResponseEntity<PaymentDto> createCardPayment(
+        @RequestHeader(value = "return-url") String returnURL,
+        @RequestHeader(value = "service-callback-url", required = false) String serviceCallbackUrl,
+        @Valid @RequestBody CardPaymentRequest request) throws CheckDigitException {
+
+        if (StringUtils.isEmpty(request.getChannel()) || StringUtils.isEmpty(request.getProvider())) {
+            request.setChannel("online");
+            request.setProvider("gov pay");
+        }
+
+        PaymentServiceRequest paymentServiceRequest = PaymentServiceRequest.paymentServiceRequestWith()
+            .description(request.getDescription())
+            .paymentReference(referenceUtil.getNext("RC"))
+            .returnUrl(returnURL)
+            .ccdCaseNumber(request.getCcdCaseNumber())
+            .caseReference(request.getCaseReference())
+            .currency(request.getCurrency().getCode())
+            .siteId(request.getSiteId())
+            .serviceType(request.getService().getName())
+            .amount(request.getAmount())
+            .serviceCallbackUrl(serviceCallbackUrl)
+            .channel(request.getChannel())
+            .provider(request.getProvider())
+            .build();
+
+        PaymentFeeLink paymentLink = delegatingPaymentService.update(paymentServiceRequest);
+        PaymentDto paymentDto = paymentDtoMapper.toCardPaymentDto(paymentLink);
+
+        if (request.getChannel().equals("telephony") && request.getProvider().equals("pci pal")) {
+            PciPalPaymentRequest pciPalPaymentRequest = PciPalPaymentRequest.pciPalPaymentRequestWith().orderAmount(request.getAmount().toString()).orderCurrency(request.getCurrency().getCode())
+                .orderReference(paymentDto.getReference()).build();
+            pciPalPaymentRequest.setCustomData2(paymentLink.getPayments().get(0).getCcdCaseNumber());
+            String link = pciPalPaymentService.getPciPalLink(pciPalPaymentRequest, request.getService().name());
+            paymentDto = paymentDtoMapper.toPciPalCardPaymentDto(paymentLink, link);
+        }
+
+        return new ResponseEntity<>(paymentDto, CREATED);
+    }
+
+    private Payment getPayment(PaymentFeeLink paymentFeeLink, String paymentReference){
+        return paymentFeeLink.getPayments().stream().
+            filter(p -> p.getReference().equals(paymentReference)).findAny().orElseThrow(InvalidPaymentGroupReferenceException::new);
+    }
+
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(InvalidPaymentGroupReferenceException.class)
