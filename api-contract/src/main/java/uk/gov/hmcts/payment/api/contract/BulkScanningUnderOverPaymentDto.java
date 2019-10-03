bbbb6df33e3c425edeb4bde4a26dac5a1b26b0f4
aravindan89 <aravindan.ranjan@hmcts.net>
2019-10-03 17:00:00 +0100

    Surplus and Shortfall report changes.

---
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/BulkScanningUnderOverPaymentDto.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.payment.api.contract;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Builder(builderMethodName = "report2DtoWith")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BulkScanningUnderOverPaymentDto {
+
+    private String respServiceId;
+
+    private String respServiceName;
+
+    private String surplusShortfall;
+
+    private BigDecimal balance;
+
+    private BigDecimal paymentAmount;
+
+    private String ccdCaseReference;
+
+    private Date processedDate;
+}
