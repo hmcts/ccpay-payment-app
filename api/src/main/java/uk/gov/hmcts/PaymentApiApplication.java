11f0fbefa5540a0779c5f0d506be19175c99648c
463952 <subroto.mukherjee@HMCTS.NET>
2019-11-28 11:33:47 +0000

    PAY-Knightly

--- api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/PaymentApiApplication.java
@@ -17,7 +17,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import uk.gov.hmcts.payment.api.logging.Markers;
-
 import javax.servlet.ServletContextListener;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
