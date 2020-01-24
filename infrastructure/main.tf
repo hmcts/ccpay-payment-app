a58e448b68f3be09360602d0620b8082a77d9027
aravindan89 <aravindan.ranjan@hmcts.net>
2020-01-24 15:41:46 +0000

    Resource creation done in another approach.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -102,21 +102,6 @@ resource "azurerm_key_vault_secret" "gov-pay-keys-cmc-claim-store" {
   key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "pba_cmc_payments_email_to" {
-  name = "pba-cmc-payments-email-to"
-  key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
-}
-
-resource "azurerm_key_vault_secret" "pba_probate_payments_email_to" {
-  name = "pba-probate-payments-email-to"
-  key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
-}
-
-resource "azurerm_key_vault_secret" "pba_finrem_payments_email_to" {
-  name = "pba-finrem-payments-email-to"
-  key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
-}
-
 data "azurerm_key_vault_secret" "gov_pay_keys_probate" {
   name = "gov-pay-keys-probate"
   key_vault_id = "${data.azurerm_key_vault.payment_key_vault.id}"
