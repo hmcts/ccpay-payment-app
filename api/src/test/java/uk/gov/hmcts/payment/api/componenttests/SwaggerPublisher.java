a17a83bbfed3ef6ee39167585b9d1a3224915738
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2020-04-15 22:21:24 +0100

    Revert "Fix SwaggerPublisher test"
    This reverts commit 68c7af0e663849ea41ab34637077c1daf2830adf.

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
@@ -1,26 +1,21 @@
 package uk.gov.hmcts.payment.api.componenttests;
 
-import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles({"local", "componenttest", "mockcallbackservice"})
@@ -30,18 +25,6 @@ public class SwaggerPublisher {
 
     @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private WebApplicationContext webAppContext;
-
-    @Before
-    public void setup() {
-        WebRequestTrackingFilter filter = new WebRequestTrackingFilter();
-        filter.init(new MockFilterConfig()); // using a mock that you construct with init params and all
-        this.mvc = webAppContextSetup(this.webAppContext)
-            .apply(springSecurity())
-            .addFilters(filter).build();
-    }
 
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
