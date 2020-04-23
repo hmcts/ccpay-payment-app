d77292860a6d0f6202b7ec3e845b92799e8fd0e6
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-04-23 13:21:15 +0100

    Merge conflict removed.

--- model/src/main/java/uk/gov/hmcts/payment/api/service/PciPalPaymentService.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/PciPalPaymentService.java
@@ -117,7 +117,4 @@ public class PciPalPaymentService implements DelegatingPaymentService<PciPalPaym
         T get() throws IOException, URISyntaxException;
     }
 
-    @Override
-    public void cancel(String paymentReference) {}
-
 }
