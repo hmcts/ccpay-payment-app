1977e918cb2efd7b68ae94d36edd90338a0dcbac
aravindan89 <aravindan.ranjan@hmcts.net>
2020-04-28 14:54:20 +0100

    Code clean up and review comments fixed.

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/ServicePaymentFilter.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/ServicePaymentFilter.java
@@ -23,7 +23,7 @@ public class ServicePaymentFilter extends OncePerRequestFilter {
         this.authTokenValidator = authTokenValidator;
     }
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
 
         String token = request.getHeader("ServiceAuthorization");
