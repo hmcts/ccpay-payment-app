d7c06ffe9bd979fccb9f71fdb3cccb68ece4ffbb
aravindan89 <aravindan.ranjan@hmcts.net>
2020-06-02 18:08:33 +0100

    userId changes updated.

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/AuthenticatedUserIdSupplier.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/AuthenticatedUserIdSupplier.java
@@ -1,24 +1,26 @@
 package uk.gov.hmcts.payment.api.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.payment.api.configuration.SecurityUtils;
 import uk.gov.hmcts.payment.api.v1.model.UserIdSupplier;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
 @Component
 public class AuthenticatedUserIdSupplier implements UserIdSupplier {
+
+    @Autowired
+    SecurityUtils securityUtils;
+
     @Override
     public String get() {
-        String userName = null;
-        if(SecurityContextHolder.getContext().getAuthentication() instanceof JwtAuthenticationToken)
-        {
-            userName = ((JwtAuthenticationToken)SecurityContextHolder.getContext().getAuthentication()).getName();
-        }
-        else
-        {
-            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            userName = user.getUsername();
-        }
-        return userName;
+
+        UserInfo userInfo = securityUtils.getUserInfo();
+
+        return userInfo != null ? userInfo.getUid() : null;
     }
 }
