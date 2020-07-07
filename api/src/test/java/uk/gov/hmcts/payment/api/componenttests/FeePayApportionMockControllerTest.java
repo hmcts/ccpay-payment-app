220d6ab18c2056a2dc30f324c38e699384507b20
aravindan89 <aravindan.ranjan@hmcts.net>
2020-07-07 18:35:09 +0100

    Apportionment - PAY-3184 and PAY-3429 changes added.

---
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/FeePayApportionMockControllerTest.java
@@ -0,0 +1,378 @@
+package uk.gov.hmcts.payment.api.componenttests;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.validator.routines.checkdigit.CheckDigitException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.payment.api.dto.FeePayApportionCCDCase;
+import uk.gov.hmcts.payment.api.model.*;
+import uk.gov.hmcts.payment.api.service.FeePayApportionService;
+import uk.gov.hmcts.payment.api.util.ReferenceUtil;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles({"local", "componenttest"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@Transactional
+public class FeePayApportionMockControllerTest {
+
+    @Autowired
+    private FeePayApportionService feePayApportionService;
+
+    @Autowired
+    private ReferenceUtil referenceUtil;
+
+    @Test
+    public void SingleFeeMultiplePay_ExactPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233334444";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.09.2020"));
+        feeCreatedDates.add(parseDate("01.10.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(50));
+        paymentAmounts.add(new BigDecimal(20));
+        paymentAmounts.add(new BigDecimal(30));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.09.2020"));
+        //paymentCreatedDates.add(parseDate("01.10.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 1))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 3))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(100), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+    }
+
+    @Test
+    public void SingleFeeMultiplePay_SurplusPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233335555";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.09.2020"));
+        feeCreatedDates.add(parseDate("01.10.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(80));
+        paymentAmounts.add(new BigDecimal(20));
+        paymentAmounts.add(new BigDecimal(30));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.09.2020"));
+        //paymentCreatedDates.add(parseDate("01.10.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 1))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 3))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(130), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+    }
+
+    @Test
+    public void SingleFeeMultiplePay_ShortfallPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233336666";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.09.2020"));
+        feeCreatedDates.add(parseDate("01.10.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(80));
+        paymentAmounts.add(new BigDecimal(10));
+        paymentAmounts.add(new BigDecimal(5));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.09.2020"));
+        //paymentCreatedDates.add(parseDate("01.10.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 1))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 3))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(95), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+    }
+
+    @Test
+    public void MultipleFeeMultiplePay_ExactPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233337777";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+        feeAmounts.add(new BigDecimal(40));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(50));
+        paymentAmounts.add(new BigDecimal(50));
+        paymentAmounts.add(new BigDecimal(40));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.09.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 2))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 3))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(100), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+        assertEquals(new BigDecimal(40), feePayApportionCCDCase.getFees().get(1).getAllocatedAmount());
+    }
+
+    @Test
+    public void MultipleFeeMultiplePay_SurplusPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233338888";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+        feeAmounts.add(new BigDecimal(25));
+        feeAmounts.add(new BigDecimal(25));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.09.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+        remissionAmounts.add(new BigDecimal(0));
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(100));
+        paymentAmounts.add(new BigDecimal(80));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 3))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 2))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(100), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+        assertEquals(new BigDecimal(25), feePayApportionCCDCase.getFees().get(1).getAllocatedAmount());
+        assertEquals(new BigDecimal(55), feePayApportionCCDCase.getFees().get(2).getAllocatedAmount());
+    }
+
+    @Test
+    public void MultipleFeeMultiplePay_ShortfallPayment() throws CheckDigitException {
+
+        String ccdCase = "1111222233339999";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(100));
+        feeAmounts.add(new BigDecimal(50));
+        feeAmounts.add(new BigDecimal(10));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.08.2020"));
+        feeCreatedDates.add(parseDate("01.09.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+        remissionAmounts.add(new BigDecimal(0));
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(80));
+        paymentAmounts.add(new BigDecimal(50));
+        paymentAmounts.add(new BigDecimal(10));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.09.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 3))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates,3))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(100), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+        assertEquals(new BigDecimal(40), feePayApportionCCDCase.getFees().get(1).getAllocatedAmount());
+        assertEquals(null, feePayApportionCCDCase.getFees().get(2).getAllocatedAmount());
+    }
+
+    @Test
+    public void MultipleFeeMultiplePay_UseCase5() throws CheckDigitException {
+
+        String ccdCase = "1111222244441111";
+
+        List<BigDecimal> feeAmounts = new ArrayList<>();
+        feeAmounts.add(new BigDecimal(500));
+
+        List<Timestamp> feeCreatedDates = new ArrayList<>();
+        feeCreatedDates.add(parseDate("01.08.2020"));
+
+        List<BigDecimal> remissionAmounts = new ArrayList<>();
+        remissionAmounts.add(new BigDecimal(0));
+
+        List<BigDecimal> paymentAmounts = new ArrayList<>();
+        paymentAmounts.add(new BigDecimal(200));
+        paymentAmounts.add(new BigDecimal(500));
+
+        List<Date> paymentCreatedDates = new ArrayList<>();
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+        paymentCreatedDates.add(parseDate("01.08.2020"));
+
+        FeePayApportionCCDCase feePayApportionCCDCase = FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+            .ccdCaseNo(ccdCase)
+            .feePayGroups(getPaymentFeeLinks(1))
+            .fees(getFees(ccdCase, feeAmounts, remissionAmounts, feeCreatedDates, 1))
+            .payments(getPayments(ccdCase, paymentAmounts, paymentCreatedDates, 2))
+            .build();
+
+        feePayApportionService.processFeePayApportion(feePayApportionCCDCase);
+
+        assertEquals(new BigDecimal(700), feePayApportionCCDCase.getFees().get(0).getAllocatedAmount());
+    }
+
+    private List<PaymentFeeLink> getPaymentFeeLinks(int count) throws CheckDigitException {
+        List<PaymentFeeLink> paymentFeeLinks = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            paymentFeeLinks.add(PaymentFeeLink.paymentFeeLinkWith()
+                .id(RandomUtils.nextInt())
+                .paymentReference(referenceUtil.getNext("GR"))
+                .build());
+        }
+        return paymentFeeLinks;
+    }
+
+    private List<Payment> getPayments(String ccdCase, List<BigDecimal> amounts, List<Date> paymentCreatedDates, int count) throws CheckDigitException {
+        List<Payment> payments = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            payments.add(Payment.paymentWith()
+                .id(RandomUtils.nextInt(100, 999))
+                .amount(amounts.get(i))
+                .reference(referenceUtil.getNext("RC"))
+                .paymentStatus(PaymentStatus.paymentStatusWith().name("success").build())
+                .dateCreated(paymentCreatedDates.get(i))
+                .paymentChannel(PaymentChannel.paymentChannelWith().name("online").build())
+                .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
+                .paymentProvider(PaymentProvider.paymentProviderWith().name("gov pay").build())
+                .serviceType("PROBATE")
+                .ccdCaseNumber(ccdCase)
+                .build());
+        }
+        return payments;
+    }
+
+    private List<PaymentFee> getFees(String ccdCase, List<BigDecimal> amounts, List<BigDecimal> remissionAmounts, List<Timestamp> feeCreatedDates, int count) throws CheckDigitException {
+        List<PaymentFee> fees = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            PaymentFee fee = PaymentFee.feeWith()
+                .id(RandomUtils.nextInt(100, 999))
+                .code("FEE00" + i)
+                .feeAmount(amounts.get(i))
+                .volume(1)
+                .calculatedAmount(amounts.get(i).multiply(new BigDecimal(1)))
+                .netAmount(amounts.get(i).multiply(new BigDecimal(1)).subtract(remissionAmounts.get(i)))
+                .currApportionAmount(new BigDecimal(0))
+                .dateCreated(feeCreatedDates.get(i))
+                .build();
+
+            fees.add(fee);
+        }
+        return fees;
+    }
+
+    private Timestamp parseDate(String date) {
+        try {
+            return new Timestamp(new SimpleDateFormat("dd.MM.yyyy").parse(date).getTime());
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
