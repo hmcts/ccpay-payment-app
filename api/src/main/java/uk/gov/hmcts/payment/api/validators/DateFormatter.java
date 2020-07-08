fa91e09dae8fd00c294918862c56aa3602e226a4
aravindan89 <aravindan.ranjan@hmcts.net>
2020-07-08 16:29:37 +0100

    Apportionment - Moved date formatter to common class.

---
+++ api/src/main/java/uk/gov/hmcts/payment/api/validators/DateFormatter.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.payment.api.validators;
+
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class DateFormatter {
+
+    public Date parseDate(String date) {
+        try {
+            return new SimpleDateFormat("dd.MM.yyyy").parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
