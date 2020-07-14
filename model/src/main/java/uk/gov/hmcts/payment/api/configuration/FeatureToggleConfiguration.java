35b76b029b54f8dabaaf21deb78e02e2f5a5c3c0
aravindan89 <aravindan.ranjan@hmcts.net>
2020-07-14 12:14:02 +0100

    Apportionment - PAY-3446 Launch Darkly configuration changes.

--- model/src/main/java/uk/gov/hmcts/payment/api/configuration/FeatureToggleConfiguration.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/configuration/FeatureToggleConfiguration.java
@@ -4,6 +4,8 @@ import com.launchdarkly.sdk.server.Components;
 import com.launchdarkly.sdk.server.LDClient;
 import com.launchdarkly.sdk.server.LDConfig;
 import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,9 @@ import java.time.Duration;
 @Configuration
 public class FeatureToggleConfiguration {
 
-    @Value("${launchDarkly.sdkKey}")
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureToggleConfiguration.class);
+
+    @Value("${launch.darkly.sdk.key}")
     private String sdkKey;
 
     @Value("${launchDarkly.connectionTimeout}")
@@ -42,6 +46,7 @@ public class FeatureToggleConfiguration {
 
     @Bean
     public LDClientInterface ldClient(LDConfig ldConfig) {
+        LOG.info("SDK key in FeatureToggleConfiguration: {}", sdkKey);
         return new LDClient(sdkKey, ldConfig);
     }
 
