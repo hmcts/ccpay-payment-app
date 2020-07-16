582ef6aae76cd0f51028b61b02b995503d3fa026
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-07-16 19:04:19 +0100

    Test Code Checked IN

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CMCCardPaymentFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CMCCardPaymentFunctionalTest.java
@@ -63,6 +63,7 @@ public class CMCCardPaymentFunctionalTest {
             USER_TOKEN = idamService.createUserWith(CMC_CITIZEN_GROUP, "citizen").getAuthorisationToken();
             USER_TOKEN_PAYMENT = idamService.createUserWith(CMC_CITIZEN_GROUP, "payments").getAuthorisationToken();
             SERVICE_TOKEN = s2sTokenService.getS2sToken(testProps.s2sServiceName, testProps.s2sServiceSecret);
+
             TOKENS_INITIALIZED = true;
         }
     }
@@ -185,18 +186,18 @@ public class CMCCardPaymentFunctionalTest {
         final String[] reference = new String[1];
         // create card payment
         List<FeeDto> fees = new ArrayList<>();
-        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999888877776666").feeAmount(new BigDecimal(20))
+        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999881577776666").feeAmount(new BigDecimal(20))
             .volume(1).version("1").calculatedAmount(new BigDecimal(20)).build());
-        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999888877776666").feeAmount(new BigDecimal(40))
+        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999881577776666").feeAmount(new BigDecimal(40))
             .volume(1).version("1").calculatedAmount(new BigDecimal(40)).build());
-        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999888877776666").feeAmount(new BigDecimal(60))
+        fees.add(FeeDto.feeDtoWith().code("FEE0271").ccdCaseNumber("9999881577776666").feeAmount(new BigDecimal(60))
             .volume(1).version("1").calculatedAmount(new BigDecimal(60)).build());
 
         CardPaymentRequest cardPaymentRequest = CardPaymentRequest.createCardPaymentRequestDtoWith()
             .amount(new BigDecimal("120"))
             .description("description")
             .caseReference("telRefNumber")
-            .ccdCaseNumber("9999888877776666")
+            .ccdCaseNumber("9999881577776666")
             .service(Service.CMC)
             .currency(CurrencyCode.GBP)
             .siteId("AA08")
