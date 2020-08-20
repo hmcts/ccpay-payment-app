bffa8bffe0604281671a3696e4ca8b0c31263ed8
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-08-20 13:42:44 +0100

    Exception Handling & RollBack Scripts

--- model/src/main/java/uk/gov/hmcts/payment/api/service/FeePayApportionServiceImpl.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/FeePayApportionServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import uk.gov.hmcts.payment.api.dto.FeePayApportionCCDCase;
 import uk.gov.hmcts.payment.api.model.*;
-import uk.gov.hmcts.payment.api.util.DateFormatter;
+import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentException;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -30,137 +30,144 @@ public class FeePayApportionServiceImpl implements FeePayApportionService {
 
     private final PaymentFeeRepository paymentFeeRepository;
 
-    private final DateFormatter dateFormatter;
-
     private final String APPORTION_TYPE_AUTO = "AUTO";
-
-    private final String APPORTION_TYPE_MANUAL = "MANUAL";
 
     private Boolean isSurplus = false;
 
     public FeePayApportionServiceImpl(PaymentFeeLinkRepository paymentFeeLinkRepository,
                                       PaymentStatusRepository paymentStatusRepository,
                                       FeePayApportionRepository feePayApportionRepository,
-                                      PaymentFeeRepository paymentFeeRepository, DateFormatter dateFormatter) {
+                                      PaymentFeeRepository paymentFeeRepository) {
         this.paymentFeeLinkRepository = paymentFeeLinkRepository;
         this.paymentStatusRepository = paymentStatusRepository;
         this.feePayApportionRepository = feePayApportionRepository;
         this.paymentFeeRepository = paymentFeeRepository;
-        this.dateFormatter = dateFormatter;
     }
 
     @Override
     public void updateFeeAmountDue(Payment payment) {
-        Optional<List<FeePayApportion>> apportions = feePayApportionRepository.findByPaymentId(payment.getId());
-        if(apportions.isPresent()) {
-            apportions.get().stream()
-                .forEach(feePayApportion -> {
-                    PaymentFee fee = paymentFeeRepository.findById(feePayApportion.getFeeId()).get();
-                    if(feePayApportion.getCallSurplusAmount() == null) {
-                        feePayApportion.setCallSurplusAmount(BigDecimal.valueOf(0));
-                    }
-                    fee.setAmountDue(fee.getAmountDue().subtract(feePayApportion.getApportionAmount()
-                        .add(feePayApportion.getCallSurplusAmount())));
-
-                    if(payment.getPaymentChannel() != null && payment.getPaymentChannel().getName() != null &&
-                        (payment.getPaymentChannel().getName().equalsIgnoreCase("telephony") ||
-                            payment.getPaymentChannel().getName().equalsIgnoreCase("online"))) {
-                        if(fee.getAmountDue() != null && fee.getAmountDue().compareTo(BigDecimal.valueOf(0)) < 0) {
-                            feePayApportion.setCallSurplusAmount(feePayApportion.getCallSurplusAmount().subtract(fee.getAmountDue()));
-                            feePayApportionRepository.save(feePayApportion);
+        try {
+            Optional<List<FeePayApportion>> apportions = feePayApportionRepository.findByPaymentId(payment.getId());
+            if(apportions.isPresent()) {
+                apportions.get().stream()
+                    .forEach(feePayApportion -> {
+                        PaymentFee fee = paymentFeeRepository.findById(feePayApportion.getFeeId()).get();
+                        if(feePayApportion.getCallSurplusAmount() == null) {
+                            feePayApportion.setCallSurplusAmount(BigDecimal.valueOf(0));
                         }
-                    }
+                        fee.setAmountDue(fee.getAmountDue().subtract(feePayApportion.getApportionAmount()
+                            .add(feePayApportion.getCallSurplusAmount())));
 
-                    paymentFeeRepository.save(fee);
-                    LOG.info("Updated FeeId " + fee.getId() + " as PaymentId " + payment.getId() + " Status Changed to " + payment.getPaymentStatus().getName());
-                });
+                        if(payment.getPaymentChannel() != null && payment.getPaymentChannel().getName() != null &&
+                            (payment.getPaymentChannel().getName().equalsIgnoreCase("telephony") ||
+                                payment.getPaymentChannel().getName().equalsIgnoreCase("online"))) {
+                            if(fee.getAmountDue() != null && fee.getAmountDue().compareTo(BigDecimal.valueOf(0)) < 0) {
+                                feePayApportion.setCallSurplusAmount(feePayApportion.getCallSurplusAmount().subtract(fee.getAmountDue()));
+                                feePayApportionRepository.save(feePayApportion);
+                            }
+                        }
+
+                        paymentFeeRepository.save(fee);
+                        LOG.info("Updated FeeId " + fee.getId() + " as PaymentId " + payment.getId() + " Status Changed to " + payment.getPaymentStatus().getName());
+                    });
+            }
+        } catch (Exception ex) {
+            throw new PaymentException("Payment Auto-Apportionment - update Fee AmountDue Failed", ex);
         }
     }
 
     @Override
     public void processApportion(Payment payment) {
-        Optional<List<PaymentFee>> savedFees = paymentFeeRepository.findByCcdCaseNumber(payment.getCcdCaseNumber());
-        if (savedFees.isPresent()) {
+        try {
+            Optional<List<PaymentFee>> savedFees = paymentFeeRepository.findByCcdCaseNumber(payment.getCcdCaseNumber());
+            if (savedFees.isPresent()) {
 
-            List<PaymentFee> sortedFees = savedFees.get()
-                .stream()
-                .filter(fee -> fee.getDateCreated() != null)
-                .sorted(Comparator.comparing(PaymentFee::getDateCreated))
-                .collect(Collectors.toList());
-            sortedFees.stream().forEach(fee -> {
-                fee.setNetAmount(fee.getNetAmount() != null
-                    ? fee.getNetAmount()
-                    : payment.getPaymentLink() != null ? getFeeCalculatedNetAmount(fee, payment.getPaymentLink().getRemissions())
-                    : getFeeCalculatedNetAmount(fee, null));
-                if (fee.getAmountDue() == null) {
-                    fee.setAmountDue(fee.getNetAmount());
-                }
-            });
-            this.processFeePayApportion(FeePayApportionCCDCase.feePayApportionCCDCaseWith()
-                .ccdCaseNo(payment.getCcdCaseNumber())
-                .fees(sortedFees)
-                .payments(Lists.newArrayList(payment))
-                .build());
+                List<PaymentFee> sortedFees = savedFees.get()
+                    .stream()
+                    .filter(fee -> fee.getDateCreated() != null)
+                    .sorted(Comparator.comparing(PaymentFee::getDateCreated))
+                    .collect(Collectors.toList());
+                sortedFees.stream().forEach(fee -> {
+                    fee.setNetAmount(fee.getNetAmount() != null
+                        ? fee.getNetAmount()
+                        : payment.getPaymentLink() != null ? getFeeCalculatedNetAmount(fee, payment.getPaymentLink().getRemissions())
+                        : getFeeCalculatedNetAmount(fee, null));
+                    if (fee.getAmountDue() == null) {
+                        fee.setAmountDue(fee.getNetAmount());
+                    }
+                });
+                this.processFeePayApportion(FeePayApportionCCDCase.feePayApportionCCDCaseWith()
+                    .ccdCaseNo(payment.getCcdCaseNumber())
+                    .fees(sortedFees)
+                    .payments(Lists.newArrayList(payment))
+                    .build());
+            }
+        } catch (Exception ex) {
+            throw new PaymentException("Payment Auto-Apportionment failed", ex);
         }
     }
 
     @Override
     public FeePayApportionCCDCase processFeePayApportion(FeePayApportionCCDCase feePayApportionCCDCase) {
 
-        BigDecimal callSurplusAmount = BigDecimal.valueOf(0);
-        BigDecimal remainingPaymentAmount;
+        try {
+            BigDecimal callSurplusAmount = BigDecimal.valueOf(0);
+            BigDecimal remainingPaymentAmount;
 
-        List<Payment> paymentsToBeApportioned = getPaymentsToBeApportioned(feePayApportionCCDCase.getPayments());
-        List<PaymentFee> feesToBeApportioned = getFeesToBeApportioned(feePayApportionCCDCase.getFees());
+            List<Payment> paymentsToBeApportioned = getPaymentsToBeApportioned(feePayApportionCCDCase.getPayments());
+            List<PaymentFee> feesToBeApportioned = getFeesToBeApportioned(feePayApportionCCDCase.getFees());
 
-        if (!paymentsToBeApportioned.isEmpty() && !feesToBeApportioned.isEmpty()) {
-            List<FeePayApportion> feePayApportions = new ArrayList<>();
+            if (!paymentsToBeApportioned.isEmpty() && !feesToBeApportioned.isEmpty()) {
+                List<FeePayApportion> feePayApportions = new ArrayList<>();
 
-            for (Payment payment : paymentsToBeApportioned) {
-                remainingPaymentAmount = payment.getAmount();
+                for (Payment payment : paymentsToBeApportioned) {
+                    remainingPaymentAmount = payment.getAmount();
 
-                for (PaymentFee fee : feesToBeApportioned) {
-                    if (fee.getAmountDue() != null && fee.getAmountDue().compareTo(BigDecimal.valueOf(0)) > 0) {
+                    for (PaymentFee fee : feesToBeApportioned) {
+                        if (fee.getAmountDue() != null && fee.getAmountDue().compareTo(BigDecimal.valueOf(0)) > 0) {
 
-                        FeePayApportion feePayApportion = applyFeePayApportion(fee, payment, remainingPaymentAmount);
-                        feePayApportions.add(feePayApportion);
+                            FeePayApportion feePayApportion = applyFeePayApportion(fee, payment, remainingPaymentAmount);
+                            feePayApportions.add(feePayApportion);
 
-                        if (remainingPaymentAmount.compareTo(fee.getAmountDue()) > 0) {
-                            remainingPaymentAmount = remainingPaymentAmount.subtract(fee.getAmountDue());
-                        }else {
-                            remainingPaymentAmount = BigDecimal.valueOf(0);
-                        }
+                            if (remainingPaymentAmount.compareTo(fee.getAmountDue()) > 0) {
+                                remainingPaymentAmount = remainingPaymentAmount.subtract(fee.getAmountDue());
+                            }else {
+                                remainingPaymentAmount = BigDecimal.valueOf(0);
+                            }
 
-                        if (remainingPaymentAmount.compareTo(BigDecimal.valueOf(0)) > 0) {
-                            continue;
-                        } else {
-                            break;
+                            if (remainingPaymentAmount.compareTo(BigDecimal.valueOf(0)) > 0) {
+                                continue;
+                            } else {
+                                break;
+                            }
                         }
                     }
+                    // End Fee Loop
+                    if (remainingPaymentAmount.compareTo(BigDecimal.valueOf(0)) > 0) {
+                        callSurplusAmount = remainingPaymentAmount;
+                        isSurplus = true;
+                    } else {
+                        isSurplus = false;
+                    }
                 }
-                // End Fee Loop
-                if (remainingPaymentAmount.compareTo(BigDecimal.valueOf(0)) > 0) {
-                    callSurplusAmount = remainingPaymentAmount;
-                    isSurplus = true;
-                } else {
-                    isSurplus = false;
-                }
-            }
-            // End Payment Loop
-            if (feePayApportions != null && !feePayApportions.isEmpty()) {
-                if (isSurplus) {
-                    findSurplusFee(feePayApportions, callSurplusAmount, feePayApportionCCDCase.getFees());
-                }
-                feePayApportions.stream().forEach(feePayApportion -> {
-                    feePayApportionRepository.save(feePayApportion);
-                });
-                feePayApportionCCDCase.getFees().stream()
-                    .forEach(fee -> {
-                        paymentFeeRepository.save(fee);
+                // End Payment Loop
+                if (feePayApportions != null && !feePayApportions.isEmpty()) {
+                    if (isSurplus) {
+                        findSurplusFee(feePayApportions, callSurplusAmount, feePayApportionCCDCase.getFees());
+                    }
+                    feePayApportions.stream().forEach(feePayApportion -> {
+                        feePayApportionRepository.save(feePayApportion);
                     });
+                    feePayApportionCCDCase.getFees().stream()
+                        .forEach(fee -> {
+                            paymentFeeRepository.save(fee);
+                        });
+                }
             }
+            return feePayApportionCCDCase;
+        } catch (Exception ex) {
+            throw new PaymentException("Payment Auto-Apportionment failed", ex);
         }
-        return feePayApportionCCDCase;
     }
 
     private FeePayApportion applyFeePayApportion(PaymentFee fee, Payment payment, BigDecimal remainingPaymentAmount) {
