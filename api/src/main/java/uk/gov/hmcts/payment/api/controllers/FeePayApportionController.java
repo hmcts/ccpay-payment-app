8965c8d3babb4e13a2512119c4c34c2453a070f0
aravindan89 <aravindan.ranjan@hmcts.net>
2020-08-21 15:21:35 +0100

    Logs added.

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/FeePayApportionController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/FeePayApportionController.java
@@ -1,8 +1,9 @@
 package uk.gov.hmcts.payment.api.controllers;
 
 import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,6 @@ import uk.gov.hmcts.payment.api.dto.PaymentGroupDto;
 import uk.gov.hmcts.payment.api.dto.mapper.PaymentGroupDtoMapper;
 import uk.gov.hmcts.payment.api.model.*;
 import uk.gov.hmcts.payment.api.service.PaymentService;
-import uk.gov.hmcts.payment.api.util.DateFormatter;
 import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentNotFoundException;
 
 import java.util.List;
@@ -31,6 +31,8 @@ public class FeePayApportionController {
     @Autowired
     private LaunchDarklyFeatureToggler featureToggler;
 
+    private static final Logger LOG = LoggerFactory.getLogger(FeePayApportionController.class);
+
     @Autowired
     public FeePayApportionController(PaymentService<PaymentFeeLink, String> paymentService,PaymentFeeRepository paymentFeeRepository,PaymentGroupDtoMapper paymentGroupDtoMapper,LaunchDarklyFeatureToggler featureToggler) {
         this.paymentService = paymentService;
@@ -47,21 +49,25 @@ public class FeePayApportionController {
     })
     @GetMapping(value = "/payment-groups/fee-pay-apportion/{paymentreference}")
     public ResponseEntity<PaymentGroupDto> retrieveApportionDetails(@PathVariable("paymentreference") String paymentReference) {
-
+        LOG.info("Invoking new API in FeePayApportionController");
         PaymentFeeLink paymentFeeLink = paymentService.retrieve(paymentReference);
         boolean apportionFeature = featureToggler.getBooleanValue("apportion-feature",false);
+        LOG.info("apportionFeature value in FeePayApportionController: {}", apportionFeature);
         Optional<Payment> payment = paymentFeeLink.getPayments().stream()
             .filter(p -> p.getReference().equals(paymentReference)).findAny();
         List<PaymentFee> feeList = paymentFeeLink.getFees();
         if (payment.isPresent() && apportionFeature)
         {
+            LOG.info("Apportion feature is true and payment is available in FeePayApportionController");
             List<FeePayApportion> feePayApportionList = paymentService.findByPaymentId(payment.get().getId());
             if(feePayApportionList != null && !feePayApportionList.isEmpty()) {
+                LOG.info("Apportion details available in FeePayApportionController");
                 feePayApportionList.stream()
                     .forEach(feePayApportion -> {
                         feeList.stream()
                             .forEach(paymentFee -> {
                                 if (feePayApportion.getFeeId().equals(paymentFee.getId())) {
+                                    LOG.info("Fee id matched in FeePayApportionController");
                                     PaymentFee fee = paymentFeeRepository.findById(feePayApportion.getFeeId()).get();
                                     fee.setApportionAmount(feePayApportion.getApportionAmount());
                                 }
