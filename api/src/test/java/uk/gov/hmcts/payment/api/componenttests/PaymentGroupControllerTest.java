68eafabb6460dd65cfcac242c518ddad563ae6ed
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-09-16 18:57:05 +0100

    PAY-3637- Payment s2s token generation changes

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentGroupControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentGroupControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
