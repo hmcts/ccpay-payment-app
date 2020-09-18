443d5b7f0971ca1f72498d8f152ffa78ad1d629a
aravindan89 <aravindan.ranjan@hmcts.net>
2020-09-18 16:22:56 +0100

    Code refactoring.

---
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/TelephonyDto.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.payment.api.contract;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Builder(builderMethodName = "telephonyDtoWith")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TelephonyDto {
+
+    private String paymentReference;
+
+    private String paymentGroupReference;
+
+    private String ccdCaseNumber;
+
+    private String status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "GMT")
+    private Date dateCreated;
+
+    @JsonProperty("_links")
+    private LinksDto links;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    @JsonInclude(NON_NULL)
+    public static class LinksDto {
+        private LinkDto nextUrl;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(NON_NULL)
+    public static class LinkDto {
+        private String href;
+        private String method;
+
+        //Added as part of PCI PAL Antenna Implementation
+        private String accessToken;
+        private String refreshToken;
+    }
+}
