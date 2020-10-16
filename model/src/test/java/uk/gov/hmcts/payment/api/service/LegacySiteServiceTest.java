c2c83f8b8d91813359530cab6149d2f69b09c5a7
VPushkaran+1-at-371816880156 <vignesh.pushkaran@hmcts.net>
2020-10-16 11:02:04 +0100

    Updated test case for Service

--- model/src/test/java/uk/gov/hmcts/payment/api/service/LegacySiteServiceTest.java
+++ model/src/test/java/uk/gov/hmcts/payment/api/service/LegacySiteServiceTest.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.payment.api.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.payment.api.model.LegacySite;
+import uk.gov.hmcts.payment.api.model.LegacySiteRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,16 +20,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LegacySiteServiceTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(LegacySiteServiceTest.class);
-    
+
+    @Mock
+    private LegacySiteRepository legacySiteRepository;
+
     @InjectMocks
-    @Autowired
     private LegacySiteServiceImpl legacySiteService;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void GetLegacySites() throws Exception {
-        List<LegacySite> legacySites = legacySiteService.getAllSites();
-        assertThat(legacySites).isNotNull();
-        LegacySite legacySite = legacySites.get(0);
+        List<LegacySite> legacySites = new ArrayList<LegacySite>();
+        legacySites.add(new LegacySite("Y402","gad seed"));
+        Mockito.when(legacySiteService.getAllSites()).thenReturn(legacySites);
+        assertThat(legacySiteService.getAllSites()).isNotNull();
+        LegacySite legacySite = legacySiteService.getAllSites().get(0);
         assertThat(legacySite.getSiteId()).isEqualTo("Y402");
     }
 }
