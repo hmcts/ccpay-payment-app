9868e09ff754a3cd32a9cc31a534332bd203bec7
aravindan89 <aravindan.ranjan@hmcts.net>
2020-10-19 11:15:41 +0100

    changed from info back to debug for logs.

--- model/src/main/java/uk/gov/hmcts/payment/api/service/PciPalPaymentService.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/PciPalPaymentService.java
@@ -55,7 +55,7 @@ public class PciPalPaymentService implements DelegatingPaymentService<PciPalPaym
         return withIOExceptionHandling(() -> {
             String ppAccountID = getppAccountId(serviceType);
 
-            LOG.info("ppAccountID: {} SERVICE_TYPE_CMC: {} serviceType: {}", ppAccountID, SERVICE_TYPE_CMC, serviceType);
+            LOG.debug("ppAccountID: {} SERVICE_TYPE_CMC: {} serviceType: {}", ppAccountID, SERVICE_TYPE_CMC, serviceType);
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair("ppAccountID", ppAccountID));
             params.add(new BasicNameValuePair("orderAmount", new BigDecimal(pciPalPaymentRequest.getOrderAmount()).movePointRight(2).toString()));
