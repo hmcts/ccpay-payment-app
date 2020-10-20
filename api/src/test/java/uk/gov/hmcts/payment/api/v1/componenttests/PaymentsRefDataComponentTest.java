750f59eb6a2f2f0e3862ea2228f3c6af28f34241
VPushkaran+1-at-371816880156 <vignesh.pushkaran@hmcts.net>
2020-10-20 14:43:50 +0100

    refactor on comments

--- api/src/test/java/uk/gov/hmcts/payment/api/v1/componenttests/PaymentsRefDataComponentTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/v1/componenttests/PaymentsRefDataComponentTest.java
@@ -1,13 +1,42 @@
 package uk.gov.hmcts.payment.api.v1.componenttests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.payment.api.controllers.PaymentReferenceDataController;
 import uk.gov.hmcts.payment.api.model.*;
+import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.UserResolverBackdoor;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 public class PaymentsRefDataComponentTest extends PaymentsComponentTest {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(PaymentReferenceDataController.class);
+
+    private static final String USER_ID = UserResolverBackdoor.AUTHENTICATED_USER_ID;
+
+    @MockBean
+    private LegacySiteRepository legacySiteRepository;
+
+    @InjectMocks
+    private PaymentReferenceDataController paymentReferenceDataController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void testFindAllPaymentChannels() throws Exception {
@@ -43,7 +72,7 @@ public class PaymentsRefDataComponentTest extends PaymentsComponentTest {
             .andExpect(status().isOk())
             .andExpect(body().asListOf(PaymentProvider.class, paymentProviders -> {
                 assertThat(paymentProviders).anySatisfy(paymentProvider -> {
-                   assertThat(paymentProvider.getName()).isEqualTo("GovPay");
+                    assertThat(paymentProvider.getName()).isEqualTo("GovPay");
                 });
             }));
     }
@@ -56,9 +85,28 @@ public class PaymentsRefDataComponentTest extends PaymentsComponentTest {
             .andExpect(status().isOk())
             .andExpect(body().asListOf(PaymentStatus.class, paymentStatuses -> {
                 assertThat(paymentStatuses).anySatisfy(paymentStatus -> {
-                   assertThat(paymentStatus.getName()).isEqualTo("Pending");
+                    assertThat(paymentStatus.getName()).isEqualTo("Pending");
                 });
             }));
     }
+
+    @Test
+    public void testFindAllLegacySites() throws Exception {
+        LegacySite legacySiteExpected = new LegacySite("site1", "site name 2");
+        LegacySite legacySiteActual = new LegacySite("site1", "site name 3");
+        when(legacySiteRepository.findAll()).thenReturn(Lists.newArrayList(legacySiteActual));
+        MvcResult mvcResult = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .withAuthorizedService("divorce")
+            .get("/refdata/legacy-sites")
+            .andExpect(status().isOk())
+            .andReturn();
+        List<LegacySite> legacySites = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<LegacySite>>() {});
+        LegacySite mockLegacySite = legacySites.get(0);
+        assertEquals(mockLegacySite.getSiteId(),legacySiteExpected.getSiteId());
+        assertNotEquals(mockLegacySite.getSiteName(),legacySiteExpected.getSiteName());
+    }
+
 
 }
