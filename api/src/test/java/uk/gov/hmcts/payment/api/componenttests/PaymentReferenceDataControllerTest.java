9a0bacd091472b034db2db357a5dc000af781e69
VPushkaran+1-at-371816880156 <vignesh.pushkaran@hmcts.net>
2020-10-21 14:06:38 +0100

    Update PaymentReferenceDataControllerTest.java

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentReferenceDataControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentReferenceDataControllerTest.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.payment.api.componenttests;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -39,10 +37,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PaymentReferenceDataControllerTest extends PaymentsComponentTest {
 
     private static final String USER_ID = UserResolverBackdoor.AUTHENTICATED_USER_ID;
-    @ClassRule
-    public static WireMockClassRule wireMockRule = new WireMockClassRule(9190);
+
     @Autowired
     private ObjectMapper objectMapper;
+
     @InjectMocks
     private PaymentReferenceDataController paymentReferenceDataController;
 
