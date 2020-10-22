9cede37a05d8387488b072073c89f1909a336090
VPushkaran+1-at-371816880156 <vignesh.pushkaran@hmcts.net>
2020-10-22 11:37:30 +0100

    Update PaymentReferenceDataControllerTest.java

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentReferenceDataControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/PaymentReferenceDataControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.payment.api.controllers.PaymentReferenceDataController;
 import uk.gov.hmcts.payment.api.model.LegacySite;
 import uk.gov.hmcts.payment.api.model.LegacySiteRepository;
@@ -24,16 +23,14 @@ import uk.gov.hmcts.payment.api.model.PaymentProvider;
 import uk.gov.hmcts.payment.api.model.PaymentProviderRepository;
 import uk.gov.hmcts.payment.api.model.PaymentStatus;
 import uk.gov.hmcts.payment.api.model.PaymentStatusRepository;
-import uk.gov.hmcts.payment.api.v1.componenttests.PaymentsComponentTest;
 import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.UserResolverBackdoor;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -61,17 +58,7 @@ public class PaymentReferenceDataControllerTest {
 
     @Test
     public void testFindAllPaymentChannels() throws Exception {
-//
-//        restActions
-//            .get("/refdata/channels")
-//            .andExpect(status().isOk())
-//            .andExpect(body().asListOf(PaymentChannel.class, paymentChannels -> {
-//                assertThat(paymentChannels).anySatisfy(paymentChannel -> {
-//                    assertThat(paymentChannel.getName()).isEqualTo("Online");
-//                });
-//            }));
 
-        //Controller Mock Test
         List<PaymentChannel> paymentChannelMock = Collections.singletonList(new PaymentChannel("card", "card payment"));
         when(paymentChannelRepository.findAll()).thenReturn(paymentChannelMock);
         List<PaymentChannel> paymentChannelMockResponse = paymentReferenceDataController.findAllPaymentChannels();
@@ -81,16 +68,6 @@ public class PaymentReferenceDataControllerTest {
     @Test
     public void testFindAllPaymentMethods() throws Exception {
 
-//        restActions
-//            .get("/refdata/methods")
-//            .andExpect(status().isOk())
-//            .andExpect(body().asListOf(PaymentMethod.class, paymentMethods -> {
-//                assertThat(paymentMethods).anySatisfy(paymentMethod -> {
-//                    assertThat(paymentMethod.getName()).isEqualTo("Card");
-//                });
-//            }));
-
-        //Controller Mock Test
         List<PaymentMethod> paymentMethodMock = Collections.singletonList(new PaymentMethod("online", "card payment"));
         when(paymentMethodRepository.findAll()).thenReturn(paymentMethodMock);
         List<PaymentMethod> paymentMethodMockResponse = paymentReferenceDataController.findAllPaymentMethods();
@@ -100,15 +77,6 @@ public class PaymentReferenceDataControllerTest {
     @Test
     public void testFindAllPaymentProviders() throws Exception {
 
-//        restActions
-//            .get("/refdata/providers")
-//            .andExpect(status().isOk())
-//            .andExpect(body().asListOf(PaymentProvider.class, paymentProviders -> {
-//                assertThat(paymentProviders).anySatisfy(paymentProvider -> {
-//                    assertThat(paymentProvider.getName()).isEqualTo("GovPay");
-//                });
-//            }));
-        //Controller Mock Test
         List<PaymentProvider> paymentProviderMock = Collections.singletonList(new PaymentProvider().GOV_PAY);
         when(paymentProviderRepository.findAll()).thenReturn(paymentProviderMock);
         List<PaymentProvider> paymentProviderMockResponse = paymentReferenceDataController.findAllPaymentProviders();
@@ -118,15 +86,6 @@ public class PaymentReferenceDataControllerTest {
     @Test
     public void testFindAllPaymentStatuses() throws Exception {
 
-//        MvcResult mvcResult = restActions
-//            .get("/refdata/status")
-//            .andExpect(status().isOk())
-//            .andReturn();
-//        List<PaymentStatus> paymentStatuses = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<PaymentStatus>>() {
-//        });
-//        PaymentStatus paymentStatus = paymentStatuses.get(0);
-//        assertEquals(paymentStatus.getName(), "created");
-        //Controller Mock Test
         List<PaymentStatus> paymentStatusMock = Collections.singletonList(new PaymentStatus().CREATED);
         when(paymentStatusRepository.findAll()).thenReturn(paymentStatusMock);
         List<PaymentStatus> paymentStatusMockResponse = paymentReferenceDataController.findAllPaymentStatuses();
@@ -136,19 +95,7 @@ public class PaymentReferenceDataControllerTest {
 
     @Test
     public void testFindAllLegacySites() throws Exception {
-//        MvcResult mvcResult = restActions
-//            .withAuthorizedUser(USER_ID)
-//            .withUserId(USER_ID)
-//            .withAuthorizedService("divorce")
-//            .get("/refdata/legacy-sites")
-//            .andExpect(status().isOk())
-//            .andReturn();
-//        List<LegacySite> legacySites = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<LegacySite>>() {
-//        });
-//        LegacySite legacySite = legacySites.get(0);
-//        assertEquals(legacySite.getSiteId(), "Y402");
-//        assertEquals(legacySite.getSiteName(), "Aberdare County Court");
-        //Controller Mock Test
+
         List<LegacySite> legacySiteMock = Collections.singletonList(new LegacySite("site 2", "Site name 1"));
         when(legacySiteRepository.findAll()).thenReturn(legacySiteMock);
         List<LegacySite> legacySiteMockResponse = paymentReferenceDataController.findAllLegacySites();
