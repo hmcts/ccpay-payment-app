3dce5452b528c05265fb52aea3cea343a354452a
Tim Jacomb <tim.jacomb@hmcts.net>
2020-12-14 12:05:28 +0000

    DTSPO-266 Enable self service read only access (#798)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,7 +36,9 @@ resource "azurerm_key_vault_secret" "gov-pay-keys-cmc-claim-store" {
 
 module "payment-database" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = join("-", [var.product, "postgres-db"])
+  product = var.product
+  component = var.component
+  name = join("-", [var.product, "postgres-db"])
   location = var.location
   env = var.env
   postgresql_user = var.postgresql_user
@@ -49,7 +51,9 @@ module "payment-database" {
 
 module "payment-database-v11" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = join("-", [var.product, "postgres-db-v11"])
+  product = var.product
+  component = var.component
+  name = join("-", [var.product, "postgres-db-v11"])
   location = var.location
   env = var.env
   postgresql_user = var.postgresql_user
