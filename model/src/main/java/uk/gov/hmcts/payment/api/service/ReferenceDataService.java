a848912aee2fd114d03ee0f55bd26b32976adcf9
VPushkaran+1-at-371816880156 <vignesh.pushkaran@hmcts.net>
2020-12-16 15:09:31 +0000

    Error handling and optimzation

--- model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataService.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataService.java
@@ -3,10 +3,9 @@ package uk.gov.hmcts.payment.api.service;
 import org.springframework.util.MultiValueMap;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ReferenceDataService<T> {
     List<T> getSiteIDs();
 
-    String getOrgId(String caseType, Map<String, String> headersMap);
+    String getOrgId(String caseType, MultiValueMap<String, String> headers);
 }
