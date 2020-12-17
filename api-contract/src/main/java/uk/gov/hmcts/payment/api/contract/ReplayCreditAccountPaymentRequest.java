f43dd2e6464d5d206d274bd32f81071fb70b06b2
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-12-17 13:22:44 +0000

    Replay PBA API Draft

--- api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/ReplayCreditAccountPaymentRequest.java
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/ReplayCreditAccountPaymentRequest.java
@@ -1,8 +1,24 @@
 package uk.gov.hmcts.payment.api.contract;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "replayCreditAccountPaymentRequestWith")
 public class ReplayCreditAccountPaymentRequest {
 
     @NotEmpty
---
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/ReplayCreditAccountPaymentServiceImpl.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.payment.api.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.payment.api.model.Payment;
+import uk.gov.hmcts.payment.api.model.Payment2Repository;
+import uk.gov.hmcts.payment.api.model.PaymentStatus;
+import uk.gov.hmcts.payment.api.model.StatusHistory;
+import uk.gov.hmcts.payment.api.util.PayStatusToPayHubStatus;
+import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentNotFoundException;
+
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+
+@Service
+public class ReplayCreditAccountPaymentServiceImpl implements ReplayCreditAccountPaymentService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReplayCreditAccountPaymentServiceImpl.class);
+
+    private final Payment2Repository paymentRespository;
+
+    @Autowired
+    public ReplayCreditAccountPaymentServiceImpl(Payment2Repository paymentRespository){
+        this.paymentRespository = paymentRespository;
+    }
+
+    @Override
+    public void updatePaymentStatusByReference(String paymentReference, PaymentStatus paymentStatus, String message) {
+
+        Payment payment = findSavedPayment(paymentReference);
+        payment.setPaymentStatus(paymentStatus);
+        payment.setStatus(paymentStatus.getName());
+        payment.setStatusHistories(Collections.singletonList(StatusHistory.statusHistoryWith()
+            .externalStatus(paymentStatus.getName())
+            .status(PayStatusToPayHubStatus.valueOf(paymentStatus.getName().toLowerCase()).getMappedStatus())
+            .message(message)
+            .build()));
+    }
+
+    private Payment findSavedPayment(@NotNull String paymentReference) {
+        return paymentRespository.findByReference(paymentReference).orElseThrow(PaymentNotFoundException::new);
+    }
+}
