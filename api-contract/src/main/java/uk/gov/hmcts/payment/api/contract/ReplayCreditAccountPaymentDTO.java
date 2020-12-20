f7c1dfdb516d41e3bc21d04a8c333528e35fcc31
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-12-20 19:56:23 +0000

    Code Review Comments

--- api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/ReplayCreditAccountPaymentDTO.java
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/ReplayCreditAccountPaymentDTO.java
@@ -3,10 +3,7 @@ package uk.gov.hmcts.payment.api.contract;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,7 +12,8 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonInclude(NON_NULL)
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(builderMethodName = "replayCreditAccountPaymentDTOWith")
