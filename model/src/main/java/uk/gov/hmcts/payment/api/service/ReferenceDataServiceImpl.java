aee28e381e68b545d30ec3c739ef2b5571c7c8e4
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-01-13 12:42:37 +0000

    Update ReferenceDataServiceImpl.java

--- model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataServiceImpl.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/ReferenceDataServiceImpl.java
@@ -1,17 +1,11 @@
 package uk.gov.hmcts.payment.api.service;
 
-import com.launchdarkly.shaded.kotlin.collections.EmptyList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.hmcts.payment.api.dto.OrganisationalServiceDto;
@@ -20,10 +14,7 @@ import uk.gov.hmcts.payment.referencedata.dto.SiteDTO;
 import uk.gov.hmcts.payment.referencedata.model.Site;
 import uk.gov.hmcts.payment.referencedata.service.SiteService;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ReferenceDataServiceImpl implements ReferenceDataService<SiteDTO> {
@@ -47,7 +38,8 @@ public class ReferenceDataServiceImpl implements ReferenceDataService<SiteDTO> {
         try{
             ResponseEntity<OrganisationalServiceDto[]> orgServiceResponse = getResponseFromLocationReference(caseType, headers);
             OrganisationalServiceDto[] orgServiceList = orgServiceResponse.getBody();
-            return orgServiceList[0];
+            OrganisationalServiceDto organisationalServiceDto=orgServiceList[0];
+            return organisationalServiceDto;
         }catch(NullPointerException e){
             throw new NoServiceFoundException( "No Service found for given CaseType");
         }
