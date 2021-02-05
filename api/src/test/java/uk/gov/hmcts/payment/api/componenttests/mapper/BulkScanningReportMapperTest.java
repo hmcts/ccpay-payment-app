a8040e12e424fa0505ee8f22a818804fb7129c7c
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-05 09:22:57 +0000

    refactoring

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/mapper/BulkScanningReportMapperTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/mapper/BulkScanningReportMapperTest.java
@@ -20,24 +20,27 @@ public class BulkScanningReportMapperTest {
     public void beforeEach() {
         List<Payment> payments = new ArrayList<Payment>();
         Integer[] num = new Integer[5];
-        Payment payment = Payment.paymentWith()
-            .amount(new BigDecimal("99.99"))
-            .caseReference("Reference" + number)
-            .ccdCaseNumber("ccdCaseNumber" + number)
-            .description("Test payments statuses for " + number)
-            .serviceType(Service.DIVORCE.getName())
-            .s2sServiceName(s2sServiceName)
-            .currency("GBP")
-            .siteId("AA0" + number)
-            .userId("USER_ID")
-            .paymentChannel(PaymentChannel.paymentChannelWith().name("online").build())
-            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
-            .paymentProvider(PaymentProvider.paymentProviderWith().name("gov pay").build())
-            .paymentStatus(PaymentStatus.paymentStatusWith().name("created").build())
-            .externalReference(externalReference)
-            .reference("RC-1519-9028-2432-000" + number)
-            .statusHistories(Arrays.asList(statusHistory))
-            .build();
+        for(Integer number:num){
+            Payment payment = Payment.paymentWith()
+                .amount(new BigDecimal("99.99"))
+                .caseReference("Reference" + number)
+                .ccdCaseNumber("ccdCaseNumber" + number)
+                .description("Test payments statuses for " + number)
+                .serviceType(Service.DIVORCE.getName())
+                .s2sServiceName("ccd_gw")
+                .currency("GBP")
+                .siteId("AA0" + number)
+                .userId("USER_ID")
+                .paymentChannel(PaymentChannel.paymentChannelWith().name("online").build())
+                .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
+                .paymentProvider(PaymentProvider.paymentProviderWith().name("gov pay").build())
+                .paymentStatus(PaymentStatus.paymentStatusWith().name("created").build())
+                .externalReference("e2kkddts5215h9qqoeuth5c0v"+)
+                .reference("RC-1519-9028-2432-000" + number)
+                .statusHistories(Arrays.asList(statusHistory))
+                .build();
+        }
+
 
     }
 
