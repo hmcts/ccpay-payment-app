1f75769b19da6e1527d0ac6ab6f413ec499da27f
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-11 10:33:48 +0000

    configtests

--- api/src/test/java/uk/gov/hmcts/payment/api/reports/config/BarPaymentReportConfigTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/reports/config/BarPaymentReportConfigTest.java
@@ -1,4 +1,15 @@
 package uk.gov.hmcts.payment.api.reports.config;
 
+import org.junit.Test;
+import uk.gov.hmcts.payment.api.reports.PaymentReportType;
+
 public class BarPaymentReportConfigTest {
+
+
+    BarPaymentReportConfig barPaymentReportConfig = new BarPaymentReportConfig();
+
+    @Test
+    public void testGetType(){
+        PaymentReportType type = barPaymentReportConfig.getType();
+    }
 }
