c66888ad4b1ed20529e7b9ec9bbe36abd92795b0
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-15 11:43:10 +0000

    cardpayment details service

--- model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
+++ model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
@@ -2,19 +2,49 @@ package uk.gov.hmcts.payment.api.service;
 
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.payment.api.external.client.dto.CardDetails;
+import uk.gov.hmcts.payment.api.external.client.dto.GovPayPayment;
+import uk.gov.hmcts.payment.api.model.Payment;
 import uk.gov.hmcts.payment.api.model.Payment2Repository;
+import uk.gov.hmcts.payment.api.model.PaymentMethod;
 import uk.gov.hmcts.payment.api.v1.model.ServiceIdSupplier;
 import uk.gov.hmcts.payment.api.v1.model.govpay.GovPayAuthUtil;
 
-public class CardDetailsServiceTest {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+public class CardDetailsServiceTest {
+    @Mock
     Payment2Repository paymentRespository;
 
+    @Mock
+    DelegatingPaymentService<GovPayPayment, String> delegate;
+
+    @Mock
+    GovPayAuthUtil govPayAuthUtil;
+
+    @Mock
+    ServiceIdSupplier serviceIdSupplier;
+
+    private final static String PAYMENT_BY_CARD = "card";
+
     @InjectMocks
-    CardDetailsServiceImpl cardDetailsService;
+    CardDetailsServiceImpl cardDetailsService = new CardDetailsServiceImpl(delegate,paymentRespository,govPayAuthUtil,serviceIdSupplier);
 
     @Test
     public void testRetrieve(){
+        Payment payment = Payment.paymentWith()
+                            .build();
+
+        when(paymentRespository.findByReferenceAndPaymentMethod(any(String.class),
+            PaymentMethod.paymentMethodWith().name(PAYMENT_BY_CARD).build())).thenReturn(java.util.Optional.ofNullable(payment));
+        when(govPayAuthUtil.getServiceName(any(String.class), any(String.class))).thenReturn("payment-service");
+        GovPayPayment govPayPayment = GovPayPayment.govPaymentWith()
+                                        .build();
+        when(delegate.retrieve(any(String.class), any(String.class))).thenReturn(govPayPayment);
+        CardDetails cardDetails = cardDetailsService.retrieve("payment-reference");
 
     }
 }
