7d2be0c8150a0f276261387b8ca16cc9cd9e7f66
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-15 08:53:19 +0000

    otp bootstrap fixed

--- otp/src/test/java/uk/gov/hmcts/payment/otp/OtpBootStrapTest.java
+++ otp/src/test/java/uk/gov/hmcts/payment/otp/OtpBootStrapTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.payment.otp;
 
 import com.amdelamar.jotp.OTP;
 import com.amdelamar.jotp.type.Type;
+import org.apache.commons.codec.binary.Base32;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -18,12 +19,6 @@ import static org.mockito.Mockito.*;
 
 public class OtpBootStrapTest {
 
-//    @Mock
-//    OTP otp;
-
-//    @InjectMocks
-//    OtpBootstrap otpBootstrap;
-
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
@@ -37,9 +32,11 @@ public class OtpBootStrapTest {
 
     @Test
     public void testMain() throws Exception {
-//        String[] input = {"secret"};
-//        OtpBootstrap.main(input);
-//        assertEquals("hello", outContent.toString());
+        Base32 base32 = new Base32();
+        String arg = base32.encodeAsString("test".getBytes());
+        String[] input = {arg};
+        OtpBootstrap.main(input);
+        assertEquals(7, outContent.toString().length());
 
     }
 
