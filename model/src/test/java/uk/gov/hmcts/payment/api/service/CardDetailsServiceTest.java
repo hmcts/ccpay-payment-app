b1c8c44e1931822e8390161176a445e79c744bb7
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-15 12:22:54 +0000

    card details service

--- model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
+++ model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
@@ -13,11 +13,12 @@ import uk.gov.hmcts.payment.api.v1.model.ServiceIdSupplier;
 import uk.gov.hmcts.payment.api.v1.model.govpay.GovPayAuthUtil;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CardDetailsServiceTest {
-    @Mock
-    Payment2Repository paymentRespository;
+//    @Mock
+//    Payment2Repository paymentRespository;
 
     @Mock
     DelegatingPaymentService<GovPayPayment, String> delegate;
@@ -30,14 +31,16 @@ public class CardDetailsServiceTest {
 
     private final static String PAYMENT_BY_CARD = "card";
 
-    @InjectMocks
-    CardDetailsServiceImpl cardDetailsService = new CardDetailsServiceImpl(delegate,paymentRespository,govPayAuthUtil,serviceIdSupplier);
+//    @InjectMocks
+//    CardDetailsServiceImpl cardDetailsService = new CardDetailsServiceImpl(delegate,paymentRespository,govPayAuthUtil,serviceIdSupplier);
 
     @Test
     public void testRetrieve(){
+
+        Payment2Repository paymentRespository = mock(Payment2Repository.class);
         Payment payment = Payment.paymentWith()
                             .build();
-
+        CardDetailsServiceImpl cardDetailsService = new CardDetailsServiceImpl(delegate,paymentRespository,govPayAuthUtil,serviceIdSupplier);
         when(paymentRespository.findByReferenceAndPaymentMethod(any(String.class),
             PaymentMethod.paymentMethodWith().name(PAYMENT_BY_CARD).build())).thenReturn(java.util.Optional.ofNullable(payment));
         when(govPayAuthUtil.getServiceName(any(String.class), any(String.class))).thenReturn("payment-service");
