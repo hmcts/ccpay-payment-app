af1cec14e81ea9933f5107dde62d561953671b44
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-15 14:14:08 +0000

    card details service test

--- model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
+++ model/src/test/java/uk/gov/hmcts/payment/api/service/CardDetailsServiceTest.java
@@ -1,9 +1,15 @@
 package uk.gov.hmcts.payment.api.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.payment.api.external.client.dto.CardDetails;
 import uk.gov.hmcts.payment.api.external.client.dto.GovPayPayment;
 import uk.gov.hmcts.payment.api.model.Payment;
@@ -12,31 +18,47 @@ import uk.gov.hmcts.payment.api.model.PaymentMethod;
 import uk.gov.hmcts.payment.api.v1.model.ServiceIdSupplier;
 import uk.gov.hmcts.payment.api.v1.model.govpay.GovPayAuthUtil;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+@RunWith(MockitoJUnitRunner.class)
 public class CardDetailsServiceTest {
+
+    @Mock
+    Payment2Repository paymentRespository;
+
+    @Mock
+    GovPayAuthUtil govPayAuthUtil;
+
+    @Mock
+    DelegatingPaymentService<GovPayPayment, String> delegate;
+
+    @Mock
+    ServiceIdSupplier serviceIdSupplier;
+
+    @InjectMocks
+    CardDetailsServiceImpl cardDetailsServiceImp;
 
     private final static String PAYMENT_BY_CARD = "card";
 
     @Test
     public void testRetrieve(){
-        Payment2Repository paymentRespository = mock(Payment2Repository.class);
-        GovPayAuthUtil govPayAuthUtil = mock(GovPayAuthUtil.class);
-        DelegatingPaymentService<GovPayPayment, String> delegate = mock(DelegatingPaymentService.class);
-        ServiceIdSupplier serviceIdSupplier = mock(ServiceIdSupplier.class);
-        Payment payment = Payment.paymentWith()
-                            .build();
-        CardDetailsServiceImpl cardDetailsService = new CardDetailsServiceImpl(delegate,paymentRespository,govPayAuthUtil,serviceIdSupplier);
-        when(paymentRespository.findByReferenceAndPaymentMethod(any(String.class),any(PaymentMethod.class))).thenReturn(java.util.Optional.ofNullable(payment));
-        when(govPayAuthUtil.getServiceName(any(String.class), any(String.class))).thenReturn("payment-service");
-
-        GovPayPayment govPayPayment = GovPayPayment.govPaymentWith()
-                                        .amount(100)
-                                        .build();
-        when(delegate.retrieve(any(String.class), any(String.class))).thenReturn(govPayPayment);
-        CardDetails cardDetails = cardDetailsService.retrieve("payment-reference");
+//        Payment payment = Payment.paymentWith()
+//                            .build();
+//        when(paymentRespository.findByReferenceAndPaymentMethod(any(String.class),any(PaymentMethod.class))).thenReturn(java.util.Optional.ofNullable(payment));
+//        when(govPayAuthUtil.getServiceName(any(String.class), any(String.class))).thenReturn("payment-service");
+//        CardDetails cardDetails = CardDetails.cardDetailsWith()
+//                                    .cardholderName("card-name")
+//                                    .lastDigitsCardNumber("1234")
+//                                    .build();
+//        GovPayPayment govPayPayment = GovPayPayment.govPaymentWith()
+//                                        .amount(100)
+//                                        .cardDetails(cardDetails)
+//                                        .build();
+//        when(delegate.retrieve(any(String.class), any(String.class))).thenReturn(govPayPayment);
+//        CardDetails resultCardDetails = cardDetailsServiceImp.retrieve("payment-reference");
+//        assertEquals("card-name",resultCardDetails.getCardholderName());
 
     }
 }
