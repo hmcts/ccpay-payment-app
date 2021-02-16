3ad8c2007a831e4749176eab81015c0b9ad0960a
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-02-16 09:17:02 +0000

    services test case

--- model/src/test/java/uk/gov/hmcts/payment/api/service/ReplayCreditAccountPaymentServiceTest.java
+++ model/src/test/java/uk/gov/hmcts/payment/api/service/ReplayCreditAccountPaymentServiceTest.java
@@ -4,42 +4,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.payment.api.external.client.dto.GovPayPayment;
 import uk.gov.hmcts.payment.api.model.Payment;
 import uk.gov.hmcts.payment.api.model.Payment2Repository;
-import uk.gov.hmcts.payment.api.model.PaymentMethod;
+import uk.gov.hmcts.payment.api.model.PaymentStatus;
 import uk.gov.hmcts.payment.api.model.StatusHistory;
-import uk.gov.hmcts.payment.api.v1.model.ServiceIdSupplier;
-import uk.gov.hmcts.payment.api.v1.model.govpay.GovPayAuthUtil;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static uk.gov.hmcts.payment.api.model.Payment.paymentWith;
-
 @RunWith(SpringRunner.class)
-public class CardDetailsServiceTest {
+public class ReplayCreditAccountPaymentServiceTest {
 
     @Mock
     Payment2Repository paymentRespository;
 
-    @Mock
-    DelegatingPaymentService<GovPayPayment, String> delegate;
-
-    @Mock
-    GovPayAuthUtil govPayAuthUtil;
-
-    @Mock
-    ServiceIdSupplier serviceIdSupplier;
-
     @InjectMocks
-    CardDetailsServiceImpl cardDetailsService;
+    ReplayCreditAccountPaymentServiceImpl replayCreditAccountPaymentService;
+
 
     @Test
-    public void testRetrieve(){
+    public void testUpdatePaymentStatusByReference(){
         Payment payment = paymentWith().amount(BigDecimal.valueOf(10000).movePointRight(2)).reference("reference1").description("desc1").returnUrl("returnUrl1")
             .ccdCaseNumber("ccdCaseNo1").caseReference("caseRef1").serviceType("cmc").currency("GBP")
             .statusHistories(Arrays.asList(StatusHistory.statusHistoryWith()
@@ -47,13 +41,8 @@ public class CardDetailsServiceTest {
                 .status("Initiated")
                 .build()))
             .build();
-        when(paymentRespository.findByReferenceAndPaymentMethod(any(String.class),
-            any(PaymentMethod.class))).thenReturn(java.util.Optional.ofNullable(payment));
-        when(govPayAuthUtil.getServiceName(any(String.class), any(String.class))).thenReturn("cmc");
-        GovPayPayment govPayPayment = GovPayPayment.govPaymentWith()
-                                            .build();
-        when(delegate.retrieve(any(String.class), any(String.class))).thenReturn(govPayPayment);
-        cardDetailsService.retrieve("reference");
+        Mockito.when(paymentRespository.findByReference(any(String.class))).thenReturn(java.util.Optional.ofNullable(payment));
+        replayCreditAccountPaymentService.updatePaymentStatusByReference("payment-reference", PaymentStatus.SUCCESS,"message");
+        verify(paymentRespository).findByReference("payment-reference");
     }
-
 }
