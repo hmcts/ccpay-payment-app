dbdb8c6bd40556c6050d9aa558047f3ba308777e
krishnakishorenuthalapati <krishna.nuthalapati@hmcts.net>
2021-03-01 02:29:10 +0000

    Added more tests

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/dsl/PaymentsTestDsl.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/dsl/PaymentsTestDsl.java
@@ -136,6 +136,12 @@ public class PaymentsTestDsl {
             return this;
         }
 
+        public PaymentWhenDsl createBulkScanPaymentStrategic(BulkScanPaymentRequest bulkScanPaymentRequest, String paymentGroupReference) {
+            response = newRequest().contentType(ContentType.JSON).body(bulkScanPaymentRequest)
+                .post("/payment-groups/{payment-group-reference}/bulk-scan-payments-strategic", paymentGroupReference );
+            return this;
+        }
+
         public PaymentWhenDsl createBulkScanPaymentWithPaymentGroup(BulkScanPaymentRequest bulkScanPaymentRequest) {
             response = newRequest().contentType(ContentType.JSON).body(bulkScanPaymentRequest)
                 .post("/payment-groups/bulk-scan-payments" );
