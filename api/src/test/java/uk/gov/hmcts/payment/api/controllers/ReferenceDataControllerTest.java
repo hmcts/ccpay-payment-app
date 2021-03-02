e1d00ac945f5c3ec3f8813d1ede977f394c1e097
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-03-02 22:46:36 +0000

    controller classes

---
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/ReferenceDataControllerTest.java
@@ -0,0 +1,93 @@
+package uk.gov.hmcts.payment.api.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.ServiceResolverBackdoor;
+import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.UserResolverBackdoor;
+import uk.gov.hmcts.payment.api.v1.componenttests.sugar.RestActions;
+import uk.gov.hmcts.payment.referencedata.dto.SiteDTO;
+import uk.gov.hmcts.payment.referencedata.model.Site;
+import uk.gov.hmcts.payment.referencedata.service.SiteService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles({"local", "componenttest"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ReferenceDataControllerTest {
+
+    @MockBean
+    private SiteService<Site, String> siteService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private ServiceResolverBackdoor serviceRequestAuthorizer;
+
+    @Autowired
+    private UserResolverBackdoor userRequestAuthorizer;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static final String USER_ID = UserResolverBackdoor.CITIZEN_ID;
+
+    private RestActions restActions;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        mockMvc = webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+        List<Site> serviceReturn = Arrays.asList(Site.siteWith()
+                .sopReference("sop")
+                .siteId("AA99")
+                .name("name")
+                .service("service")
+                .id(1)
+                .build(),
+            Site.siteWith()
+                .sopReference("sop")
+                .siteId("AA001")
+                .name("name")
+                .service("service")
+                .id(1)
+                .build()
+        );
+
+        when(siteService.getAllSites()).thenReturn(serviceReturn);
+    }
+
+
+    @Test
+    public void shouldReturnReferenceDataSites() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/reference-data/sites"))
+                                        .andExpect(status().isOk())
+                                        .andReturn();
+        List<SiteDTO> actualResponse = objectMapper.readValue(mvcResult.getResponse().getContentAsString(),List.class);
+        assertEquals(2,actualResponse.size());
+    }
+}
