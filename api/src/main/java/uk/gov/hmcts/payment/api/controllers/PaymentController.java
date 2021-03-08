94d85af35edeb4bfac8cc21ac7038998e829a9f3
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-03-08 17:25:43 +0000

    Revert "Merge branch 'master' into PAY-3900"
    This reverts commit 6548ee0ce702589b3a2e9db7df9bee4e038c8dd4, reversing
    changes made to 4b72cbe3e257858e29ccc1812b3ed5b168055087.

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentController.java
@@ -136,7 +136,7 @@ public class PaymentController {
         final List<PaymentDto> paymentDtos = new ArrayList<>();
         LOG.info("No of paymentFeeLinks retrieved for Liberata Pull : {}", paymentFeeLinks.size());
         for (final PaymentFeeLink paymentFeeLink: paymentFeeLinks) {
-            populatePaymentDtos(paymentDtos, paymentFeeLink, fromDateTime, toDateTime);
+            populatePaymentDtos(paymentDtos, paymentFeeLink);
         }
         return new PaymentsResponse(paymentDtos);
     }
@@ -189,23 +189,9 @@ public class PaymentController {
             .filter(p -> p.getReference().equals(reference)).findAny();
     }
 
-    private void populatePaymentDtos(final List<PaymentDto> paymentDtos, final PaymentFeeLink paymentFeeLink, Date fromDateTime, Date toDateTime) {
+    private void populatePaymentDtos(final List<PaymentDto> paymentDtos, final PaymentFeeLink paymentFeeLink) {
         //Adding this filter to exclude Exela payments if the bulk scan toggle feature is disabled.
         List<Payment> payments = getFilteredListBasedOnBulkScanToggleFeature(paymentFeeLink);
-
-        //Additional filter to retrieve payments within specified Date Range for Liberata Pull
-        if (null != fromDateTime && null != toDateTime) {
-            payments = payments.stream().filter(payment -> (payment.getDateUpdated().compareTo(fromDateTime) >= 0
-                && payment.getDateUpdated().compareTo(toDateTime) <= 0 ))
-                .collect(Collectors.toList());
-        } else if (null != fromDateTime) {
-            payments = payments.stream().filter(payment -> (payment.getDateUpdated().compareTo(fromDateTime) >= 0))
-                .collect(Collectors.toList());
-        } else if (null != toDateTime) {
-            payments = payments.stream().filter(payment -> (payment.getDateUpdated().compareTo(toDateTime) <= 0 ))
-                .collect(Collectors.toList());
-        }
-
         boolean apportionFeature = featureToggler.getBooleanValue("apportion-feature",false);
 
         LOG.info("BSP Feature ON : No of Payments retrieved for Liberata Pull : {}", payments.size());
