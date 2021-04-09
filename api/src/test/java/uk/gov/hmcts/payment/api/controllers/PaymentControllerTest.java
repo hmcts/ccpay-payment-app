eb2dd6ef465c7e2e2888aa7ee5bb3bdeb0e26ca3
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-04-09 14:07:15 +0100

    Updated testclass

--- api/src/test/java/uk/gov/hmcts/payment/api/controllers/PaymentControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/PaymentControllerTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.payment.api.componenttests;
+package uk.gov.hmcts.payment.api.controllers;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.payment.api.componenttests.PaymentDbBackdoor;
 import uk.gov.hmcts.payment.api.componenttests.util.PaymentsDataUtil;
 import uk.gov.hmcts.payment.api.configuration.LaunchDarklyFeatureToggler;
 import uk.gov.hmcts.payment.api.contract.FeeDto;
--- api/src/test/java/uk/gov/hmcts/payment/api/controllers/PaymentReportControllerMockTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/PaymentReportControllerMockTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.payment.api.componenttests;
+package uk.gov.hmcts.payment.api.controllers;
 
 import org.joda.time.DateTime;
 import org.junit.Before;
