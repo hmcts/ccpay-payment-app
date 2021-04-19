19d2a801bc010a81a239d3df9d81e22d308cca9e
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-04-19 09:54:40 +0100

    changes made for case details and paymentfeelink

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/CaseController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/CaseController.java
@@ -6,11 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.payment.api.contract.PaymentDto;
 import uk.gov.hmcts.payment.api.contract.PaymentsResponse;
+import uk.gov.hmcts.payment.api.domain.service.CaseDetailsService;
 import uk.gov.hmcts.payment.api.dto.PaymentGroupDto;
 import uk.gov.hmcts.payment.api.dto.PaymentGroupResponse;
 import uk.gov.hmcts.payment.api.dto.PaymentSearchCriteria;
 import uk.gov.hmcts.payment.api.dto.mapper.PaymentDtoMapper;
 import uk.gov.hmcts.payment.api.dto.mapper.PaymentGroupDtoMapper;
+import uk.gov.hmcts.payment.api.model.CaseDetails;
 import uk.gov.hmcts.payment.api.model.PaymentFeeLink;
 import uk.gov.hmcts.payment.api.service.PaymentGroupService;
 import uk.gov.hmcts.payment.api.service.PaymentService;
@@ -19,6 +21,7 @@ import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentGroupNotFoundExceptio
 import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentNotFoundException;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -32,6 +35,9 @@ public class CaseController {
     private final PaymentGroupService<PaymentFeeLink, String> paymentGroupService;
     private final PaymentDtoMapper paymentDtoMapper;
     private final PaymentGroupDtoMapper paymentGroupDtoMapper;
+
+    @Autowired
+    private CaseDetailsService caseDetailsService;
 
     @Autowired
     public CaseController(PaymentService<PaymentFeeLink, String> paymentService, PaymentGroupService paymentGroupService,
@@ -81,6 +87,28 @@ public class CaseController {
             .map(paymentGroupDtoMapper::toPaymentGroupDto)
             .collect(Collectors.toList());
 
+        if(paymentGroups == null || paymentGroups.isEmpty()) {
+            throw new PaymentGroupNotFoundException();
+        }
+
+        return new PaymentGroupResponse(paymentGroups);
+    }
+
+    @ApiOperation(value = "Get payment groups for a case", notes = "Get payment groups for a case")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Payment Groups retrieved"),
+        @ApiResponse(code = 400, message = "Bad request"),
+        @ApiResponse(code = 403, message = "Payment Info Forbidden"),
+        @ApiResponse(code = 404, message = "Payment Groups not found")
+    })
+    @RequestMapping(value = "/orderpoc/cases/{ccdcasenumber}/paymentgroups", method = GET)
+    public PaymentGroupResponse retrieveCasePaymentGroups_NewAPI(@PathVariable(name = "ccdcasenumber") String ccdCaseNumber) {
+
+        CaseDetails caseDetails = caseDetailsService.findByCcdCaseNumber(ccdCaseNumber);
+        Set<PaymentFeeLink> paymentFeeLinks  = caseDetails.getOrders();
+        List<PaymentGroupDto> paymentGroups = paymentFeeLinks.stream()
+                                                .map(paymentGroupDtoMapper::toPaymentGroupDto)
+                                                .collect(Collectors.toList());
         if(paymentGroups == null || paymentGroups.isEmpty()) {
             throw new PaymentGroupNotFoundException();
         }
