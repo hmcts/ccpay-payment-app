bbb77e9f1c3fd8d87cdad1d9e3273d017578331a
Anil Dhawale <anil.dhawale@hmcts.net>
2021-04-26 15:16:32 +0100

    PAY-3599 : IAC PBA Supplementary info

---
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/SupplementaryMainDto.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.payment.api.contract;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.payment.api.model.PaymentAllocationStatus;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Builder(builderMethodName = "supplementaryMainDtoWith")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SupplementaryMainDto {
+
+    @NotNull
+    private List<SupplementaryInfoDto> supplementaryInfo;
+
+    @NotNull
+    private MissingSupplementaryDetailsDto missingSupplementaryInfo;
+}
