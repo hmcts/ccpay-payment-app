414a96451a646f72b6499298517843fbcb6f8717
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-04-29 13:15:10 +0100

    optimized imports

--- api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/TelephonyPaymentRequest.java
+++ api-contract/src/main/java/uk/gov/hmcts/payment/api/contract/TelephonyPaymentRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 import lombok.experimental.Wither;
