4f90c5216a23133c9345e6788bb9dc7c2069e239
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-04-30 13:44:16 +0100

    Update PaymentBarPerformanceLiberataTest.java

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PaymentBarPerformanceLiberataTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PaymentBarPerformanceLiberataTest.java
@@ -22,10 +22,12 @@ import uk.gov.hmcts.payment.functional.config.TestConfigProperties;
 import uk.gov.hmcts.payment.functional.dsl.PaymentsTestDsl;
 import uk.gov.hmcts.payment.functional.idam.IdamService;
 import uk.gov.hmcts.payment.functional.s2s.S2sTokenService;
+
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.HashSet;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static uk.gov.hmcts.payment.functional.idam.IdamService.CMC_CITIZEN_GROUP;
@@ -67,7 +69,7 @@ public class PaymentBarPerformanceLiberataTest {
     @Test
     public void createPaymentRecordAndValidateSearchResults() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
-        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(3).toDate());
+        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(30).toDate());
 
         dsl.given().userToken(USER_TOKEN)
             .s2sToken(SERVICE_TOKEN)
@@ -79,7 +81,7 @@ public class PaymentBarPerformanceLiberataTest {
         // search payment and assert the result
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -165,7 +167,7 @@ public class PaymentBarPerformanceLiberataTest {
     @Test
     public void createBarPostalOrderPaymentRecordAndValidateSearchResults() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
-        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(3).toDate());
+        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(30).toDate());
 
         dsl.given().userToken(USER_TOKEN)
             .s2sToken(SERVICE_TOKEN)
@@ -267,7 +269,7 @@ public class PaymentBarPerformanceLiberataTest {
     @Test
     public void createBarChequePaymentRecordAndValidateSearchResults() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
-        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(3).toDate());
+        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(30).toDate());
 
         dsl.given().userToken(USER_TOKEN)
             .s2sToken(SERVICE_TOKEN)
@@ -369,7 +371,7 @@ public class PaymentBarPerformanceLiberataTest {
     @Test
     public void createBarCardPaymentRecordAndValidateSearchResults() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
-        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(3).toDate());
+        String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusSeconds(30).toDate());
 
         dsl.given().userToken(USER_TOKEN)
             .s2sToken(SERVICE_TOKEN)
