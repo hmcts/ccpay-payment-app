a3f8c0da36c5648c2140415fae59ed62685e6328
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-04-30 16:47:27 +0100

    Unit test cases for CaseController

--- api/src/test/java/uk/gov/hmcts/payment/api/controllers/CaseControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/CaseControllerTest.java
@@ -6,6 +6,7 @@ import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,12 +22,17 @@ import uk.gov.hmcts.payment.api.componenttests.util.PaymentsDataUtil;
 import uk.gov.hmcts.payment.api.contract.FeeDto;
 import uk.gov.hmcts.payment.api.contract.PaymentDto;
 import uk.gov.hmcts.payment.api.contract.PaymentsResponse;
+import uk.gov.hmcts.payment.api.domain.service.CaseDetailsDomainService;
+import uk.gov.hmcts.payment.api.domain.service.FeeDomainService;
+import uk.gov.hmcts.payment.api.domain.service.PaymentDomainService;
 import uk.gov.hmcts.payment.api.dto.PaymentGroupDto;
 import uk.gov.hmcts.payment.api.dto.PaymentGroupResponse;
 import uk.gov.hmcts.payment.api.dto.RemissionRequest;
+import uk.gov.hmcts.payment.api.exception.CaseDetailsNotFoundException;
 import uk.gov.hmcts.payment.api.model.*;
 import uk.gov.hmcts.payment.api.reports.FeesService;
 import uk.gov.hmcts.payment.api.service.ReferenceDataServiceImpl;
+import uk.gov.hmcts.payment.api.util.DateUtil;
 import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.ServiceResolverBackdoor;
 import uk.gov.hmcts.payment.api.v1.componenttests.backdoors.UserResolverBackdoor;
 import uk.gov.hmcts.payment.api.v1.componenttests.sugar.RestActions;
@@ -35,13 +41,18 @@ import uk.gov.hmcts.payment.referencedata.service.SiteService;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -91,6 +102,15 @@ public class CaseControllerTest extends PaymentsDataUtil {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private CaseDetailsDomainService caseDetailsDomainService;
+
+    @MockBean
+    private FeeDomainService feeDomainService;
+
+    @MockBean
+    private PaymentDomainService paymentDomainService;
 
     RestActions restActions;
 
@@ -573,5 +593,178 @@ public class CaseControllerTest extends PaymentsDataUtil {
             .andExpect(status().isNotFound())
             .andReturn();
     }
+
+    @Test
+    public void testGettingPaymentGroupDetailsWithValidCcdCaseNumber() throws Exception {
+
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenReturn(getCaseDetails());
+
+        when(feeDomainService.getFeePayApportionsByFee(Mockito.any(PaymentFee.class))).thenReturn(Arrays.asList(getFeePayApportion()));
+
+        when(paymentDomainService.getPaymentByApportionment(Mockito.any(FeePayApportion.class))).thenReturn(getPayment());
+
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108455502/paymentgroups")
+            .andExpect(status().isOk())
+            .andReturn();
+        PaymentGroupResponse paymentGroupResponse = objectMapper.readValue(result2.getResponse().getContentAsString(),PaymentGroupResponse.class);
+
+        BigDecimal actualAmount = paymentGroupResponse.getPaymentGroups().get(0).getPayments().get(0).getAmount();
+        String actualFeeCode = paymentGroupResponse.getPaymentGroups().get(0).getFees().get(0).getCode();
+
+        assertEquals(BigDecimal.valueOf(99.99) ,actualAmount);
+        assertEquals("FEE0001",actualFeeCode);
+    }
+
+    @Test
+    public void testGettingPaymentGroupDetailsWithInValidCcdCaseNumberSendsBadRequest() throws Exception {
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/11/paymentgroups")
+            .andExpect(status().isBadRequest())
+            .andReturn();
+    }
+
+    @Test
+    public void testGettingPaymentGroupWithUnavailableCcdCaseNumber_ThrowCaseDetailsNotFoundException() throws Exception {
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenThrow(new CaseDetailsNotFoundException("Case Details Not found for 1607065108000002"));
+
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108000002/paymentgroups")
+            .andExpect(status().isNotFound())
+            .andReturn();
+    }
+
+    @Test
+    public void  testGettingEmptyPaymentGroupThrowPaymentNotFoundException() throws Exception{
+        CaseDetails caseDetails = getCaseDetails();
+        caseDetails.setOrders(Collections.<PaymentFeeLink>emptySet());
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenReturn(caseDetails);
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108000002/paymentgroups")
+            .andExpect(status().isNotFound())
+            .andReturn();
+    }
+
+    @Test
+    public void testGettingPaymentDetailsWithInValidCcdCaseNumberSendsBadRequest() throws Exception {
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/11/paymentgroups")
+            .andExpect(status().isBadRequest())
+            .andReturn();
+    }
+
+    @Test
+    public void testGettingPaymentWithUnavailableCcdCaseNumber_ThrowCaseDetailsNotFoundException() throws Exception {
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenThrow(new CaseDetailsNotFoundException("Case Details Not found for 1607065108000002"));
+
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108000002/paymentgroups")
+            .andExpect(status().isNotFound())
+            .andReturn();
+    }
+
+    @Test
+    public void  testGettingEmptyPaymentThrowPaymentNotFoundException() throws Exception{
+        CaseDetails caseDetails = getCaseDetails();
+        caseDetails.setOrders(Collections.<PaymentFeeLink>emptySet());
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenReturn(caseDetails);
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108000002/payments")
+            .andExpect(status().isNotFound())
+            .andReturn();
+    }
+
+    @Test
+    public void testGettingPaymentDetailsWithValidCcdCaseNumber() throws Exception {
+
+        when(caseDetailsDomainService.findByCcdCaseNumber(anyString())).thenReturn(getCaseDetails());
+
+        when(feeDomainService.getFeePayApportionsByFee(Mockito.any(PaymentFee.class))).thenReturn(Arrays.asList(getFeePayApportion()));
+
+        when(paymentDomainService.getPaymentByApportionment(Mockito.any(FeePayApportion.class))).thenReturn(getPayment());
+
+        MvcResult result2 = restActions
+            .withAuthorizedUser(USER_ID)
+            .withUserId(USER_ID)
+            .get("/orderpoc/cases/1607065108455502/payments")
+            .andExpect(status().isOk())
+            .andReturn();
+        PaymentsResponse paymentsResponse = objectMapper.readValue(result2.getResponse().getContentAsString(),PaymentsResponse.class);
+
+        BigDecimal actualAmount = paymentsResponse.getPayments().get(0).getAmount();
+        String actualCaseReference = paymentsResponse.getPayments().get(0).getCaseReference();
+
+        assertEquals(BigDecimal.valueOf(99.99) ,actualAmount);
+        assertEquals("Reference1",actualCaseReference);
+    }
+
+
+    private FeePayApportion getFeePayApportion(){
+        return FeePayApportion.feePayApportionWith()
+            .apportionAmount(new BigDecimal("99.99"))
+            .apportionType("AUTO")
+            .feeId(1)
+            .paymentId(1)
+            .feeAmount(new BigDecimal("99.99"))
+            .paymentId(1)
+            .paymentLink(getPaymentFeeLink())
+            .build();
+    }
+
+    private PaymentFeeLink getPaymentFeeLink(){
+        return PaymentFeeLink.paymentFeeLinkWith()
+            .id(1)
+            .orgId("org-id")
+            .enterpriseServiceName("enterprise-service-name")
+            .paymentReference("payment-ref")
+            .ccdCaseNumber("1607065108455502")
+            .fees(Arrays.asList(PaymentFee.feeWith().calculatedAmount(new BigDecimal("99.99")).version("1").code("FEE0001").volume(1).build()))
+            .build();
+    }
+
+    private CaseDetails getCaseDetails(){
+        return CaseDetails.caseDetailsWith()
+            .id(1)
+            .ccdCaseNumber("1607065108455502")
+            .dateCreated(DateUtil.convertToDateViaInstant(LocalDateTime.now()))
+            .dateUpdated(DateUtil.convertToDateViaInstant(LocalDateTime.now()))
+            .orders(Arrays.asList(getPaymentFeeLink()).stream().collect(Collectors.toSet()))
+            .build();
+    }
+
+    private Payment getPayment(){
+        return Payment.paymentWith()
+            .id(1)
+            .amount(new BigDecimal("99.99"))
+            .caseReference("Reference1")
+            .ccdCaseNumber("1607065108455502")
+            .description("Test payments statuses  ")
+            .serviceType("PROBATE")
+            .currency("GBP")
+            .siteId("AA09")
+            .userId(USER_ID)
+            .paymentChannel(PaymentChannel.paymentChannelWith().name("online").build())
+            .paymentMethod(PaymentMethod.paymentMethodWith().name("card").build())
+            .paymentProvider(PaymentProvider.paymentProviderWith().name("gov pay").build())
+            .paymentStatus(PaymentStatus.paymentStatusWith().name("created").build())
+            .externalReference("e2kkddts5215h9qqoeuth5c0v")
+            .reference("RC-1519-9028-2432-0001")
+            .build();
+    }
+
 
 }
