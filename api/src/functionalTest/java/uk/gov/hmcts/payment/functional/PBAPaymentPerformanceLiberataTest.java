bd06e2aa73084caefeb02e5f587ab8e55c65c3de
Anil Dhawale <anil.dhawale@hmcts.net>
2021-05-06 17:32:22 +0100

    functional test cases  fix

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PBAPaymentPerformanceLiberataTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PBAPaymentPerformanceLiberataTest.java
@@ -608,8 +608,12 @@ public class PBAPaymentPerformanceLiberataTest {
         PaymentsResponse liberataResponseOld = paymentTestService.getLiberatePullPaymentsByStartAndEndDate(SERVICE_TOKEN, startDate,endDate, 30L)
             .statusCode(OK.value()).extract().as(PaymentsResponse.class);
 
+        LOG.info("liberataResponseOld size-----> : {}", liberataResponseOld.getPayments().size());
+
         PaymentsResponse liberataResponseApproach1 = paymentTestService.getLiberatePullPaymentsByStartAndEndDateApproach1(SERVICE_TOKEN, startDate,endDate, 30L)
             .statusCode(OK.value()).extract().as(PaymentsResponse.class);
+
+        LOG.info("liberataResponseApproach1 size-----> : {}", liberataResponseApproach1.getPayments().size());
 
         //Comparing the response size of old and new approach
         assertThat(liberataResponseOld.getPayments().size()).isGreaterThanOrEqualTo(PaymentCount);
@@ -617,6 +621,9 @@ public class PBAPaymentPerformanceLiberataTest {
 
         //Comparing the response of old and new approach
         Boolean compareResult = new HashSet<>(liberataResponseOld.getPayments()).equals(new HashSet<>(liberataResponseApproach1.getPayments()));
+
+        LOG.info("compareResult -----> : {}", compareResult);
+
         assertThat(compareResult).isEqualTo(true);
         LOG.info("Comparison of old and new api end point response for 5 PBA payment is same");
 
