d5097f5d2cb3d9ad85a5c3269156a6d5de02db6e
Anil Dhawale <anil.dhawale@hmcts.net>
2021-05-18 14:46:54 +0100

    Code Refactoring

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.payment.api.configuration;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -9,6 +10,13 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
+
+    @Value("${iac.connect.timeout}")
+    private String iacConnectTimeout;
+
+    @Value("${iac.read.timeout}")
+    private String iacReadTimeout;
+
     @Bean(name = {"paymentsHttpClient", "serviceTokenParserHttpClient", "userTokenParserHttpClient"})
     public CloseableHttpClient paymentsHttpClient() {
         return HttpClients.custom()
@@ -24,8 +32,8 @@ public class RestTemplateConfiguration {
     @Bean (value = "restTemplateIacSupplementaryInfo")
     public RestTemplate restTemplateIacSupplementaryInfo() {
         var factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(10000);
+        factory.setConnectTimeout(Integer.parseInt(iacConnectTimeout));
+        factory.setReadTimeout((Integer.parseInt(iacReadTimeout)));
         return new RestTemplate(factory);
     }
 }
