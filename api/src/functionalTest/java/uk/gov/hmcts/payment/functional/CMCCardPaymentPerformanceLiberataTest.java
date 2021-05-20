0c591ce9f077a6ff4f583dabd83b335ab7118f50
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-05-20 15:07:43 +0100

    Update CMCCardPaymentPerformanceLiberataTest.java

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CMCCardPaymentPerformanceLiberataTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/CMCCardPaymentPerformanceLiberataTest.java
@@ -454,7 +454,7 @@ public class CMCCardPaymentPerformanceLiberataTest {
         String startDate = formatter.format(LocalDateTime.now(zoneUTC).minusMinutes(5).toDate());
 
         for(int i=0; i<PaymentCount;i++) {
-            cardPaymentRequest[i] = PaymentFixture.cardPaymentRequestall("455.00", "Divorce");
+            cardPaymentRequest[i] = PaymentFixture.cardPaymentRequestall("455.00", "DIVORCE");
 
             paymentTestService.postcardPayment(USER_TOKEN, SERVICE_TOKEN, cardPaymentRequest[i])
                 .then()
