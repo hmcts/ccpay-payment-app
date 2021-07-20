75b4be0120a62524de66b46da13ea37384f27b73
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-07-20 07:27:44 +0100

    Update BarPaymentFunctionalTest.java

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/BarPaymentFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/BarPaymentFunctionalTest.java
@@ -103,7 +103,7 @@ public class BarPaymentFunctionalTest {
             assertThat(paymentsResponse.getPayments().get(0).getReportedDateOffline()).isNotNull();
             assertThat(paymentsResponse.getPayments().get(0).getGiroSlipNo()).isEqualTo("12345");
             FeeDto feeDto = paymentsResponse.getPayments().get(0).getFees().get(0);
-            assertThat(feeDto.getCode()).isEqualTo("FEE0002");
+            assertThat(feeDto.getCode()).isEqualTo("FEE0333");
             assertThat(feeDto.getVersion()).isEqualTo("4");
             assertThat(feeDto.getCalculatedAmount()).isEqualTo(new BigDecimal("550.00"));
             assertThat(feeDto.getReference()).isNotNull();
@@ -128,7 +128,7 @@ public class BarPaymentFunctionalTest {
                 Arrays.asList(
                     FeeDto.feeDtoWith()
                         .calculatedAmount(new BigDecimal("550.00"))
-                        .code("FEE0002")
+                        .code("FEE0333")
                         .version("4")
                         .volume(1)
                         .reference("REF_123")
