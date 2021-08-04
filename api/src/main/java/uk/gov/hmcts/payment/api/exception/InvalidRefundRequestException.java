a8f148b988f9e067c2bfc234c14a7dc8db078336
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-08-04 12:01:48 +0100

    Update InvalidRefundRequestException.java

--- api/src/main/java/uk/gov/hmcts/payment/api/exception/InvalidRefundRequestException.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/exception/InvalidRefundRequestException.java
@@ -1,10 +1,7 @@
 package uk.gov.hmcts.payment.api.exception;
 
 public class InvalidRefundRequestException extends RuntimeException {
-
-    public InvalidRefundRequestException() {
-    }
-
+    
     public InvalidRefundRequestException(String message) {
         super(message);
     }
