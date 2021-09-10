666a37eff11a1c4af061d68f26e511eb23df01b4
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2021-09-10 18:14:11 +0100

    PAY-5180- New Payhub Api to update remission details

---
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/ResubmitRefundRemissionRequest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.payment.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@With
+@JsonInclude(NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "resubmitRefundRemissionRequestWith")
+public class ResubmitRefundRemissionRequest {
+
+    @NotNull
+    private String refundReason;
+
+    @NotNull
+    @Positive
+    @Digits(integer = 10, fraction = 2, message = "Payment amount cannot have more than 2 decimal places")
+    private BigDecimal amount;
+
+}
--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsService.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsService.java
@@ -5,10 +5,14 @@ import org.springframework.util.MultiValueMap;
 import uk.gov.hmcts.payment.api.dto.PaymentRefundRequest;
 import uk.gov.hmcts.payment.api.dto.RefundResponse;
 
+import java.math.BigDecimal;
+
 public interface PaymentRefundsService {
 
     ResponseEntity<RefundResponse> createRefund(PaymentRefundRequest paymentRefundRequest, MultiValueMap<String, String> headers);
 
     ResponseEntity<RefundResponse> createAndValidateRetroSpectiveRemissionRequest(String remissionReference, MultiValueMap<String, String> headers);
+
+    ResponseEntity updateTheRemissionAmount(String paymentReference, BigDecimal amount, String refundReason);
 
 }
