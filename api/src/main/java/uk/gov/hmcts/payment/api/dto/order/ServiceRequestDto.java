7ed0e7c38e0cbccefe7bbeac54d18a16ff870f64
priyatham-bhikkumalla <priyatham.bhikkumalla@hmcts.net>
2021-09-20 15:22:15 +0100

    PAY-5075
    fees validation fix

--- api/src/main/java/uk/gov/hmcts/payment/api/dto/order/ServiceRequestDto.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/order/ServiceRequestDto.java
@@ -3,18 +3,11 @@ package uk.gov.hmcts.payment.api.dto.order;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import uk.gov.hmcts.payment.api.dto.CasePaymentRequest;
 
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,7 +28,7 @@ public class ServiceRequestDto {
     private String ccdCaseNumber;
 
     @NotEmpty
-    private List<ServiceRequestFeeDto> fees;
+    private List<@Valid ServiceRequestFeeDto> fees;
 
     @NotNull
     private CasePaymentRequest casePaymentRequest;
@@ -52,8 +45,12 @@ public class ServiceRequestDto {
     @AssertFalse(message = "Fee code cannot be duplicated")
     private boolean isFeeCodeUnique() {
         Set<String> unique = new HashSet<>();
+
+        if(fees!=null)
         return fees.stream()
             .anyMatch(p -> !unique.add(p.getCode()));
+
+        else return false;
     }
 
 }
