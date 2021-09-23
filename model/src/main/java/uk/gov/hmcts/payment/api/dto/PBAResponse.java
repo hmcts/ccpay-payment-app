0311366a2ba61f390829c62af986bf6c8faf3c1f
aravindan89 <aravindan.ranjan@hmcts.net>
2021-09-23 13:37:22 +0100

    modified response object

--- model/src/main/java/uk/gov/hmcts/payment/api/dto/PBAResponse.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/dto/PBAResponse.java
@@ -1,36 +1,54 @@
 package uk.gov.hmcts.payment.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
 
 import java.util.List;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
 @Builder(builderMethodName = "pbaDtoWith")
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
 public class PBAResponse{
-    public OrganisationEntityResponse organisationEntityResponse;
+    private OrganisationEntityResponse organisationEntityResponse;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    @JsonInclude(NON_NULL)
+    public static class OrganisationEntityResponse{
+        public String name;
+        public String organisationIdentifier;
+        public String status;
+        public String sraId;
+        public boolean sraRegulated;
+        public String companyNumber;
+        public String companyUrl;
+        public SuperUser superUser;
+        public List<String> paymentAccount;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    @JsonInclude(NON_NULL)
+    public static class SuperUser{
+        public String firstName;
+        public String lastName;
+        public String email;
+    }
 }
 
-class SuperUser{
-    public String firstName;
-    public String lastName;
-    public String email;
-}
-class OrganisationEntityResponse{
-    public String name;
-    public String organisationIdentifier;
-    public String status;
-    public String sraId;
-    public boolean sraRegulated;
-    public String companyNumber;
-    public String companyUrl;
-    public SuperUser superUser;
-    public List<String> paymentAccount;
-}
+
+
+
 
 
