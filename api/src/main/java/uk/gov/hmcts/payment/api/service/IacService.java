02cb674b150e47cec24aa5be3eb0322164b04034
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2021-11-23 09:55:25 +0000

    Update IacService.java

--- api/src/main/java/uk/gov/hmcts/payment/api/service/IacService.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/IacService.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.payment.api.service;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.payment.api.contract.PaymentDto;
 import uk.gov.hmcts.payment.api.dto.SupplementaryPaymentDto;
 import java.util.List;
-
+@Service
 public interface IacService {
     ResponseEntity<SupplementaryPaymentDto> getIacSupplementaryInfo(List<PaymentDto> paymentDtos, String serviceName);
 }
