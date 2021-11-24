419994dd41075269c1ad34d45314e012e15eb9ee
kaushikmaitra <kaushik.maitra@hmcts.net>
2021-11-24 16:52:14 +0000

    test commit

--- api/src/test/java/uk/gov/hmcts/payment/api/domain/service/ServiceRequestDomainServiceTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/domain/service/ServiceRequestDomainServiceTest.java
@@ -254,7 +254,6 @@ public class ServiceRequestDomainServiceTest {
          when(accountService.retrieve(any())).thenReturn(accountDto);
 
          serviceRequestDomainService.addPayments(getPaymentFeeLink(),"123",serviceRequestPaymentDto);
-
          serviceRequestDomainService.addPayments(getPaymentFeeLink(),"123", serviceRequestPaymentDto);
 
      }
