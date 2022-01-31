40f3873c4d2b7b18eb8a45dd42df36938840f5dc
sinanghmcts <sinan.goktas@hmcts.net>
2022-01-31 17:19:59 +0000

    functional test fix - negative scenario bulk scan

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/RefundRequestorJourneyBulkscanPaymentFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/RefundRequestorJourneyBulkscanPaymentFunctionalTest.java
@@ -178,6 +178,7 @@ public class RefundRequestorJourneyBulkscanPaymentFunctionalTest {
 
     @Test
     public void negative_givenAFeeInPG_WhenABulkScanPaymentNeedsMappingthenPaymentShouldBeAddedToExistingGroup_under_lag_time() throws Exception {
+
         String[] paymentMethod = {"CHEQUE", "POSTAL_ORDER", "CASH"};
         String[] lag_time = {"15", "15", "3"};
 
@@ -238,20 +239,6 @@ public class RefundRequestorJourneyBulkscanPaymentFunctionalTest {
 
                             paymentReference.set(paymentDto.getReference());
 
-
-                            paymentTestService.updateThePaymentDateByCcdCaseNumberForCertainHours(USER_TOKEN, SERVICE_TOKEN,
-                                ccdCaseNumber, "15");
-
-                            // initiate the refund
-                            PaymentRefundRequest paymentRefundRequest
-                                = PaymentFixture.aRefundRequest("RR001", paymentReference.get());
-                            Response refundResponse = paymentTestService.postInitiateRefund(USER_TOKEN_PAYMENTS_REFUND_REQUESTOR_ROLE,
-                                SERVICE_TOKEN_PAYMENT,
-                                paymentRefundRequest);
-
-                            System.out.println(refundResponse.getBody().prettyPrint());
-                            assertThat(refundResponse.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-                            assertThat(refundResponse.getBody().asString()).isEqualTo("This payment is not yet eligible for refund");
                         });
 
                 });
@@ -260,6 +247,7 @@ public class RefundRequestorJourneyBulkscanPaymentFunctionalTest {
                 ccdCaseNumber, lag_time[i]);
             System.out.println(rollbackPaymentResponse.getBody().prettyPrint());
 
+            // initiate the refund
             PaymentRefundRequest paymentRefundRequest
                 = PaymentFixture.aRefundRequest("RR001", paymentReference.get());
             Response refundResponse = paymentTestService.postInitiateRefund(USER_TOKEN_PAYMENTS_REFUND_REQUESTOR_ROLE,
