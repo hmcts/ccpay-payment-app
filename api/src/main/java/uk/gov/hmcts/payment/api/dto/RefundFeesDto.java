b85bb4c25151c004c6ad1efedaf82970079c6a93
Anooj Kurup <anooj.kurup@hmcts.net>
2022-02-15 20:55:44 +0000

    PAY-5422 - updated RefundRequestDto with list of RefundFeesDto for partial refund functionality

---
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/RefundFeesDto.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.payment.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "refundFeesDtoWith")
+public class RefundFeesDto {
+
+    private Integer id;
+    private String code;
+    private String version;
+    private Integer volume;
+    private BigDecimal refundAmount;
+
+}
