5c3095b98159385f3c2f2a448cff37b08760fb6a
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-04-08 09:47:42 +0100

    updated

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -85,7 +85,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
         Payment payment = paymentRepository.findByReference(paymentRefundRequest.getPaymentReference()).orElseThrow(PaymentNotFoundException::new);
 
 
-        validateRefund(paymentRefundRequest,payment.getPaymentLink().getFees());
+        //validateRefund(paymentRefundRequest,payment.getPaymentLink().getFees());
 
         validateThePaymentBeforeInitiatingRefund(payment,headers);
 
@@ -402,7 +402,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
         headerMultiValueMap.put("ServiceAuthorization", Collections.singletonList(serviceAuthorisation));
 
         HttpHeaders httpHeaders = new HttpHeaders(headerMultiValueMap);
-        
+
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
 
