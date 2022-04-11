8d69a59fe467e19c4b5cb6a43c8ca5653b6b5979
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-04-11 10:42:07 +0100

    updated

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -294,9 +294,12 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
                         refundListDtoResponse.getRefundList().forEach(refundDto -> {
 
+
                             if(refundDto.getPaymentReference().equals(paymentDto.getPaymentReference())
-                                && (refundDto.getRefundStatus().getName().equals("Accepted") || refundDto.getRefundStatus().getName().equals("Approved")))
+                                && (refundDto.getRefundStatus().getName().equals("Accepted") || refundDto.getRefundStatus().getName().equals("Approved"))) {
                                 lambdaContext.refundAmount = lambdaContext.refundAmount.add(refundDto.getAmount());
+                                paymentDto.setOverPayment(BigDecimal.ZERO);
+                            }
 
                             //When there is no available balance
                             //Then ISSUE REFUND/ADD REMISSION/ADD REFUND option should not be available
