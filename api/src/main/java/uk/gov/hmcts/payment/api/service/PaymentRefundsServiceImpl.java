53656ba28b67cd7ad32e2623e10b7472d2fd4c47
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-04-12 10:37:30 +0100

    updated

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -310,6 +310,11 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
                                 paymentDto.setOverPayment(BigDecimal.ZERO);
                             }
 
+
+                            if(refundDto.getPaymentReference().equals(paymentDto.getPaymentReference())) {
+                                paymentDto.setOverPayment(BigDecimal.ZERO);
+                            }
+
                             //When there is no available balance
                             //Then ISSUE REFUND/ADD REMISSION/ADD REFUND option should not be available
 
@@ -362,8 +367,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
                         refundListDtoResponse.getRefundList().forEach(refundDto -> {
 
-                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber())
-                                && (refundDto.getRefundStatus().getName().equals("Accepted") || refundDto.getRefundStatus().getName().equals("Approved"))) {
+                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber())) {
                                 feeDto.setOverPayment(BigDecimal.ZERO);
                             }
                         });
