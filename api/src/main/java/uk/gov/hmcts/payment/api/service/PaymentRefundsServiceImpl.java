bcc33e1aa5706f58850d60b967a46be73424376b
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-04-13 03:09:04 +0100

    fixed null pointer

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -230,7 +230,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
                 totalPaymentAmount = totalPaymentAmount.add(payment.getAmount());
 
                 //if theres a refund available for this payment, get the total refund amount
-                if (refundListDtoResponse.getRefundList() != null) {
+                if (refundListDtoResponse != null) {
                     for (RefundDto refundDto : refundListDtoResponse.getRefundList()) {
 
                         //Condition to check that a valid refund corresponding with the payment reference is considered only
@@ -269,7 +269,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
     public List<String> getAllRefundedFeeIds(RefundListDtoResponse refundListDtoResponse){
         List<String> refundedFees = new ArrayList<String>();
 
-        if(refundListDtoResponse.getRefundList() !=null) {
+        if(refundListDtoResponse !=null) {
             for (RefundDto refundDto : refundListDtoResponse.getRefundList()) {
                 if (refundDto.getReason().equals("Retrospective remission")) {
                     refundedFees = Arrays.asList(refundDto.getFeeIds().split(","));
@@ -302,9 +302,9 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
 
             //When there is no refund found for the ccdcasenumber, create an empty refundList
-            if(refundListDtoResponse ==null){
-                refundListDtoResponse = new RefundListDtoResponse();
-            }
+//            if(refundListDtoResponse ==null){
+//                refundListDtoResponse = new RefundListDtoResponse();
+//            }
 
             for(PaymentGroupDto paymentGroupDto : paymentGroupResponse.getPaymentGroups()){
 
