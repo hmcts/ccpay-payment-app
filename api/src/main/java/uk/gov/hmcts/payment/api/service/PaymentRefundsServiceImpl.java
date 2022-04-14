2af3c06256091acf6b2e4cb0cd44bfa18284f1ca
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-04-14 12:02:31 +0100

    updated

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -374,7 +374,8 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
                         refundListDtoResponse.getRefundList().forEach(refundDto -> {
 
-                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber())) {
+                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber()) &&
+                                refundDto.getFeeIds().equals(feeDto.getId())) {
                                 feeDto.setOverPayment(BigDecimal.ZERO);
                             }
                         });
@@ -741,7 +742,8 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
                         refundListDtoResponse.getRefundList().forEach(refundDto -> {
 
-                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber())) {
+                            if(refundDto.getCcdCaseNumber().equals(feeDto.getCcdCaseNumber()) &&
+                                refundDto.getFeeIds().equals(feeDto.getId())) {
                                 feeDto.setOverPayment(BigDecimal.ZERO);
                             }
                         });
@@ -751,7 +753,7 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
                         refundListDtoResponse.getRefundList().forEach(refundDto -> {
 
-                            if(refundDto.getCcdCaseNumber().equals(paymentDto1.getCcdCaseNumber())) {
+                            if(refundDto.getPaymentReference().equals(paymentDto1.getReference())) {
                                 paymentDto.setOverPayment(BigDecimal.ZERO);
                             }
                         });
