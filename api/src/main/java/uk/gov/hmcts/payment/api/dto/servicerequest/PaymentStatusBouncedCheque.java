37700d13f47514b4b80bf199bfbae9bf13d9c550
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2022-05-19 13:14:10 +0100

    api gate way testng

---
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/servicerequest/PaymentStatusBouncedCheque.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.payment.api.dto.servicerequest;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "paymentStatusBouncedChequeRequestWith")
+public class PaymentStatusBouncedCheque {
+
+    private String paymentReference;
+
+    private String failureReference;
+
+    private String ccdCaseNumber;
+
+    private String reason;
+
+    private BigDecimal amount;
+
+    private String additionalReference;
+    private String event_date_time;
+}
