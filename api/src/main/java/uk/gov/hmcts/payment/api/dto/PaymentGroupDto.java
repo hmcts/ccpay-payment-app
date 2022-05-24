aa6969c36902d296d1a5bb54fb11468170de21d3
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-05-24 02:25:03 +0100

    Update PaymentGroupDto.java

--- api/src/main/java/uk/gov/hmcts/payment/api/dto/PaymentGroupDto.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/PaymentGroupDto.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.payment.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
@@ -16,7 +16,7 @@ import java.util.List;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(NON_NULL)
 @Builder(builderMethodName = "paymentGroupDtoWith")
 @AllArgsConstructor
