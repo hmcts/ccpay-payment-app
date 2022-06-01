92f4e4f4b6f09fbc7367c908b54c6f3e6935148e
“sayali-salunkhe” <sayali.salunkhe@HMCTS.net>
2022-06-01 10:49:13 +0100

    Removed commented code

--- api-contract/src/test/java/uk/gov/hmcts/payment/api/contract/PaymentDtoTest.java
+++ api-contract/src/test/java/uk/gov/hmcts/payment/api/contract/PaymentDtoTest.java
@@ -93,11 +93,6 @@ public class PaymentDtoTest {
         apportionedPayment = new BigDecimal(1);
         dateReceiptProcessed = currentDateTime.toDate();
 
-        /*feeWithVolumeDto = new FeeDto(1, feeWithVolumeCode, feeVersion, volume, calculatedAmountForFeeWithVolume,
-            feeAmount, memoLine, naturalAccountCode, null, null, null, jurisdiction1, jurisdiction2, feeDescription,caseReference,apportionAmount,allocatedAmount,dateApportioned,dateCreated,dateUpdated,amountDue,paymentGroupReference,apportionedPayment,dateReceiptProcessed,remissionEnable);
-
-        feeNoVolumeDto = new FeeDto(1, feeNoVolumeCode, feeVersion, volume, calculatedAmountForFeeNoVolume,
-            feeAmount, memoLine, naturalAccountCode, null, null, null, jurisdiction1, jurisdiction2, feeDescription,caseReference,apportionAmount,allocatedAmount,dateApportioned,dateCreated,dateUpdated,amountDue,paymentGroupReference,apportionedPayment,dateReceiptProcessed,remissionEnable);*/
     }
 
     @Before
@@ -138,12 +133,6 @@ public class PaymentDtoTest {
         refundEnable = true;
         remissionEnable=true;
 
-
-        /*testDto = new PaymentDto(id, amount, description, reference, dateCreated, dateUpdated,
-            gbp, ccdNumber, caseReference, paymentReference, channel, method, externalProvider,
-            status, externalReference, siteId, serviceName, customerReference, accountNumber,
-            organisationName, paymentGroupReference, reportedDateOffline,documentControlNumber,bankedDate,payerName,true,
-            null, statusHistories, paymentAllocations,giroSlipNo, internalReference,links);*/
     }
 
     @Test
