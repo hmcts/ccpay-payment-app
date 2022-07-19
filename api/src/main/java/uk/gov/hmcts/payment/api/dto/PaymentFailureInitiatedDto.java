5a4e584d21cd9f88318df8b5cb8fd7ab138a6430
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2022-07-19 12:50:13 +0100

    dto mapper change

--- api/src/main/java/uk/gov/hmcts/payment/api/dto/PaymentFailureInitiatedDto.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/PaymentFailureInitiatedDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -12,7 +13,7 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(NON_NULL)
-@Builder(builderMethodName = "paymentFailureInitiateResponseDtoWith")
+@SuperBuilder(builderMethodName = "paymentFailureInitiateResponseDtoWith")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
