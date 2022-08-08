166a583349775c742fe50518328ecfe8b65f99a6
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2022-08-08 13:22:55 +0100

    removed unwanted import

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentStatusController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/PaymentStatusController.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.payment.api.controllers;
 
-import com.microsoft.azure.servicebus.IMessageReceiver;
-import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +19,6 @@ import uk.gov.hmcts.payment.api.service.PaymentStatusUpdateService;
 import uk.gov.hmcts.payment.api.v1.model.exceptions.PaymentNotFoundException;
 
 import javax.validation.Valid;
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
