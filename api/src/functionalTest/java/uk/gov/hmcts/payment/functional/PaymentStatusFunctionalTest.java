64b59834b48b5da77f90784b67f106b52cb9abf0
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2022-08-16 13:38:03 +0100

    FT fix

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PaymentStatusFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/PaymentStatusFunctionalTest.java
@@ -308,7 +308,7 @@ public class PaymentStatusFunctionalTest {
                 paymentStatusUpdateSecond);
 
         assertEquals(ping2Response.getStatusCode(), OK.value());
-        assertEquals("Successful operation", ping2Response.getBody().prettyPrint());
+        assertEquals("successful operation", ping2Response.getBody().prettyPrint());
 
         // delete payment record
         paymentTestService.deletePayment(USER_TOKEN, SERVICE_TOKEN, paymentDto.getReference()).then().statusCode(NO_CONTENT.value());
@@ -398,7 +398,7 @@ public class PaymentStatusFunctionalTest {
                 paymentStatusUpdateSecond);
 
         assertEquals(ping2Response.getStatusCode(), OK.value());
-        assertEquals("Successful operation", ping2Response.getBody().prettyPrint());
+        assertEquals("successful operation", ping2Response.getBody().prettyPrint());
 
         // delete payment record
         paymentTestService.deletePayment(USER_TOKEN, SERVICE_TOKEN, paymentReference.get()).then().statusCode(NO_CONTENT.value());
