1e8f31cc648625de18333591d132090fef286585
“larslunde-hmcts” <lars.lunde@HMCTS.net>
2022-09-13 14:11:55 +0100

    added hashmap for servicetypes

--- api/src/main/java/uk/gov/hmcts/payment/api/reports/PaymentReportType.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/reports/PaymentReportType.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.payment.api.reports;
 
 import uk.gov.hmcts.payment.api.util.PaymentMethodType;
 
+import java.util.HashMap;
+
 public enum PaymentReportType {
 
     CARD,
@@ -17,7 +19,7 @@ public enum PaymentReportType {
     public static PaymentReportType from(PaymentMethodType paymentMethodType, String serviceType) {
         String value = "";
 
-        String serviceTypeEnum = getServiceTypeEnum(serviceType);
+        String serviceTypeEnum = getServiceTypeEnum2(serviceType);
 
         if (paymentMethodType != null && serviceTypeEnum != null) {
             value = String.join("_", paymentMethodType.name(), serviceTypeEnum);
@@ -61,5 +63,26 @@ public enum PaymentReportType {
             return "PRL";
         }
         return serviceType;
+    }
+    private static String getServiceTypeEnum2(String ServiceType) {
+        HashMap<String, String> serviceTypeHashMap = new HashMap<>();
+
+        serviceTypeHashMap.put("Specified Money Claims", "CMC");
+        serviceTypeHashMap.put("Civil Money Claims", "CMC");
+        serviceTypeHashMap.put("Financial Remedy", "FINREM");
+        serviceTypeHashMap.put("Finrem", "FINREM");
+        serviceTypeHashMap.put("Family Public Law", "FPL");
+        serviceTypeHashMap.put("Digital Bar", "DIGITAL_BAR");
+        serviceTypeHashMap.put("Divorce", "DIVORCE");
+        serviceTypeHashMap.put("Probate","PROBATE");
+        serviceTypeHashMap.put("Civil","CIVIL");
+        serviceTypeHashMap.put("Family Private Law","PRL");
+
+        if (ServiceType != null) {
+            return serviceTypeHashMap.get(ServiceType);
+        }
+
+//        Throw some error here?
+        return "invalid ServiceType";
     }
 }
