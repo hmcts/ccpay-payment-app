ed740866d54cf5aba87086bb7bb03942cd3739fd
Anooj Kurup <anooj.kurup@hmcts.net>
2022-09-16 15:37:39 +0100

    added civil and prl to configMap

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/PaymentReportConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/PaymentReportConfiguration.java
@@ -4,14 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.payment.api.reports.PaymentReportType;
-import uk.gov.hmcts.payment.api.reports.config.BarPaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.CardPaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PbaCmcPaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PbaDivorcePaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PbaFinremPaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PbaProbatePaymentReportConfig;
-import uk.gov.hmcts.payment.api.reports.config.PbaFplPaymentReportConfig;
+import uk.gov.hmcts.payment.api.reports.config.*;
 
 import java.util.Map;
 
@@ -25,7 +18,9 @@ public class PaymentReportConfiguration {
                                                                  PbaProbatePaymentReportConfig pbaProbatePaymentReportConfig,
                                                                  PbaFinremPaymentReportConfig pbaFinremPaymentReportConfig,
                                                                  PbaDivorcePaymentReportConfig pbaDivorcePaymentReportConfig,
-                                                                 PbaFplPaymentReportConfig pbaFplPaymentReportConfig) {
+                                                                 PbaFplPaymentReportConfig pbaFplPaymentReportConfig,
+                                                                 PbaCivilPaymentReportConfig pbaCivilPaymentReportConfig,
+                                                                 PbaPrlPaymentReportConfig pbaPrlPaymentReportConfig) {
         return ImmutableMap.<PaymentReportType, PaymentReportConfig>builder()
             .put(PaymentReportType.CARD, cardPaymentReportConfig)
             .put(PaymentReportType.DIGITAL_BAR, barPaymentReportConfig)
@@ -34,6 +29,8 @@ public class PaymentReportConfiguration {
             .put(PaymentReportType.PBA_FINREM, pbaFinremPaymentReportConfig)
             .put(PaymentReportType.PBA_DIVORCE, pbaDivorcePaymentReportConfig)
             .put(PaymentReportType.PBA_FPL, pbaFplPaymentReportConfig)
+            .put(PaymentReportType.PBA_CIVIL, pbaCivilPaymentReportConfig)
+            .put(PaymentReportType.PBA_PRL, pbaPrlPaymentReportConfig)
             .build();
 
     }
