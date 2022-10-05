76409860aecb712e76ea9debde3f25f89503d5de
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-10-05 10:49:10 +0100

    Update AccountNotFoundException.java

--- api/src/main/java/uk/gov/hmcts/payment/api/exception/AccountNotFoundException.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/exception/AccountNotFoundException.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.payment.api.exception;
 
 public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException (String message) {
+        System.out.print(message)
         super(message);
     }
 }
