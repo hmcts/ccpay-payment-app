4252638dc898d5a55ef69fd1d699d6f9fe3b8003
kaushikmaitra <kaushik.maitra@hmcts.net>
2022-12-16 12:22:59 +0000

    added log statements

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentStatusUpdateServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentStatusUpdateServiceImpl.java
@@ -244,12 +244,10 @@ public class PaymentStatusUpdateServiceImpl implements PaymentStatusUpdateServic
                 throw new PaymentNotFoundException("No Payment Failure available for the given Failure reference");
             } else {
                 Date pingTwoDate =  DateTime.parse(paymentStatusUpdateSecond.getRepresentmentDate()).withZone(DateTimeZone.UTC).toDate();
-                LOG.info("pingTwoDate Day{}", pingTwoDate.getDay());
+
                 LOG.info("pingTwoDate time{}", pingTwoDate.getTime());
-                LOG.info("pingTwoDate Date{}", pingTwoDate.getDate());
-                LOG.info("pingTwoDate hours{}", pingTwoDate.getHours());
-                LOG.info("pingTwoDate minutes{}", pingTwoDate.getMinutes());
-                LOG.info("pingTwoDate month{}", pingTwoDate.getMonth());
+                LOG.info("representation Date time{}", DateTime.parse(paymentStatusUpdateSecond.getRepresentmentDate()));
+                LOG.info("getFailureEventDateTime time{}", paymentFailure.get().getFailureEventDateTime().getTime());
                 if (pingTwoDate.before(paymentFailure.get().getFailureEventDateTime())){
                     throw new InvalidPaymentFailureRequestException("Representment date can not be prior to failure event date");
                 }
