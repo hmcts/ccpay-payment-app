49c648199a03ef3c4fa33507fac2bd3cee591ceb
Sayali Salunkhe <83658952+sayali-salunkhe@users.noreply.github.com>
2023-02-21 11:31:57 +0000

    Added @IgnoreNoPactsToVerify (#1361)
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>

--- api/src/contractTest/java/uk/gov/hmcts/payment/api/controllers/provider/AccountProviderTest.java
+++ api/src/contractTest/java/uk/gov/hmcts/payment/api/controllers/provider/AccountProviderTest.java
@@ -31,6 +31,7 @@ import static org.mockito.Mockito.when;
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}", host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:80}", consumerVersionSelectors = {
     @VersionSelector(tag = "master")})
 @Import(CreditAccountPaymentProviderTestConfiguration.class)
+@IgnoreNoPactsToVerify
 class AccountProviderTest {
 
     @Value("${PACT_BRANCH_NAME}")
