94f1c1ef6e1ebbb70a610f6576d1bc5cb11a7022
it.consulting@tutanota.com <it.consulting@tutanota.com>
2023-06-20 12:53:22 +0100

    PAY-5980: SPIKE: Duplicate PBA Payments -Idempotency Change      [PAY-5980] (https://tools.hmcts.net/jira/browse/PAY-5980).

--- api/src/test/java/uk/gov/hmcts/payment/api/controllers/ServiceRequestControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/ServiceRequestControllerTest.java
@@ -96,7 +96,7 @@ public class ServiceRequestControllerTest {
     @MockBean
     PaymentGroupDtoMapper paymentGroupDtoMapper;
 
-    @Autowired
+    @MockBean
     private IdempotencyService idempotencyService;
 
     @Autowired
@@ -259,6 +259,71 @@ public class ServiceRequestControllerTest {
             .andReturn();
     }
 
+    @Test
+    public void createPBAPaymentWithServiceRequestFailDueToConflict() throws Exception {
+
+        //Creation of serviceRequest-reference
+        String serviceRequestReferenceResult = getServiceRequestReference();
+
+        //ServiceRequest Payment DTO
+        ServiceRequestPaymentDto serviceRequestPaymentDto = ServiceRequestPaymentDto
+            .paymentDtoWith().accountNumber("PBAFUNC12345")
+            .amount(BigDecimal.valueOf(300))
+            .currency("GBP")
+            .idempotencyKey(UUID.randomUUID().toString())
+            .organisationName("sommin")
+            .customerReference("testCustReference").
+            build();
+
+
+        AccountDto liberataAccountResponse = AccountDto.accountDtoWith()
+            .accountNumber("PBAFUNC12345")
+            .accountName("CAERPHILLY COUNTY BOROUGH COUNCIL")
+            .creditLimit(BigDecimal.valueOf(28879))
+            .availableBalance(BigDecimal.valueOf(30000))
+            .status(AccountStatus.ACTIVE)
+            .build();
+
+        IdempotencyKeys idempotencyKeys = IdempotencyKeys.idempotencyKeysWith()
+            .responseCode(HttpStatus.CREATED.value()).build();
+
+        List<IdempotencyKeys> result = List.of(idempotencyKeys);
+        when(idempotencyService.findTheRecordByRequestHashcode(any())).thenReturn(result);
+        when(accountService.retrieve("PBAFUNC12345")).thenReturn(liberataAccountResponse);
+
+
+
+        ServiceRequestPaymentBo serviceRequestPaymentBo = ServiceRequestPaymentBo.serviceRequestPaymentBoWith().
+            paymentReference("RC-reference").
+            dateCreated("20-09-2021").
+            status("success").
+            build();
+
+        ResponseEntity<ServiceRequestPaymentBo> responseEntity =
+            new ResponseEntity<>(objectMapper.readValue("{\"response_body\":\"response_body\"}", ServiceRequestPaymentBo.class), HttpStatus.CREATED);
+
+        ResponseEntity<ServiceRequestPaymentBo> responseEntity2 =
+            new ResponseEntity<>(objectMapper.readValue("{\"response_body\":\"response_body\"}", ServiceRequestPaymentBo.class), HttpStatus.CONFLICT);
+
+        ResponseEntity<ServiceRequestPaymentBo> responseEntity3 =
+            new ResponseEntity<>(objectMapper.readValue("{\"response_body\":\"response_body\"}", ServiceRequestPaymentBo.class), HttpStatus.PRECONDITION_FAILED);
+
+        when(serviceRequestDomainService.addPayments(any(),any(),any())).thenReturn(serviceRequestPaymentBo);
+
+        when(serviceRequestDomainService.createIdempotencyRecord(any(),any(),any(),any(),any(),any())).
+            thenReturn(responseEntity, responseEntity, responseEntity2,responseEntity3);
+
+        ServiceRequestResponseDto serviceRequestResponseDtoSample = ServiceRequestResponseDto.serviceRequestResponseDtoWith()
+            .serviceRequestReference("2021-1632746723494").build();
+
+        when(serviceRequestDomainService.create(any(),any())).thenReturn(serviceRequestResponseDtoSample);
+
+        MvcResult successServiceRequestPaymentResult = restActions
+            .post("/service-request/" + serviceRequestReferenceResult + "/pba-payments", serviceRequestPaymentDto)
+            .andExpect(status().isConflict())
+            .andReturn();
+
+    }
 
     @Test
     public void createPBAPaymentWithServiceRequestTimeOutTest() throws Exception {
