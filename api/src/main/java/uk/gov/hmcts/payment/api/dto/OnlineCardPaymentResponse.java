bf68768c4be9555e4a24ad790ac6b593053558ed
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-02-08 09:43:10 +0000

    PAY-5998 remove stray import

--- api/src/main/java/uk/gov/hmcts/payment/api/dto/OnlineCardPaymentResponse.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/dto/OnlineCardPaymentResponse.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.payment.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
