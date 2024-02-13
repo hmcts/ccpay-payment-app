89a939cf11a845e6b68ebd768e8af722786f2f1e
it.consulting@tutanota.com <it.consulting@tutanota.com>
2024-02-13 12:45:44 +0000

    PAY-5985: Restriction on Refund AMOUNT due to a Remission (Upfront and Retro) - 02      [PAY-5985] (https://tools.hmcts.net/jira/browse/PAY-5985).

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -202,7 +202,6 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
     private BigDecimal getRefundAmount(Payment payment,Remission remission){
 
         if (remission.getFee() == null || remission.getFee().getRemissions() == null ||  remission.getFee().getRemissions().isEmpty()) {
-            LOG.info("No Remission found for the fee, The refund value to used is {}",remission.getHwfAmount());
             return remission.getHwfAmount();
         }
         BigDecimal remissionAmount = payment.getAmount().subtract(
@@ -211,10 +210,8 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
                     )
                 );
         if (remissionAmount.compareTo(BigDecimal.ZERO) > 0){
-            LOG.info("Calculated refund to be sent to refund service {}",remissionAmount);
             return remissionAmount;
         }
-        LOG.info("Refund to be sent to refund service {}",remission.getHwfAmount());
         return remission.getHwfAmount();
     }
 
