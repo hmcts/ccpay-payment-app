6b4e955440551bfab6d406cd0b891753aa8cdf43
Ollie-Smith <oliver.smith@hmcts.net>
2024-03-07 11:07:46 +0000

    PAY-6933

--- api/src/main/java/uk/gov/hmcts/payment/api/controllers/AccountController.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/controllers/AccountController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,20 @@ import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.hmcts.payment.api.dto.AccountDto;
 import uk.gov.hmcts.payment.api.exception.AccountNotFoundException;
 import uk.gov.hmcts.payment.api.exception.LiberataServiceInaccessibleException;
+import uk.gov.hmcts.payment.api.exception.PbaAccountStatusException;
 import uk.gov.hmcts.payment.api.service.AccountService;
 
 @RestController
 @Tag(name = "AccountController", description = "Account REST API")
 public class AccountController {
 
+    private static final String STATUS_DELETED = "Deleted";
+    private static final String STATUS_ON_HOLD = "On-Hold";
+    private static final String JSON_STATUS = "status";
+    private static final String JSON_ERROR_MESSAGE = "error_message";
 
+    private static final String JSON_ERROR_MESSAGE_GONE = "The account has been deleted and is no longer available.";
+    private static final String JSON_ERROR_MESSAGE_ON_HOLD = "The account is on hold and temporarily unavailable.";
 
     private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
 
@@ -39,7 +47,12 @@ public class AccountController {
     @Operation(summary = "Get the account status and available balance for a PBA account number", description = "Get the account status and available balance for a PBA account number")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Account details retrieved"),
+        @ApiResponse(responseCode = "401", description = "Unauthorised"),
+        @ApiResponse(responseCode = "403", description = "Forbidden"),
         @ApiResponse(responseCode = "404", description = "Account not found"),
+        @ApiResponse(responseCode = "410", description = "PBA account was available but has now been deleted"),
+        @ApiResponse(responseCode = "412", description = "PBA account has been put on hold"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error"),
         @ApiResponse(responseCode = "503", description = "Failed to connect with Liberata")
     })
     @GetMapping(value = "/accounts/{accountNumber}")
@@ -48,12 +61,29 @@ public class AccountController {
             return accountService.retrieve(accountNumber);
         } catch (HttpClientErrorException ex) {
             LOG.error("Error while  calling account", ex);
-            throw new AccountNotFoundException("Account not found");
+            if (ex.getStatusCode() == HttpStatus.GONE || ex.getStatusCode() == HttpStatus.PRECONDITION_FAILED) {
+
+                JSONObject jsonResponse = new JSONObject();
+
+                switch (ex.getStatusCode()) {
+                    case GONE:
+                        jsonResponse.appendField(JSON_STATUS, STATUS_DELETED);
+                        jsonResponse.appendField(JSON_ERROR_MESSAGE, JSON_ERROR_MESSAGE_GONE);
+                        throw new PbaAccountStatusException(jsonResponse.toJSONString());
+                    case PRECONDITION_FAILED:
+                        jsonResponse.appendField(JSON_STATUS, STATUS_ON_HOLD);
+                        jsonResponse.appendField(JSON_ERROR_MESSAGE, JSON_ERROR_MESSAGE_ON_HOLD);
+                        throw new PbaAccountStatusException(jsonResponse.toJSONString());
+                    default:
+                        throw ex;
+                }
+            } else {
+                throw new AccountNotFoundException("Account not found");
+            }
+
         } catch (Exception ex) {
             throw new LiberataServiceInaccessibleException("Failed to connect with Liberata. " + ex.getMessage());
         }
-
-
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
--- api/src/test/java/uk/gov/hmcts/payment/api/controllers/AccountControllerTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/controllers/AccountControllerTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.payment.api.controllers;
 
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,13 +18,19 @@ import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.hmcts.payment.api.dto.AccountDto;
 import uk.gov.hmcts.payment.api.exception.AccountNotFoundException;
 import uk.gov.hmcts.payment.api.exception.LiberataServiceInaccessibleException;
+import uk.gov.hmcts.payment.api.exception.PbaAccountStatusException;
 import uk.gov.hmcts.payment.api.service.AccountService;
 import uk.gov.hmcts.payment.api.util.AccountStatus;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import static org.apache.commons.collections.MapUtils.toMap;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -91,6 +99,108 @@ public class AccountControllerTest {
         //For Any other account return account object
         when(accountServiceMock.retrieve(not(eq("PBA4324")))).thenReturn(expectedDto);
         accountController.getAccounts("PBA4324");
+    }
+
+
+    @Test
+    public void testGetAccounts_AccountGone() throws JSONException {
+        String accountNumber = "123";
+        JSONObject expectedJsonObject = new JSONObject();
+        expectedJsonObject.put("status", "Deleted");
+        expectedJsonObject.put("error_message", "The account has been deleted and is no longer available.");
+
+        HttpClientErrorException preconditionFailedException = new HttpClientErrorException(HttpStatus.GONE);
+
+        when(accountServiceMock.retrieve(accountNumber)).thenThrow(preconditionFailedException);
+
+        PbaAccountStatusException exception = assertThrows(PbaAccountStatusException.class,
+            () -> accountController.getAccounts(accountNumber));
+
+        Map<String, Object> expectedMap = toMap(expectedJsonObject);
+        String actualJsonString = exception.getMessage();
+
+        // Parse the actual JSON string into a JSONObject
+        JSONObject actualJsonObject = new JSONObject(actualJsonString);
+
+        // Convert the actual JSONObject to a map
+        Map<String, Object> actualMap = toMap(actualJsonObject);
+
+        assertEquals(expectedMap,actualMap);
+    }
+    @Test
+    public void testGetAccounts_AccountOnHold() throws JSONException {
+        String accountNumber = "123";
+        JSONObject expectedJsonObject = new JSONObject();
+        expectedJsonObject.put("status", "On-Hold");
+        expectedJsonObject.put("error_message", "The account is on hold and temporarily unavailable.");
+
+        HttpClientErrorException preconditionFailedException = new HttpClientErrorException(HttpStatus.PRECONDITION_FAILED);
+
+        when(accountServiceMock.retrieve(accountNumber)).thenThrow(preconditionFailedException);
+
+        PbaAccountStatusException exception = assertThrows(PbaAccountStatusException.class,
+            () -> accountController.getAccounts(accountNumber));
+
+        Map<String, Object> expectedMap = toMap(expectedJsonObject);
+        String actualJsonString = exception.getMessage();
+
+        // Parse the actual JSON string into a JSONObject
+        JSONObject actualJsonObject = new JSONObject(actualJsonString);
+
+        // Convert the actual JSONObject to a map
+        Map<String, Object> actualMap = toMap(actualJsonObject);
+
+        assertEquals(expectedMap,actualMap);
+    }
+
+
+    @Test
+    public void testGetAccounts_InternalServerError() {
+        String accountNumber = "123";
+        when(accountServiceMock.retrieve(accountNumber))
+            .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR));
+
+        AccountNotFoundException exception = assertThrows(AccountNotFoundException.class,
+            () -> accountController.getAccounts(accountNumber));
+
+        assertEquals("Account not found", exception.getMessage());
+    }
+
+    @Test
+    public void testGetAccounts_UnauthorisedError() {
+        String accountNumber = "123";
+        when(accountServiceMock.retrieve(accountNumber))
+            .thenThrow(new HttpClientErrorException(HttpStatus.UNAUTHORIZED));
+
+        AccountNotFoundException exception = assertThrows(AccountNotFoundException.class,
+            () -> accountController.getAccounts(accountNumber));
+
+        assertEquals("Account not found", exception.getMessage());
+    }
+
+
+    @Test
+    public void testGetAccounts_ForbiddenError() {
+        String accountNumber = "123";
+
+        when(accountServiceMock.retrieve(accountNumber))
+            .thenThrow(new HttpClientErrorException(HttpStatus.FORBIDDEN));
+
+        AccountNotFoundException exception = assertThrows(AccountNotFoundException.class,
+            () -> accountController.getAccounts(accountNumber));
+
+        assertEquals("Account not found", exception.getMessage());
+    }
+
+
+    private Map<String, Object> toMap(JSONObject jsonObject) throws JSONException {
+        Map<String, Object> map = new HashMap<>();
+        Iterator<String> keys = jsonObject.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            map.put(key, jsonObject.get(key));
+        }
+        return map;
     }
 
 }
