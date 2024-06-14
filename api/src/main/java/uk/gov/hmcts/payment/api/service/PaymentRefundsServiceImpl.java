8e65b0595ba489e9551fc8511ea65a495aa83369
it.consulting@tutanota.com <it.consulting@tutanota.com>
2024-06-14 09:07:25 +0100

    Merge branch 'master' of github.com:hmcts/ccpay-payment-app into PAY-7000
    # Conflicts:
    #	api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java

--- api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/service/PaymentRefundsServiceImpl.java
@@ -283,7 +283,6 @@ public class PaymentRefundsServiceImpl implements PaymentRefundsService {
 
         } catch (HttpClientErrorException e) {
             LOG.error("Catch the refund error", e.getMessage());
-            e.printStackTrace();
             LOG.error("There has been an error calling refunds endpoint.", e);
             refundListDtoResponse = null;
         }
