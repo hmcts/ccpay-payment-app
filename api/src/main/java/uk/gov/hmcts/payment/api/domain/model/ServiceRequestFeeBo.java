fa3f0988743cc6cbe09e31a706e07fe610a29627
David Jones <dave.jones@hmcts.net>
2024-09-09 23:14:08 +0100

    PAY-7311: Remove unused import.

--- api/src/main/java/uk/gov/hmcts/payment/api/domain/model/ServiceRequestFeeBo.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/domain/model/ServiceRequestFeeBo.java
@@ -8,7 +8,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import java.math.BigDecimal;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
