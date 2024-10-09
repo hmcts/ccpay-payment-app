23b52ea3db4cf8b2a09b17acd11386d42ee3d733
Sammi Chong <shinyee.chong@hmcts.net>
2024-10-09 16:23:41 +0100

    Remove WebSecurityConfigurerAdapter

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/serviceanduser/ServiceAndUserTestApplication.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/serviceanduser/ServiceAndUserTestApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -86,7 +85,7 @@ public class ServiceAndUserTestApplication {
 
     @Configuration
     @EnableWebSecurity
-    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    public class SecurityConfiguration {
 
         @Autowired
         private AuthCheckerServiceAndAnonymousUserFilter filter;
