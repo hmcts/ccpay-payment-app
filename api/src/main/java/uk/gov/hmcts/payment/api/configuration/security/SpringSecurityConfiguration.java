10a668502646876b51b51fbc39b8abcb43c9a539
Sammi Chong <shinyee.chong@hmcts.net>
2024-10-23 18:09:21 +0100

    Fix 503 errors

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/SpringSecurityConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/security/SpringSecurityConfiguration.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.payment.api.configuration.security;
 
 
-import edu.umd.cs.findbugs.annotations.SuppressWarnings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,9 +8,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
@@ -24,6 +22,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+@Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration {
 
@@ -35,90 +34,48 @@ public class SpringSecurityConfiguration {
     private static final String CASE_MANAGER_ROLE = "pui-case-manager";
     private static final String AUTHORISED_REFUNDS_ROLE = "payments-refund";
     private static final String AUTHORISED_REFUNDS_APPROVER_ROLE = "payments-refund-approver";
+    private final AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
+    private final AuthCheckerServiceAndAnonymousUserFilter authCheckerUserOnlyFilter;
 
-    @Configuration
-    @Order(1)
-    public static class ExternalApiSecurityConfigurationAdapter {
-
-
-        private AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
-
-        @Autowired
-        public ExternalApiSecurityConfigurationAdapter(RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                                       AuthenticationManager authenticationManager) {
-            authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
-            authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
-        }
-
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                .addFilter(authCheckerServiceOnlyFilter)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.GET, "/payments").authenticated()
-                    .requestMatchers(HttpMethod.GET, "/payments1").authenticated()
-                    .requestMatchers(HttpMethod.GET, "/card-payments/*/status").authenticated()
-                    .requestMatchers(HttpMethod.PATCH, "/payments/**").authenticated()
-                    .requestMatchers(HttpMethod.POST, "/telephony/callback").authenticated()
-                    .requestMatchers("/jobs/**").authenticated()
-                    .anyRequest().authenticated()
-                );
-        }
+    public SpringSecurityConfiguration(
+        RequestAuthorizer<Service> serviceRequestAuthorizer,
+        AuthenticationManager authenticationManager, RequestAuthorizer<User> userRequestAuthorizer
+    ) {
+        authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
+        authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
+        authCheckerUserOnlyFilter = new AuthCheckerServiceAndAnonymousUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
+        authCheckerUserOnlyFilter.setAuthenticationManager(authenticationManager);
     }
 
-    @Configuration
+    @Bean
+    @Order(1)
+    protected SecurityFilterChain configureExternal(HttpSecurity http) throws Exception {
+        http
+            .addFilter(authCheckerServiceOnlyFilter)
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/payments").authenticated()
+                .requestMatchers(HttpMethod.GET, "/payments1").authenticated()
+                .requestMatchers(HttpMethod.GET, "/card-payments/*/status").authenticated()
+                .requestMatchers(HttpMethod.PATCH, "/payments/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/telephony/callback").authenticated()
+                .requestMatchers("/jobs/**").authenticated()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+
+    @Bean
     @Order(2)
-    public static class InternalApiSecurityConfigurationAdapter {
-
-        private AuthCheckerServiceAndAnonymousUserFilter authCheckerFilter;
-
-
-        @Autowired
-        public InternalApiSecurityConfigurationAdapter(RequestAuthorizer<User> userRequestAuthorizer,
-                                                       RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                                       AuthenticationManager authenticationManager) {
-            authCheckerFilter = new AuthCheckerServiceAndAnonymousUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
-            authCheckerFilter.setAuthenticationManager(authenticationManager);
-        }
-
-        public void configure(WebSecurity web) {
-            web.ignoring().requestMatchers("/swagger-ui.html",
-                "/webjars/springfox-swagger-ui/**",
-                "/swagger-resources/**",
-                "/swagger-ui/**",
-                "/v3/**",
-                "/refdata/methods",
-                "/refdata/channels",
-                "/refdata/providers",
-                "/refdata/status",
-                "/health",
-                "/health/liveness",
-                "/health/readiness",
-                "/info",
-                "/favicon.ico",
-                "/mock-api/**",
-                "/");
-        }
-
-        @Bean
-        public AccessDeniedHandler accessDeniedHandler() {
-            return (request, response, ex) -> {
-                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                response.setContentType(MediaType.TEXT_HTML_VALUE);
-                response.setStatus(403);
-                response.getWriter().print("User does not have a valid role");
-            };
-        }
-
-        @SuppressWarnings(value = "SPRING_CSRF_PROTECTION_DISABLED", justification = "It's safe to disable CSRF protection as application is not being hit directly from the browser")
-        protected void configure(HttpSecurity http) throws Exception {
-            http.exceptionHandling(exceptionHandling -> exceptionHandling.accessDeniedHandler(accessDeniedHandler()));
-            http.addFilter(authCheckerFilter)
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))
-                .csrf(AbstractHttpConfigurer::disable) //NOSONAR
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-                .authorizeRequests()
+    protected SecurityFilterChain configureInternal(HttpSecurity http) throws Exception {
+        http.exceptionHandling(exceptionHandling -> exceptionHandling.accessDeniedHandler(accessDeniedHandler()));
+        http
+            .addFilter(authCheckerUserOnlyFilter)
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))
+            .csrf(AbstractHttpConfigurer::disable) //NOSONAR
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.GET, "/cases/{ccdcasenumber}/paymentgroups").hasAnyAuthority(PAYMENTS_ROLE, CASE_MANAGER_ROLE, FINANCE_MANAGER_ROLE, ORGANISATION_MANAGER_ROLE, USER_MANAGER_ROLE)
                 .requestMatchers(HttpMethod.GET, "/cases/{case}/payments").hasAuthority(PAYMENTS_ROLE)
                 .requestMatchers(HttpMethod.DELETE, "/fees/**").hasAuthority(PAYMENTS_ROLE)
@@ -136,8 +93,18 @@ public class SpringSecurityConfiguration {
                 .requestMatchers(HttpMethod.POST, "/payment-failures/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/payment-failures/**").hasAuthority(PAYMENTS_ROLE)
                 .requestMatchers(HttpMethod.PATCH, "/payment-failures/**").permitAll()
-                .anyRequest().authenticated();
-        }
+                .anyRequest().authenticated()
+            );
+        return http.build();
     }
 
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, ex) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.TEXT_HTML_VALUE);
+            response.setStatus(403);
+            response.getWriter().print("User does not have a valid role");
+        };
+    }
 }
