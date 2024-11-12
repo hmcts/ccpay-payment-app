720557e2be250c9a74ebdd9465f11fbe4ceca6d6
Sammi Chong <shinyee.chong@hmcts.net>
2024-11-12 09:56:04 +0000

    Edit SwaggerPublisher to use OpenTelemetry (unfinished)

--- api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/componenttests/SwaggerPublisher.java
@@ -1,10 +1,17 @@
 package uk.gov.hmcts.payment.api.componenttests;
 
+import com.azure.opentelemetry.exporters.azuremonitor.AzureMonitorExporterBuilder;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -18,6 +25,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -31,12 +39,21 @@ public class SwaggerPublisher {
 
     @Autowired
     private MockMvc mvc;
+
     @Autowired
     private WebApplicationContext webAppContext;
 
-    @BeforeEach
+    private Tracer tracer;
+
+    @Value("${azure.application-insights.instrumentation-key}")
+    private String instrumentationKey;
+
+    @Before
     public void setup() {
-        this.mvc = webAppContextSetup(webAppContext).build();
+        this.tracer = configureCustomTracer();
+        this.mvc = webAppContextSetup(this.webAppContext)
+            .apply(springSecurity())
+            .build();
     }
 
     @After
@@ -47,23 +64,52 @@ public class SwaggerPublisher {
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     public void generateDocs() throws Exception {
-        generateDocsForGroup("payment2");
-        generateDocsForGroup("payment-external-api");
-        generateDocsForGroup("reference-data");
+        Span span = tracer.spanBuilder("generateDocs").startSpan();
+        try {
+            generateDocsForGroup("payment2");
+            generateDocsForGroup("payment-external-api");
+            generateDocsForGroup("reference-data");
+        } finally {
+            span.end();
+        }
     }
 
     private void generateDocsForGroup(String groupName) throws Exception {
-        byte[] specs = mvc.perform(
-                get("/v3/api-docs?group=" + groupName)
-                    .header("Authorization", "Bearer spoof")
-            )
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsByteArray();
+        Span span = tracer.spanBuilder("generateDocsForGroup").startSpan();
+        try {
+            byte[] specs = mvc.perform(
+                    get("/v3/api-docs?group=" + groupName)
+                        .header("Authorization", "Bearer spoof")
+                )
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs." + groupName + ".json"))) {
-            outputStream.write(specs);
+            try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs." + groupName + ".json"))) {
+                outputStream.write(specs);
+            }
+        } finally {
+            span.end();
         }
+    }
+
+    private Tracer configureCustomTracer() {
+        //Creates an Azure Monitor Exporter using the instrumentationKey
+        var applicationInsightsExporter = new AzureMonitorExporterBuilder()
+            .instrumentationKey(instrumentationKey)
+            .buildExporter();
+
+        //Configures a TracerProvider to batch-process spans and send them to the Azure Monitor Exporter
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+            .addSpanProcessor(BatchSpanProcessor.builder(applicationInsightsExporter).build())
+            .build();
+
+        //Initializes the OpenTelemetry SDK allowing SwaggerPublisherTracer to be retrieved anywhere
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+            .setTracerProvider(tracerProvider)
+            .buildAndRegisterGlobal();
+
+        return openTelemetrySdk.getTracer("SwaggerPublisherTracer");
     }
 }
