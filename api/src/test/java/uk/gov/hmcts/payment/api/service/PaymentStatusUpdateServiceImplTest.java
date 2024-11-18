d3d31adf2ddc97ba0bd9a872a81b79b08e0c003c
Ollie-Smith <120727833+Ollie-Smith@users.noreply.github.com>
2024-11-18 15:56:39 +0000

    Pay 7452 (#1699)
    * PAY-7452
    
    * PAY-7452
    
    * PAY-7452
    
    * PAY-7452
    
    * PAY-7452

--- api/src/test/java/uk/gov/hmcts/payment/api/service/PaymentStatusUpdateServiceImplTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/service/PaymentStatusUpdateServiceImplTest.java
@@ -226,7 +226,6 @@ public class PaymentStatusUpdateServiceImplTest {
         Payment payment = getPayment();
         PaymentStatusChargebackDto paymentStatusChargebackDto =getPaymentStatusChargebackDtoForBadRequest();
         when(paymentRepository.findByReference(any())).thenReturn(Optional.of(payment));
-        when(paymentFailureRepository.findByPaymentReference(any())).thenReturn(Optional.of(getPaymentFailuresList()));
         assertThrows(
             InvalidPaymentFailureRequestException.class,
             () -> paymentStatusUpdateServiceImpl.insertChargebackPaymentFailure(paymentStatusChargebackDto)
@@ -704,7 +703,7 @@ public class PaymentStatusUpdateServiceImplTest {
 
         return PaymentStatusChargebackDto.paymentStatusChargebackRequestWith()
             .additionalReference("AR1234")
-            .amount(BigDecimal.valueOf(500))
+            .amount(BigDecimal.valueOf(600))
             .failureReference("FR12345")
             .eventDateTime("2021-10-10T10:10:10")
             .ccdCaseNumber("123456")
