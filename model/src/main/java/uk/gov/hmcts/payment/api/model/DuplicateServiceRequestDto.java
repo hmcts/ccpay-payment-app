0c7fcfc3a95e62c0ec46d6369b7028ce0a6ff684
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-12-13 11:23:01 +0000

    PAY-7154 more columns

--- model/src/main/java/uk/gov/hmcts/payment/api/model/DuplicateServiceRequestDto.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/model/DuplicateServiceRequestDto.java
@@ -1,7 +1,8 @@
 package uk.gov.hmcts.payment.api.model;
 
 public interface DuplicateServiceRequestDto {
+    String getFee_codes();
+    Integer getPayment_link_id();
     String getCcd_case_number();
-
-    Integer getCount();
+    String getEnterprise_service_name();
 }
