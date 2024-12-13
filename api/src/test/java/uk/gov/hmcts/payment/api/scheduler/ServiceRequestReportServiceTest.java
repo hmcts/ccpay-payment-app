621c273a8f01c0f80ddb2bf0bd806d3982bcd097
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-12-13 15:09:40 +0000

    PAY-7154 logic changes

--- api/src/test/java/uk/gov/hmcts/payment/api/scheduler/ServiceRequestReportServiceTest.java
+++ api/src/test/java/uk/gov/hmcts/payment/api/scheduler/ServiceRequestReportServiceTest.java
@@ -20,11 +20,10 @@ import java.util.*;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(org.mockito.junit.MockitoJUnitRunner.class)
-@DirtiesContext(classMode= DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ServiceRequestReportServiceTest {
 
     @InjectMocks
@@ -41,13 +40,14 @@ public class ServiceRequestReportServiceTest {
 
     private ServiceRequestReportConfig serviceRequestReportConfig;
 
-    private DuplicateServiceRequestDto duplicateServiceRequestDto;
+    private DuplicateServiceRequestDto duplicateServiceRequestDto1;
+    private DuplicateServiceRequestDto duplicateServiceRequestDto2;
 
     @Before
     public void setup() {
         serviceRequestReportConfig = new ServiceRequestReportConfig("fromEmail", new String[]{"toEmail"}, "emailSubject", "emailMessage", true);
 
-        duplicateServiceRequestDto = new DuplicateServiceRequestDto() {
+        duplicateServiceRequestDto1 = new DuplicateServiceRequestDto() {
             @Override
             public String getFee_codes() {
                 return "FEE0123";
@@ -67,14 +67,35 @@ public class ServiceRequestReportServiceTest {
             public String getEnterprise_service_name() {
                 return "Probate";
             }
-
         };
 
-        DuplicateServiceRequestKey duplicateServiceRequestKey =  DuplicateServiceRequestKey.builder()
-            .service("Probate")
-            .feeCodes("FEE0123")
-            .ccd("123")
-            .build();
+        duplicateServiceRequestDto2 = new DuplicateServiceRequestDto() {
+            @Override
+            public String getFee_codes() {
+                return "FEE0123";
+            }
+
+            @Override
+            public Integer getPayment_link_id() {
+                return 124;
+            }
+
+            @Override
+            public String getCcd_case_number() {
+                return "123";
+            }
+
+            @Override
+            public String getEnterprise_service_name() {
+                return "Probate";
+            }
+        };
+
+    DuplicateServiceRequestKey duplicateServiceRequestKey = DuplicateServiceRequestKey.builder()
+        .service("Probate")
+        .feeCodes("FEE0123")
+        .ccd("123")
+        .build();
 
         when(reportConfig.getFrom()).thenReturn("fromEmail");
         when(reportConfig.getTo()).thenReturn(new String[]{"toEmail"});
@@ -87,9 +108,10 @@ public class ServiceRequestReportServiceTest {
     public void generateDuplicateSRCsvAndSendEmailWithValidDate() {
         LocalDate date = LocalDate.of(2024, 6, 1);
         List<DuplicateServiceRequestDto> duplicateServiceRequestDtos = new ArrayList<>();
-        duplicateServiceRequestDtos.add(duplicateServiceRequestDto);
+        duplicateServiceRequestDtos.add(duplicateServiceRequestDto1);
+        duplicateServiceRequestDtos.add(duplicateServiceRequestDto2);
 
-        when(paymentFeeLinkRepository.getDuplicates(any(LocalDate.class))).thenReturn( Optional.of(duplicateServiceRequestDtos));
+        when(paymentFeeLinkRepository.getDuplicates(any(LocalDate.class))).thenReturn(Optional.of(duplicateServiceRequestDtos));
 
         serviceRequestReportService.generateDuplicateSRCsvAndSendEmail(date);
 
@@ -101,6 +123,19 @@ public class ServiceRequestReportServiceTest {
         assertThat(argument.getValue().getMessage()).isEqualTo("emailMessage");
         assertThat(argument.getValue().hasAttachments()).isTrue();
         assertThat(argument.getValue().getAttachments().get(0).getFilename()).startsWith(serviceRequestReportConfig.DUPLICATE_SR_CSV_FILE_PREFIX);
+    }
+
+    @Test
+    public void generateDuplicateSRCsvAndSendEmailWithValidDateButNoDuplicates() {
+        LocalDate date = LocalDate.of(2024, 6, 1);
+        List<DuplicateServiceRequestDto> duplicateServiceRequestDtos = new ArrayList<>();
+        duplicateServiceRequestDtos.add(duplicateServiceRequestDto1);
+
+        when(paymentFeeLinkRepository.getDuplicates(any(LocalDate.class))).thenReturn(Optional.of(duplicateServiceRequestDtos));
+
+        serviceRequestReportService.generateDuplicateSRCsvAndSendEmail(date);
+
+        verifyNoInteractions(emailService);
 
     }
 }
