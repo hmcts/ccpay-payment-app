cd179f907e97b633cc21e41672d3030b08868fd0
it.consulting@tutanota.com <it.consulting@tutanota.com>
2025-09-16 14:43:57 +0100

    PAY-7523: Can the amount due be confirmed on this case.          [PAY-7523] (https://tools.hmcts.net/jira/browse/PAY-7523).

--- model/src/main/java/uk/gov/hmcts/payment/api/service/FeePayApportionServiceImpl.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/FeePayApportionServiceImpl.java
@@ -42,13 +42,14 @@ public class FeePayApportionServiceImpl implements FeePayApportionService {
 
 
     private List<FeePayApportion> getFeePayApportion(Payment payment) {
-        Optional<List<FeePayApportion>> apportions = feePayApportionRepository.findByPaymentId(payment.getId());
-        if (apportions.isPresent()) {
-            LOG.info("-----------FeePayApportion from the DB.");
-            return apportions.get();
-        } else {
+        List<FeePayApportion>  apportions = payment.getPaymentLink().getApportions();
+        if (apportions!=null) {
             LOG.info("-----------FeePayApportion from Memory and not persisted yet.");
-            return payment.getPaymentLink().getApportions();
+            return apportions;
+        } else {
+            Optional<List<FeePayApportion>> apportionsFromTheDB = feePayApportionRepository.findByPaymentId(payment.getId());
+            LOG.info("-----------FeePayApportion from the DB.");
+            return apportionsFromTheDB.orElse(Collections.emptyList());
         }
     }
 
