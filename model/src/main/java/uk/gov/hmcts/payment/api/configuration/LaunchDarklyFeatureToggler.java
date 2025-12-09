942990fa4428064b30c93710d9c3778661db8d8c
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-12-09 10:53:54 +0000

    CME-840 reduce logging

--- model/src/main/java/uk/gov/hmcts/payment/api/configuration/LaunchDarklyFeatureToggler.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/configuration/LaunchDarklyFeatureToggler.java
@@ -22,8 +22,6 @@ public class LaunchDarklyFeatureToggler implements FeatureToggler {
     }
 
     public boolean getBooleanValue(String key, Boolean defaultValue) {
-
-        LOG.info("userName in LaunchDarklyFeatureToggler: {}", userName);
         LDUser user = new LDUser(userName);
 
         return ldClient.boolVariation(
