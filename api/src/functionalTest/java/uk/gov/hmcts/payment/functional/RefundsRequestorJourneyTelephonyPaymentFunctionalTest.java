55af922a75ea476df38cb51629c636eab23e6105
shravanmechineni <shravanmechineni5@gmail.com>
2026-06-17 11:32:19 +0100

    enable telephony refund request tests

--- api/src/functionalTest/java/uk/gov/hmcts/payment/functional/RefundsRequestorJourneyTelephonyPaymentFunctionalTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/payment/functional/RefundsRequestorJourneyTelephonyPaymentFunctionalTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.payment.functional;
 
 import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -42,10 +43,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @ContextConfiguration(classes = TestContextConfiguration.class)
 @ActiveProfiles({"functional-tests", "liberataMock"})
-//@SpringBootTest(classes = {PaymentApiApplication.class})
 public class RefundsRequestorJourneyTelephonyPaymentFunctionalTest {
 
     private static String USER_TOKEN;
