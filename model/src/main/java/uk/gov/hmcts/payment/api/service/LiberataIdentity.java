8f6732375ba89bb1967855ef9695a60b7f3cde2b
it.consulting@tutanota.com <it.consulting@tutanota.com>
2026-07-06 14:32:09 +0100

    PAY-8592: ISSUE-157 - Unauthenticated access to payment status by UUID (IDOR)                 [PAY-8614] (https://tools.hmcts.net/jira/browse/PAY-8614).

--- model/src/main/java/uk/gov/hmcts/payment/api/service/LiberataIdentity.java
+++ model/src/main/java/uk/gov/hmcts/payment/api/service/LiberataIdentity.java
@@ -1,14 +1,27 @@
 package uk.gov.hmcts.payment.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.payment.api.dto.liberata.identity.TokenResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import uk.gov.hmcts.payment.api.dto.LiberataDto;
+import java.util.Collections;
 
 @Service
 public class LiberataIdentity {
 
     private TokenResponse cachedToken;
+
+    @Autowired()
+    @Qualifier("liberataRestTemplate")
+    private RestTemplate liberataRestTemplate;
 
     private TokenResponse getToken() {
         if (cachedToken != null && !cachedToken.isExpired()) {
@@ -34,10 +47,35 @@ public class LiberataIdentity {
         return fetchNewToken();
     }
 
+
     private TokenResponse fetchNewToken() {
-        // call API
-        return new TokenResponse();
+
+        String url = "https://lascustomerportaluat.liberata.com/pba_api_uat/api/auth/token";
+
+        LiberataDto payload = new LiberataDto();
+        payload.setEmail("joe@bloggs.com");
+        payload.setPassword("Password123");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<LiberataDto> request =
+            new HttpEntity<>(payload, headers);
+
+        try
+        {
+            ResponseEntity<TokenResponse> response =
+                liberataRestTemplate.postForEntity(url, request, TokenResponse.class);
+
+            System.out.println("Error fetching token from Liberata: " + response);
+            System.out.println("Error fetching token from Liberata: " + response.getBody());
+            return response != null ? response.getBody() : null;
+
+        } catch (Exception exception) {
+            System.out.println("Error fetching token from Liberata: " + exception.getMessage());
+            exception.printStackTrace();
+            return null;
+        }
     }
-
-
 }
