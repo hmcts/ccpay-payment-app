150cff44324b284a3e1240baed24c3e7a58efa88
it.consulting@tutanota.com <it.consulting@tutanota.com>
2026-09-18 14:55:23 +0100

    https://hmcts.atlassian.net/browse/PAY-8739

--- api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/payment/api/configuration/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.payment.api.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -11,8 +12,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
 @Configuration
 public class RestTemplateConfiguration {
@@ -52,10 +64,88 @@ public class RestTemplateConfiguration {
         return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
     }
 
-    @Bean (value = "liberataRestTemplate")
+
+    @Bean(name = "liberataRestTemplate")
     public RestTemplate liberataRestTemplate() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+            new HttpComponentsClientHttpRequestFactory();
+
+        // Connection settings
+        requestFactory.setConnectionRequestTimeout(30000);
+        requestFactory.setConnectionRequestTimeout(30000);
+        requestFactory.setReadTimeout(30000);
+
+        RestTemplate restTemplate = new RestTemplate(
+            new BufferingClientHttpRequestFactory(requestFactory));
+
+        restTemplate.setInterceptors(
+            Collections.singletonList(new LoggingInterceptor()));
+
+        return restTemplate;
     }
+
+    @Slf4j
+    public static class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(
+            HttpRequest request,
+            byte[] body,
+            ClientHttpRequestExecution execution) throws IOException {
+
+            logRequest(request, body);
+
+            ClientHttpResponse response = execution.execute(request, body);
+
+            logResponse(response);
+
+            return response;
+        }
+
+        private void logRequest(HttpRequest request, byte[] body) {
+
+            System.out.println("=================================================");
+            System.out.println("HTTP REQUEST");
+            System.out.println("=================================================");
+            System.out.println("URI         : " + request.getURI());
+            System.out.println("Method      : " + request.getMethod());
+
+            System.out.println("Headers:");
+            request.getHeaders().forEach((key, value) ->
+                System.out.println(key + " : " + value));
+
+            System.out.println("Body:");
+            System.out.println(new String(body, StandardCharsets.UTF_8));
+
+            System.out.println("=================================================");
+        }
+
+        private void logResponse(ClientHttpResponse response)
+            throws IOException {
+
+            String responseBody =
+                StreamUtils.copyToString(
+                    response.getBody(),
+                    StandardCharsets.UTF_8);
+
+            System.out.println("=================================================");
+            System.out.println("HTTP RESPONSE");
+            System.out.println("=================================================");
+            System.out.println("Status Code : " + response.getStatusCode());
+            System.out.println("Status Text : " + response.getStatusText());
+
+            System.out.println("Headers:");
+            response.getHeaders().forEach((key, value) ->
+                System.out.println(key + " : " + value));
+
+            System.out.println("Body:");
+            System.out.println(responseBody);
+
+            System.out.println("=================================================");
+        }
+    }
+
 
     @Bean (value = "restTemplateIacSupplementaryInfo")
     public RestTemplate restTemplateIacSupplementaryInfo() {
