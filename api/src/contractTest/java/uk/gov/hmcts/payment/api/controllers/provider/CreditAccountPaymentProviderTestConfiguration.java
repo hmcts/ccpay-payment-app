bd3e251c19acb26be123b54690a7d8990afab6d2
it.consulting@tutanota.com <it.consulting@tutanota.com>
2026-09-21 12:55:16 +0100

    https://hmcts.atlassian.net/browse/PAY-8739

--- api/src/contractTest/java/uk/gov/hmcts/payment/api/controllers/provider/CreditAccountPaymentProviderTestConfiguration.java
+++ api/src/contractTest/java/uk/gov/hmcts/payment/api/controllers/provider/CreditAccountPaymentProviderTestConfiguration.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.payment.api.dto.mapper.PaymentDtoMapper;
 import uk.gov.hmcts.payment.api.mapper.CreditAccountPaymentRequestMapper;
 import uk.gov.hmcts.payment.api.mapper.PBAPaymentMapper;
 import uk.gov.hmcts.payment.api.mapper.PBAStatusErrorMapper;
+import uk.gov.hmcts.payment.api.mapper.liberata.identity.AccessTokenDtoToTokenResponseMapper;
 import uk.gov.hmcts.payment.api.model.FeePayApportionRepository;
 import uk.gov.hmcts.payment.api.model.Payment2Repository;
 import uk.gov.hmcts.payment.api.model.PaymentChannelRepository;
@@ -85,6 +86,11 @@ public class CreditAccountPaymentProviderTestConfiguration {
     @Bean
     public PBAPaymentMapper  pBAPaymentMapper() {
         return Mockito.mock(PBAPaymentMapper.class);
+    }
+
+    @Bean
+    public AccessTokenDtoToTokenResponseMapper  accessTokenDtoToTokenResponseMapper() {
+        return Mockito.mock(AccessTokenDtoToTokenResponseMapper.class);
     }
 
 
