a1266fb1b1e932b3eae184c1b8a3ffda5596b9dc
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-09-24 12:07:01 +0100

    CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -89,7 +89,7 @@ module "payment-database-v15" {
   pgsql_version        = var.postgresql_flexible_sql_version
 
   # Add service criticality param
-  service_criticality = var.service_criticality
+  service_criticality  = var.service_criticality
 
   action_group_name          = join("-", [var.db_monitor_action_group_name, local.db_server_name, var.env])
   email_address_key          = var.db_alert_email_address_key
